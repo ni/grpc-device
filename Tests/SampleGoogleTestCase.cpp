a9c6b48c5ceb6f9141b35a6710db19303e983733
Ryan Eckenrode <ryan.eckenrode@ni.com>
2021-01-14 10:19:57 -0600

    GoogleTest samples as part of build
    Initial submission has two google tests that should be run as part of the build. This first submission has one of the tests failing to make sure it fails the build.

---
+++ Tests/SampleGoogleTestCase.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+TEST(SampleTest, Expect_Less_Than)
+{
+    // Simple google less than test with a custom failure message.
+    EXPECT_LT(2, 2) << "1 should be less than 2.";
+}
+
+TEST(SampleTest, Expect_Equals)
+{
+    // Simple google test  with a custom failure message.
+    EXPECT_EQ(42, 42) << "42 should equal 42.";
+}
