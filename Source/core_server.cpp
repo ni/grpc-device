707973a5877c31e4060efd5c32de8562a1b2db59
Kyle Sullivan <kyle.sullivan@ni.com>
2021-01-14 16:49:32 -0600

    Implement review feedback in core_server

--- Source/core_server.cpp
+++ Source/core_server.cpp
@@ -1,49 +1,31 @@
-//---------------------------------------------------------------------
-//---------------------------------------------------------------------
-#include <thread>
-#include <sstream>
-#include <fstream>
-#include <iostream>
-#include <atomic>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 
-//---------------------------------------------------------------------
-//---------------------------------------------------------------------
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
+const char* klocalhostAddress = "0.0.0.0:50051";
 
-//---------------------------------------------------------------------
-//---------------------------------------------------------------------
-using namespace std;
-
-const string localhostAddress = "0.0.0.0:50051";
-
-//---------------------------------------------------------------------
-//---------------------------------------------------------------------
-void RunServer(int argc, char **argv)
+namespace {
+	
+static void RunServer(int argc, char **argv)
 {
-	grpc::EnableDefaultHealthCheckService(true);
-	grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+   grpc::EnableDefaultHealthCheckService(true);
+   grpc::reflection::InitProtoReflectionServerBuilderPlugin();
 
-	ServerBuilder builder;
+   grpc::ServerBuilder builder;
 	
-    // Listen on the given address without any authentication mechanism.
-	builder.AddListeningPort(localhostAddress, grpc::InsecureServerCredentials());
+   // Listen on the given address without any authentication mechanism.
+   builder.AddListeningPort(klocalhostAddress, grpc::InsecureServerCredentials());
 	
-	// Assemble the server.
-	auto server = builder.BuildAndStart();	
+   // Assemble the server.
+   auto server = builder.BuildAndStart();	
 
-	cout << "Server listening on " << localhostAddress << endl;
+   std::cout << "Server listening on " << klocalhostAddress << std::endl;
 	
-	// This call will block until another thread shuts down the server.
-	server->Wait();
+   // This call will block until another thread shuts down the server.
+   server->Wait();
 }
 
-//---------------------------------------------------------------------
-//---------------------------------------------------------------------
+}
+
 int main(int argc, char **argv)
 {
 	RunServer(argc, argv);
