8194d92f0177a3ae52f0498f967ae3beecfacb7f
Eric Peterson <eric.peterson@ni.com>
2021-01-21 17:29:56 -0600

    Working test

--- tests/unit/hardware/grpc/core_service_tests.cpp
+++ tests/unit/hardware/grpc/core_service_tests.cpp
@@ -1,18 +1,21 @@
 #include "../source/hardware/grpc/core_service.h"
 #include <gtest/gtest.h>
+#include <grpcpp/impl/grpc_library.h>
+
+// fixes seg faults caused by https://github.com/grpc/grpc/issues/14633
+static grpc::internal::GrpcLibraryInitializer g_gli_initializer;
 
 TEST(ManageSessionTests, ReserveWithNewName_Reserves)
 {
     ni::hardware::grpc::impl::CoreService* service = new ni::hardware::grpc::impl::CoreService();
-    auto session = new ni::hardware::grpc::ViSession;
-    session->set_sessionname("session_name");
+    auto session = service->AddSession(ni::hardware::grpc::ViSession(), "userId", NULL);
 
     auto request = new ni::hardware::grpc::ReserveRequest;
     request->set_allocated_session(session);
 
     ::grpc::ServerContext context;
-    ni::hardware::grpc::ReserveResponse response;
-    service->Reserve(&context, request, &response);
+    auto response = new ni::hardware::grpc::ReserveResponse;
+    service->Reserve(&context, request, response);
 
-    EXPECT_EQ(response.status, ReserveResponse_ReserveStatus_Reserved);
+    EXPECT_EQ(response->status(), ni::hardware::grpc::ReserveResponse_ReserveStatus_Reserved);
 }
