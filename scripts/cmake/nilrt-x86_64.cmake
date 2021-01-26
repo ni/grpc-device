3a1004b2098a1b8726785174b7d5da3d7465b150
Charlie J <5367780+SparkingSpork@users.noreply.github.com>
2021-01-26 11:39:56 -0500

    Fixing up comments, adding badge, simplifying CMake toolchain

--- scripts/cmake/nilrt-x86_64.cmake
+++ scripts/cmake/nilrt-x86_64.cmake
@@ -1,23 +1,34 @@
+#----------------------------------------------------------------------
 # CMake toolchain file for cross-compiling for NI Linux Real-Time
+#----------------------------------------------------------------------
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-# root path for toolchains
+#----------------------------------------------------------------------
+# Path variables for toolchains
+#----------------------------------------------------------------------
 find_program(COMPILER_PATH x86_64-nilrt-linux-gcc)
 get_filename_component(toolchain_path ${COMPILER_PATH}/../../../../.. REALPATH DIRECTORY)
+set(include_path core2-64-nilrt-linux/usr/include/c++/6.3.0)
 
-# compilers
+#----------------------------------------------------------------------
+# Compilers
+#----------------------------------------------------------------------
 set(CMAKE_C_COMPILER x86_64-nilrt-linux-gcc)
 set(CMAKE_CXX_COMPILER x86_64-nilrt-linux-g++)
 
-# compiler flags
+#----------------------------------------------------------------------
+# Default compiler flags
+#----------------------------------------------------------------------
 set(CMAKE_SYSROOT ${toolchain_path}/core2-64-nilrt-linux)
-set(CMAKE_<LANG>_STANDARD_INCLUDE_DIRECTORIES ${toolchain_path}/core2-64-nilrt-linux/usr/include/c++/6.3.0 ${toolchain_path}/core2-64-nilrt-linux/usr/include/c++/6.3.0/x86_64-nilrt-linux)
+set(CMAKE_<LANG>_STANDARD_INCLUDE_DIRECTORIES ${toolchain_path}/${include_path} ${toolchain_path}/${include_path}/x86_64-nilrt-linux)
 set(CMAKE_<LANG>_FLAGS "-Wall -fmessage-length=0")
 set(CMAKE_<LANG>_FLAGS_DEBUG "-O0 -g3")
 set(CMAKE_<LANG>_FLAGS_RELEASE "-O3")
 
-# define search behavior
+#----------------------------------------------------------------------
+# Define proper search behavior for cross compilation
+#----------------------------------------------------------------------
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
