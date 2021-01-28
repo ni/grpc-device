c071ae17f0d9b7ec0e5e3cb923544dbb9aeb8c55
Jonathan Lugo <48842985+jonlugoatx@users.noreply.github.com>
2021-01-28 17:03:09 -0600

    Add SharedLibrary class to use when dynamically loading driver API libraries. (#13)
    * Add SharedLibrary and move tests into source
    
    * Add test api library - not building yet
    
    * Fix test API build failures and add .orig to .gitignore
    
    * Write auto tests and get them passing
    
    * Fix Linux build errors and address PR feedback.
    
    * Add new line to ends of files.
    
    * Fixed Windows build errors
    
    * Remove extern C from function implementations in cpp

---
+++ source/core_server/hardware/grpc/internal/shared_library.h
@@ -0,0 +1,50 @@
+#ifndef NI_HARDWARE_GRPC_INTERNAL_SHARED_LIBRARY
+#define NI_HARDWARE_GRPC_INTERNAL_SHARED_LIBRARY
+
+#include <string>
+
+#if defined(_MSC_VER)
+   #include <Windows.h>
+#endif
+
+
+namespace ni
+{
+namespace hardware
+{
+namespace grpc
+{
+namespace internal
+{
+
+#ifdef _MSC_VER
+   typedef HMODULE LibraryHandle;
+#else
+   typedef void* LibraryHandle;
+#endif
+
+   class SharedLibrary
+   {
+   public:
+      SharedLibrary(const char* library_name);
+      SharedLibrary(const SharedLibrary& other);
+      virtual ~SharedLibrary();
+
+      void swap(SharedLibrary& other);
+      bool is_loaded() const;
+      LibraryHandle get_handle() const;
+      void load();
+      void unload();
+      const void* get_function_pointer(const char* name) const;
+
+   private:
+      std::string library_name_;
+      LibraryHandle handle_;
+   };
+
+} // namespace internal
+} // namespace grpc
+} // namespace hardware
+} // namespace ni
+
+#endif // NI_HARDWARE_GRPC_INTERNAL_SHARED_LIBRARY
---
+++ source/tests/test_api/test_api.h
@@ -0,0 +1,32 @@
+
+#include <cstddef>
+
+#if defined(_MSC_VER)
+   #define EXPORT __declspec(dllexport)
+   #define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+   #define EXPORT __attribute__((visibility("default")))
+   #define IMPORT
+#else
+   #define EXPORT
+   #define IMPORT
+   #pragma warning Unknown dynamic link import/export semantics
+#endif
+
+#if defined(TEST_API_BUILDING)
+   #define TEST_API_FUNCTION extern "C" EXPORT
+#else
+   #define TEST_API_FUNCTION extern "C" IMPORT
+#endif
+
+typedef void* TestSession;
+
+TEST_API_FUNCTION int niTestApiCreateSession(TestSession* session);
+
+TEST_API_FUNCTION int niTestApiCloseSession(TestSession session);
+
+TEST_API_FUNCTION int niTestApiWriteSessionData(TestSession session, int attribute_id, const char* value);
+
+TEST_API_FUNCTION int niTestApiReadSessionData(TestSession session, int attribute_id, size_t* value_length, char* value_buffer);
+
+TEST_API_FUNCTION int niTestApiRemoveSessionData(TestSession session, int attribute_id);
