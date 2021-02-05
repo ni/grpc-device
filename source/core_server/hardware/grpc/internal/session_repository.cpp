741480084d2b3b4d823b2c926db5e9194c579a51
Kyle Sullivan <kyle.sullivan@ni.com>
2021-02-05 12:18:44 -0600

    fix spacing after merge

--- source/core_server/hardware/grpc/internal/session_repository.cpp
+++ source/core_server/hardware/grpc/internal/session_repository.cpp
@@ -12,7 +12,7 @@ namespace internal
    {
    }
 
-int SessionRepository::add_session(
+   int SessionRepository::add_session(
       const std::string& session_name,
       std::function<std::tuple<int, uint64_t>()> init_func,
       CleanupSessionFunc cleanup_func,
