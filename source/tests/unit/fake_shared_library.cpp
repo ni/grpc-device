4886dc7bac127642ee1b322f02b62459c120532b
Charlie J <5367780+SparkingSpork@users.noreply.github.com>
2021-02-05 19:00:46 -0500

    Adding fake for shared library class

---
+++ source/tests/unit/fake_shared_library.cpp
@@ -0,0 +1,69 @@
+#include "fake_shared_library.h"
+
+namespace ni
+{
+namespace tests
+{
+namespace unit
+{
+namespace hardware
+{
+namespace grpc
+{
+namespace internal
+{
+
+  FakeSharedLibrary::FakeSharedLibrary(const char* library_name)
+    : SharedLibrary(library_name)
+  {
+  }
+
+  FakeSharedLibrary::~FakeSharedLibrary()
+  {
+    unload();
+  }
+
+  bool FakeSharedLibrary::is_loaded() const
+  {
+    return is_loaded_;
+  }
+
+  void FakeSharedLibrary::load()
+  {
+    if (!mock_function_pointers_.empty())
+    {
+      return;
+    }
+    create_function_map();
+    handle_ = nullptr;
+    is_loaded_ = true;
+  }
+
+  void FakeSharedLibrary::unload()
+  {
+    mock_function_pointers_.clear();
+    handle_ = nullptr;
+    is_loaded_ = false;
+  }
+
+  const void* FakeSharedLibrary::get_function_pointer(const char* name) const
+  {
+    return get_function_from_map(name);
+  }
+
+  void FakeSharedLibrary::register_function(const char* name, void* func_pointer)
+  {
+    mock_function_pointers_[name] = func_pointer;
+  }
+
+  const void* FakeSharedLibrary::get_function_from_map(const char* name) const
+  {
+    return mock_function_pointers_.at(name);
+  }
+
+} // namespace internal
+} // namespace grpc
+} // namespace hardware
+} // namespace unit
+} // namespace tests
+} // namespace ni
---
+++ source/tests/unit/fake_shared_library.h
@@ -0,0 +1,48 @@
+#ifndef NI_HARDWARE_GRPC_INTERNAL_SHARED_LIBRARY_FAKE
+#define NI_HARDWARE_GRPC_INTERNAL_SHARED_LIBRARY_FAKE
+
+#include "core_server/hardware/grpc/internal/shared_library.h"
+#include <map>
+
+namespace ni
+{
+namespace tests
+{
+namespace unit
+{
+namespace hardware
+{
+namespace grpc
+{
+namespace internal
+{
+
+  class FakeSharedLibrary : public ni::hardware::grpc::internal::SharedLibrary
+  {
+  public:
+    FakeSharedLibrary(const char* library_name);
+    virtual ~FakeSharedLibrary();
+
+    virtual void create_function_map() = 0;
+    virtual bool is_loaded() const final;
+    virtual void load() final;
+    virtual void unload() final;
+    virtual const void* get_function_pointer(const char* name) const final;
+
+  protected:
+    void register_function(const char* name, void* func_pointer);
+
+  private:
+    std::map<const char*, void*> mock_function_pointers_;
+    bool is_loaded_;
+    const void* get_function_from_map(const char* name) const;
+  };
+
+} // namespace internal
+} // namespace grpc
+} // namespace hardware
+} // namespace unit
+} // namespace tests
+} // namespace ni
+
+#endif // NI_HARDWARE_GRPC_INTERNAL_SHARED_LIBRARY_FAKE
