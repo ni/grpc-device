2264f78479f349a0dab9ffb1ceeb21ea935de79c
Jonathan Lugo <jonathan.lugo@ni.com>
2021-02-06 19:15:05 -0600

    Creating .clang-format file and running format on files.

--- source/core_server/hardware/grpc/internal/semaphore.cpp
+++ source/core_server/hardware/grpc/internal/semaphore.cpp
@@ -1,35 +1,33 @@
 #include "semaphore.h"
 
-namespace ni
-{
-namespace hardware
-{
-namespace grpc
-{
-namespace internal
-{
-   Semaphore::Semaphore (int count)
-      : count_(count)
-   {
-   }
+namespace ni {
+namespace hardware {
+namespace grpc {
+namespace internal {
 
-   void Semaphore::notify()
-   {
-      std::unique_lock<std::mutex> lock(mtx_);
-      ++count_;
-      cv_.notify_one();
-   }
+Semaphore::Semaphore(int count)
+    : count_(count)
+{
+}
 
-   void Semaphore::wait()
-   {
-      std::unique_lock<std::mutex> lock(mtx_);
+void Semaphore::notify()
+{
+  std::unique_lock<std::mutex> lock(mtx_);
+  ++count_;
+  cv_.notify_one();
+}
 
-      while(count_ == 0) {
-         cv_.wait(lock);
-      }
-      count_--;
-   }
-} // namespace internal
-} // namespace grpc
-} // namespace hardware
-} // namespace ni
+void Semaphore::wait()
+{
+  std::unique_lock<std::mutex> lock(mtx_);
+
+  while (count_ == 0) {
+    cv_.wait(lock);
+  }
+  count_--;
+}
+
+}  // namespace internal
+}  // namespace grpc
+}  // namespace hardware
+}  // namespace ni
