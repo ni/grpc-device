ef1a34d468f8f955a01258a351643254ff650377
Kyle Sullivan <kyle.sullivan@ni.com>
2021-02-11 23:40:42 -0600

    Add server config back

---
+++ source/core_server/hardware/grpc/internal/server_configuration.cpp
@@ -0,0 +1,63 @@
+#include "server_configuration.h"
+
+namespace ni {
+namespace hardware {
+namespace grpc {
+namespace internal {
+  
+const char* ServerConfiguration::klocalhostAddressPrefix = "0.0.0.0:";
+const char* ServerConfiguration::kconfigurationFile = "server.json";
+const char* ServerConfiguration::kportKey = "port";
+const char* ServerConfiguration::kConfigFileNotFoundMessage = "The server configuration file was not found.";
+const char* ServerConfiguration::kinvalidPortMessage = "The specified port number must between 0 and 65535.";
+
+ServerConfiguration::ServerConfiguration(const std::string& config_file_path)
+  : config_file_path_(config_file_path)
+{
+}
+
+void ServerConfiguration::parse_config()
+{
+   if(config_file_ == nullptr){
+     // TODO: Prefer a passed in configuration file path
+     // and then search here (next to the binary) and at
+     // platform specific default config file locations.
+      std::ifstream input_stream(kconfigurationFile);
+      if (!input_stream) {
+        throw ConfigFileNotFoundException();
+      }
+      input_stream >> config_file_;
+   }
+}
+
+std::string ServerConfiguration::get_address()
+{
+   int parsed_port = 0;
+   parse_config();
+   if (config_file_ != nullptr){
+      std::string key(kportKey);
+      auto it = config_file_.find(key);
+      if (it != config_file_.end()) {
+         parsed_port = config_file_[key].get<int>();
+      }
+   }
+   
+   if (parsed_port < 0 || parsed_port > 65535) {
+      throw InvalidPortException();
+   }
+   return klocalhostAddressPrefix + std::to_string(parsed_port);
+}
+
+ServerConfiguration::ConfigFileNotFoundException::ConfigFileNotFoundException()
+  : std::runtime_error(std::string(kConfigFileNotFoundMessage))
+{
+}
+
+ServerConfiguration::InvalidPortException::InvalidPortException()
+  : std::runtime_error(std::string(kinvalidPortMessage))
+{
+}
+}  // namespace internal
+}  // namespace grpc
+}  // namespace hardware
+}  // namespace ni
