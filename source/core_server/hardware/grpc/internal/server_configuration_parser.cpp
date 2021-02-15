142f9cbe877b247d13f01cfb7010db4483fdec4c
Kyle Sullivan <kyle.sullivan@ni.com>
2021-02-15 11:18:21 -0600

    Implement feedback on parser

--- source/core_server/hardware/grpc/internal/server_configuration_parser.cpp
+++ source/core_server/hardware/grpc/internal/server_configuration_parser.cpp
@@ -1,34 +1,34 @@
 #include "server_configuration_parser.h"
-#include <iostream>
+
 #include <climits>
+#include <iostream>
 
 namespace ni {
 namespace hardware {
 namespace grpc {
 namespace internal {
-  
-const char* ServerConfigurationParser::kLocalhostAddressPrefix = "0.0.0.0:";
-const char* ServerConfigurationParser::kDefaultFilename = "server_config.json";
-const char* ServerConfigurationParser::kPortKey = "port";
-const char* ServerConfigurationParser::kConfigFileNotFoundMessage = "The server configuration file was not found at the provided or default locations.";
-const char* ServerConfigurationParser::kInvalidPortMessage = "The specified port number must between 0 and 65535.";
-const char* ServerConfigurationParser::kMalformedJsonMessage = "The JSON in the server configuration file is malformed: \n\n";
-const char* ServerConfigurationParser::kWrongPortTypeMessage = "The server port must be specified in the server's configuration file as an integer: \n\n";
-const char* ServerConfigurationParser::kUnspecifiedPortMessage = "The server port must be specified in the server's configuration file.";
+
+static const char* kDefaultAddressPrefix = "0.0.0.0:";
+static const char* kDefaultFilename = "server_config.json";
+static const char* kPortKey = "port";
+static const char* kConfigFileNotFoundMessage = "The server configuration file was not found at the provided or default locations.";
+static const char* kInvalidPortMessage = "The specified port number must between 0 and 65535.";
+static const char* kMalformedJsonMessage = "The JSON in the server configuration file is malformed: \n\n";
+static const char* kWrongPortTypeMessage = "The server port must be specified in the server's configuration file as an integer: \n\n";
+static const char* kUnspecifiedPortMessage = "The server port must be specified in the server's configuration file.";
 
 ServerConfigurationParser::ServerConfigurationParser()
+    : config_file_(load(kDefaultFilename))
 {
-  config_file_ = ServerConfigurationParser::load("");
 }
 
 ServerConfigurationParser::ServerConfigurationParser(const char* config_file_path)
-  : config_file_path_(config_file_path)
+    : config_file_path_(config_file_path), config_file_(load(config_file_path))
 {
-  config_file_ = ServerConfigurationParser::load(config_file_path);
 }
 
 ServerConfigurationParser::ServerConfigurationParser(const nlohmann::json& config_file)
-  : config_file_(config_file)
+    : config_file_(config_file)
 {
 }
 
@@ -36,15 +36,7 @@ nlohmann::json ServerConfigurationParser::load(const char* config_file_path)
 {
   // TODO: Prefer a passed in configuration file path and then search next to
   // the binary and finally at platform specific default config file locations.
-  std::string file_path;
-  if (config_file_path == "") {
-    file_path = kDefaultFilename;
-  }
-  else {
-    file_path = config_file_path;
-  }
-  
-  std::ifstream input_stream(file_path);
+  std::ifstream input_stream(config_file_path);
   if (!input_stream) {
     throw ConfigFileNotFoundException();
   }
@@ -64,45 +56,45 @@ std::string ServerConfigurationParser::parse_address()
 
   auto it = config_file_.find(key);
   if (it != config_file_.end()) {
-      try {
-        parsed_port = it->get<int>();
-      }
-      catch (const nlohmann::json::type_error& ex) {
-        throw WrongPortTypeException(ex.what());
-      }
+    try {
+      parsed_port = it->get<int>();
+    }
+    catch (const nlohmann::json::type_error& ex) {
+      throw WrongPortTypeException(ex.what());
+    }
   }
   else {
     throw UnspecifiedPortException();
   }
-   
+
   if (parsed_port < 0 || parsed_port > USHRT_MAX) {
     throw InvalidPortException();
   }
-  return kLocalhostAddressPrefix + std::to_string(parsed_port);
+  return kDefaultAddressPrefix + std::to_string(parsed_port);
 }
 
 ServerConfigurationParser::ConfigFileNotFoundException::ConfigFileNotFoundException()
-  : std::runtime_error(kConfigFileNotFoundMessage)
+    : std::runtime_error(kConfigFileNotFoundMessage)
 {
 }
 
 ServerConfigurationParser::InvalidPortException::InvalidPortException()
-  : std::runtime_error(kInvalidPortMessage)
+    : std::runtime_error(kInvalidPortMessage)
 {
 }
 
 ServerConfigurationParser::MalformedJsonException::MalformedJsonException(const std::string& parse_error_details)
-  : std::runtime_error(kMalformedJsonMessage + parse_error_details)
+    : std::runtime_error(kMalformedJsonMessage + parse_error_details)
 {
 }
 
 ServerConfigurationParser::WrongPortTypeException::WrongPortTypeException(const std::string& type_error_details)
-  : std::runtime_error(kWrongPortTypeMessage + type_error_details)
+    : std::runtime_error(kWrongPortTypeMessage + type_error_details)
 {
 }
 
 ServerConfigurationParser::UnspecifiedPortException::UnspecifiedPortException()
-  : std::runtime_error(kUnspecifiedPortMessage)
+    : std::runtime_error(kUnspecifiedPortMessage)
 {
 }
 
--- source/core_server/hardware/grpc/internal/server_configuration_parser.h
+++ source/core_server/hardware/grpc/internal/server_configuration_parser.h
@@ -1,8 +1,9 @@
 #ifndef NI_HARDWARE_GRPC_INTERNAL_SERVERCONFIGURATION
 #define NI_HARDWARE_GRPC_INTERNAL_SERVERCONFIGURATION
 
-#include "nlohmann/json.hpp"
 #include <fstream>
+
+#include "nlohmann/json.hpp"
 
 namespace ni {
 namespace hardware {
@@ -10,53 +11,38 @@ namespace grpc {
 namespace internal {
 
 class ServerConfigurationParser {
-
  public:
   ServerConfigurationParser();
   ServerConfigurationParser(const char* config_file_path);
   ServerConfigurationParser(const nlohmann::json& config_file);
 
   std::string parse_address();
-  
-  struct ConfigFileNotFoundException : public std::runtime_error
-  {
+
+  struct ConfigFileNotFoundException : public std::runtime_error {
     ConfigFileNotFoundException();
   };
-  
-  struct InvalidPortException : public std::runtime_error
-  {
+
+  struct InvalidPortException : public std::runtime_error {
     InvalidPortException();
   };
 
-  struct MalformedJsonException : public std::runtime_error
-  {
+  struct MalformedJsonException : public std::runtime_error {
     MalformedJsonException(const std::string& parse_error_details);
   };
 
-  struct WrongPortTypeException : public std::runtime_error
-  {
+  struct WrongPortTypeException : public std::runtime_error {
     WrongPortTypeException(const std::string& type_error_details);
   };
 
-  struct UnspecifiedPortException : public std::runtime_error
-  {
+  struct UnspecifiedPortException : public std::runtime_error {
     UnspecifiedPortException();
   };
 
  private:
-  static const char* kLocalhostAddressPrefix;
-  static const char* kDefaultFilename;
-  static const char* kPortKey;
-  static const char* kConfigFileNotFoundMessage;
-  static const char* kInvalidPortMessage;
-  static const char* kMalformedJsonMessage;
-  static const char* kWrongPortTypeMessage;
-  static const char* kUnspecifiedPortMessage;
-  
+  static nlohmann::json load(const char* config_file_path);
+
   nlohmann::json config_file_;
   std::string config_file_path_;
-
-  static nlohmann::json load(const char* config_file_path);
 };
 
 }  // namespace internal
