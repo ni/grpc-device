4dc337689cb495e07e4aa5422e96e6bf885d770f
kruthi-NI <77618357+kruthi-NI@users.noreply.github.com>
2021-02-19 12:00:12 +0530

    [Owners] Implementing manual loading of syscfg library for Device Enumeration (#37)
    * Initial implementation of DeviceEnumeration using manual loading of syscfg dll at runtime
    
    * Rmeoving space before *
    
    * Including syscfg header files
    
    * Changes to cmake lists to not include header location of syscfg
    
    * Using contructor injection
    
    * Fixing indentation
    
    * Added intial unit tests for library not found
    
    * Removed check for network devices
    
    * Update comments
    
    * Removing default constructor for shared library
    
    * Filtering remote devices
    
    * Self review
    
    * Review comments
    
    * Adding default constructor for shared library
    
    * Shared library tests
    
    * Review comments for docs
    
    * draft pr
    
    * Resolving read access violation error
    
    * Review changes
    
    * Moving syscfg files to imports location
    
    * Review comments
    
    * Changing NiDevicePropertes to DeviceProperties
    
    * Adding extra lines
    
    * Review changes requested by Jonathan
    
    * changing device managemnet to device enumerator
    
    * Changing device managemnt to enumerator
    
    * Remove braces in proto file

---
+++ source/core_server/hardware/grpc/internal/device_enumerator.h
@@ -0,0 +1,36 @@
+#ifndef NI_HARDWARE_GRPC_INTERNAL_DEVICEMANAGEMENT
+#define NI_HARDWARE_GRPC_INTERNAL_DEVICEMANAGEMENT
+
+#include <grpcpp/grpcpp.h>
+#include <nisyscfg.h>
+
+#include "session_repository.h"
+#include "shared_library.h"
+
+namespace ni {
+namespace hardware {
+namespace grpc {
+namespace internal {
+  
+class DeviceEnumerator
+{
+ public:
+  DeviceEnumerator();
+  DeviceEnumerator(const char* library_name);
+
+  ::grpc::Status enumerate_devices(google::protobuf::RepeatedPtrField<DeviceProperties>* devices);
+  std::string get_syscfg_library_name() const;
+  bool is_syscfg_library_loaded() const;
+
+ private:
+  NISysCfgStatus get_list_of_devices(google::protobuf::RepeatedPtrField<DeviceProperties>* devices);
+
+  internal::SharedLibrary syscfg_library_;
+};
+
+} // namespace internal
+} // namespace grpc
+} // namespace hardware
+} // namespace ni
+
+#endif // NI_HARDWARE_GRPC_INTERNAL_DEVICEMANAGEMENT
