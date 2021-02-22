39b686c3c234be625c9e92412680374238d1fe32
Palash Khare <palash.khare@ni.com>
2021-02-22 10:41:20 +0530

    Changes made according to variable name convention and added more comments  to various sections of the code

--- examples/device enumerator/EnumerateDevice.py
+++ examples/device enumerator/EnumerateDevice.py
@@ -1,10 +1,14 @@
 #
-# Example communication with niFgen over gRPC
+# This example lists out devices/chassis in a Hierarchial Manner 
+# by estabilishing communication with the server over gRPC
 #
 # Copyright 2020 National Instruments
 # Licensed under the MIT license
 #
 # Getting Started:
+#
+# To run this example, users need to have the "NI System Configuration API" installed on their machine.
+# Link : https://www.ni.com/en-in/support/downloads/drivers/download.system-configuration.html
 #
 # Install the gRPC tools for Python
 #     > pip install grpcio-tools
@@ -12,92 +16,97 @@
 #     > conda install grpcio-tools
 #
 # Generate the python API from the gRPC definition (.proto) files
-#   > py -m grpc_tools.protoc -I../../source --python_out=. --grpc_python_out=. server_utilities.proto
-#
-# Update the server address 
-#
+#   > python -m grpc_tools.protoc -I../../source --python_out=. --grpc_python_out=. server_utilities.proto
 #
 
 import grpc
-import server_utilities_pb2 as server_utilities_type
-import server_utilities_pb2_grpc as grpc_server_utilities
+import server_utilities_pb2 as serverType
+import server_utilities_pb2_grpc as grpcServer
 
 from collections import defaultdict
 
-# This is the location (ipaddress or machine name):(port) of the server
+# Create the communcation channel for the remote host (in this case we are connecting to a local server)
 serverAddress = "localhost:50051"
-
 channel = grpc.insecure_channel(serverAddress)
-server_utilities_stub = grpc_server_utilities.ServerUtilitiesStub(channel)
+
+# Create communication with the server using the created communication channel
+server = grpcServer.ServerUtilitiesStub(channel)
+print("Connecting: ")
+
+# Give error message if connection is not estabilished
 
 ###################################################################################################
 
-# Enumerate Device API
-DeviceEnumerator = server_utilities_stub.EnumerateDevices(server_utilities_type.EnumerateDevicesRequest(
-    ))
+# Enumerate the devices connected to the server machine.
+# This API gives a list of devices/chassis connected to the server machine.
+enumerateDeviceResponse = server.EnumerateDevices(serverType.EnumerateDevicesRequest())
 
 # For storing properties of the device/chassis
 class Properties :
-    def __init__(self, name , model , vendor , serial_number, parent , slot_number) :
+    def __init__(self, name , model , vendor , serialNumber, parent , slotNumber) :
         self.name = name
         self.model = model
         self.vendor = vendor
-        self.serial_number = serial_number
+        self.serialNumber = serialNumber
         self.parent = parent
-        self.slot_number = slot_number
+        self.slotNumber = slotNumber
 
-# Since every device/chassis will have a unique name 
-# so properties of that device/chassis can be mapped to its name in a dictionary
-name_to_properties = defaultdict()
+# Properties of a perticular device/chassis can be mapped to its name 
+# because every device/chassis has a unique name
+nameToProperties = defaultdict()
 
-def comparator( name ):
-    name_property = name_to_properties[name]
-    slot_number = int(name_property.slot_number)
-    return slot_number
+# This will be used for custom sorting the devices based on the Slot Number
+def comparator(name):
+    deviceProperty = nameToProperties[name]
+    slotNumber = int(deviceProperty.slotNumber)
+    return slotNumber
 
 class Tree :
-
     def __init__(self) :
         self.tree = defaultdict(list)
 
     def addEdge(self , parent , child) :
         self.tree[parent].append(child)
 
-    def printHW(self , name , level) :
+    def printHardwareProperties(self , name , level) :
         space = ""
         for i in range(level) :
             space += "   "
-
         if (level != 0) :
-            device_property = name_to_properties[name]
+            deviceProperty = nameToProperties[name]
             if (level > 1) :
-                print(space + "Slot " + device_property.slot_number + ": " + name )
+                print(space + "Slot " + deviceProperty.slotNumber + ": " + name)
             else :
                 if (level == 1) :
                     print(space + name)
-
-            print(space + "   " + "Model: " + device_property.model)
-            print(space + "   " + "Vendor: " + device_property.vendor )
-            print(space + "   " + "Serial Number: " + device_property.serial_number + "\n" )
+            print(space + "   " + "Model: " + deviceProperty.model)
+            print(space + "   " + "Vendor: " + deviceProperty.vendor)
+            print(space + "   " + "Serial Number: " + deviceProperty.serialNumber + "\n")
 
         if name in self.tree :
             children = self.tree[name]
             children = sorted(children , key=comparator , reverse=False)
             for child in children :
-                self.printHW( child , level+1 )
-        
+                self.printHardwareProperties(child , level+1)      
         return
 
-def list_of_devices() :
-
-    print("Connecting: ")
-    print("Devices: " + "\n")
-
+def listOfDevices() :
+    print("Devices: " + "\n") 
     tree = Tree()
 
     # Creating the tree for the Hierarchy
-    for it in DeviceEnumerator.devices :
-        device_property = Properties(
+    # "My System" would be the root node and all the devices/chassis would be connected to it.
+    # for example                 
+    #                                    "My System"
+    #                                  /      |       \
+    #                        "Chassis1"  "Chassis2"  "Device1"
+    #                     /        \         |
+    #            "Block Carrier1"  "device2"  "device3"
+    #                   |
+    #                 "device4"
+
+    for it in enumerateDeviceResponse.devices :
+        deviceProperty = Properties(
             it.name,
             it.model,
             it.vendor,
@@ -105,16 +114,16 @@ def list_of_devices() :
             it.parent,
             it.slot_number
         )
-        name_to_properties[it.name] = device_property
-        
+        nameToProperties[it.name] = deviceProperty        
         if (len(it.parent) == 0) :
             tree.addEdge("My System" , it.name)
         else :
             tree.addEdge(it.parent , it.name)
     
-    # Function to list devices in a Hierarchial Manner
-    tree.printHW("My System" , 0)
+    # Function to list devices along with the properties in a Hierarchial Manner
+    # from the root node
+    tree.printHardwareProperties("My System" , 0)
 
-# Calling List Device function
-list_of_devices()    
+# Calling listOfDevices() function
+listOfDevices()    
 ###############################################################################################################
