f6f6bdadd139c12f2222a7ee1644ac31f1e1d3f7
Jonathan Lugo <jonathan.lugo@ni.com>
2021-02-22 16:16:17 -0600

    Reorder lines in files to make merge easier.

--- source/codegen/generate_service.py
+++ source/codegen/generate_service.py
@@ -34,8 +34,7 @@ def generate_all(metadata_dir, gen_dir, gen_mock):
   generate_service_file(metadata, "library_interface.h.mako", "_library_interface.h", gen_dir)
   generate_service_file(metadata, "library.cpp.mako", "_library.cpp", gen_dir)
   generate_service_file(metadata, "library.h.mako", "_library.h", gen_dir)
-  if gen_mock:
-      generate_service_file(metadata, "library_mock.h.mako", "_mock_library.h", gen_dir)
+  generate_service_file(metadata, "mock_library.h.mako", "_mock_library.h", gen_dir)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description = "Generate files for specified NI driver API gRPC service.")
