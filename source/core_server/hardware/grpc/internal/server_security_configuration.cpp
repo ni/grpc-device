75f02dd0dcbbde893dcb9c0993d24d7c5098712f
Kyle Sullivan <kyle.sullivan@ni.com>
2021-02-22 22:38:11 -0600

    Fix clang-format for ternary operators

--- source/core_server/hardware/grpc/internal/server_security_configuration.cpp
+++ source/core_server/hardware/grpc/internal/server_security_configuration.cpp
@@ -13,18 +13,18 @@ ServerSecurityConfiguration::ServerSecurityConfiguration()
 }
 
 ServerSecurityConfiguration::ServerSecurityConfiguration(const std::string& server_cert, const std::string& server_key, const std::string& root_cert)
-  : is_insecure_credentials_(server_cert.empty() || server_key.empty())
+    : is_insecure_credentials_(server_cert.empty() || server_key.empty())
 {
   if (is_insecure_credentials_) {
     server_credentials_ = ::grpc::InsecureServerCredentials();
     return;
   }
 
-  ::grpc::SslServerCredentialsOptions::PemKeyCertPair key_cert_pair  = { server_key, server_cert };
+  ::grpc::SslServerCredentialsOptions::PemKeyCertPair key_cert_pair = {server_key, server_cert};
   credentials_options_.pem_key_cert_pairs.push_back(key_cert_pair);
   credentials_options_.client_certificate_request = root_cert.empty()
-                                                        ? GRPC_SSL_DONT_REQUEST_CLIENT_CERTIFICATE
-                                                        : GRPC_SSL_REQUEST_AND_REQUIRE_CLIENT_CERTIFICATE_AND_VERIFY;
+      ? GRPC_SSL_DONT_REQUEST_CLIENT_CERTIFICATE
+      : GRPC_SSL_REQUEST_AND_REQUIRE_CLIENT_CERTIFICATE_AND_VERIFY;
   credentials_options_.pem_root_certs = root_cert;
   server_credentials_ = ::grpc::SslServerCredentials(credentials_options_);
 }
