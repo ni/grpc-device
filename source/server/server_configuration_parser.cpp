0d3ca601f0515446b9046d161cbdd47eec0b5d96
kylesull30 <kyle.sullivan@ni.com>
2021-02-26 10:43:20 -0500

    [Owners] Implement command line parsing of configuration file path (#73)

--- source/server/server_configuration_parser.cpp
+++ source/server/server_configuration_parser.cpp
@@ -33,7 +33,7 @@ ServerConfigurationParser::ServerConfigurationParser()
 {
 }
 
-ServerConfigurationParser::ServerConfigurationParser(const char* config_file_path)
+ServerConfigurationParser::ServerConfigurationParser(const std::string& config_file_path)
     : config_file_path_(config_file_path), config_file_(load(config_file_path))
 {
 }
@@ -61,12 +61,10 @@ std::string ServerConfigurationParser::get_exe_path()
 
 nlohmann::json ServerConfigurationParser::load(const std::string& config_file_path)
 {
-  // TODO: Prefer a passed in configuration file path and then search next to
-  // the binary and finally at platform specific default config file locations.
   std::ifstream input_stream(config_file_path);
 
   if (!input_stream) {
-    throw ConfigFileNotFoundException();
+    throw ConfigFileNotFoundException(config_file_path);
   }
 
   try {
@@ -151,8 +149,8 @@ std::string ServerConfigurationParser::read_keycert(const std::string& filename)
   return data;
 }
 
-ServerConfigurationParser::ConfigFileNotFoundException::ConfigFileNotFoundException()
-    : std::runtime_error(kConfigFileNotFoundMessage)
+ServerConfigurationParser::ConfigFileNotFoundException::ConfigFileNotFoundException(const std::string& config_file_path)
+    : std::runtime_error(kConfigFileNotFoundMessage + config_file_path)
 {
 }
 
--- source/server/server_configuration_parser.h
+++ source/server/server_configuration_parser.h
@@ -9,7 +9,7 @@ namespace hardware {
 namespace grpc {
 namespace internal {
 
-static const char* kConfigFileNotFoundMessage = "The server configuration file was not found at the provided or default locations.";
+static const char* kConfigFileNotFoundMessage = "The server configuration file was not found at: ";
 static const char* kInvalidPortMessage = "The specified port number must between 0 and 65535.";
 static const char* kMalformedJsonMessage = "The JSON in the server configuration file is malformed: \n\n";
 static const char* kWrongPortTypeMessage = "The server port must be specified in the server's configuration file as an integer: \n\n";
@@ -22,7 +22,7 @@ static const char* kDefaultAddressPrefix = "[::]:";
 class ServerConfigurationParser {
  public:
   ServerConfigurationParser();
-  ServerConfigurationParser(const char* config_file_path);
+  ServerConfigurationParser(const std::string& config_file_path);
   ServerConfigurationParser(const nlohmann::json& config_file);
   virtual ~ServerConfigurationParser() {}
 
@@ -32,7 +32,7 @@ class ServerConfigurationParser {
   std::string parse_root_cert() const;
 
   struct ConfigFileNotFoundException : public std::runtime_error {
-    ConfigFileNotFoundException();
+    ConfigFileNotFoundException(const std::string& config_file_path);
   };
 
   struct InvalidPortException : public std::runtime_error {
