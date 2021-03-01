86e35c459e7ba9f209c7d6336286d2d63a61eee2
Ryan Eckenrode <ryan.eckenrode@ni.com>
2021-03-01 10:16:17 -0600

    Add custom service handler file per driver.

---
+++ source/custom/nifake_service.custom.cpp
@@ -0,0 +1,16 @@
+#include <atomic>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include "nifake_service.h"
+
+namespace ni {
+namespace fake {
+namespace grpc {
+
+namespace internal = ni::hardware::grpc::internal;
+
+}  // namespace grpc
+}  // namespace fake
+}  // namespace ni
---
+++ source/custom/niswitch_service.custom.cpp
@@ -0,0 +1,16 @@
+#include <atomic>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include "niswitch_service.h"
+
+namespace ni {
+namespace niswitch {
+namespace grpc {
+
+namespace internal = ni::hardware::grpc::internal;
+
+}  // namespace grpc
+}  // namespace niswitch
+}  // namespace ni
