f2413d2e5dde4ce1fc60a6bee34b207114e7e14f
Ryan Eckenrode <ryan.eckenrode@ni.com>
2021-03-01 17:13:42 -0600

    Add custom handlers for ni-fake python-code methods.

--- source/custom/nifake_service.custom.cpp
+++ source/custom/nifake_service.custom.cpp
@@ -21,7 +21,7 @@ namespace internal = ni::hardware::grpc::internal;
     ViInt32 a_number = request->a_number();
 
     std::string a_string(a_number, '\0');
-    auto status = library_->GetAStringUsingPythonCode(vi, a_number, (ViChar*)a_string.data());
+    ViStatus status = library_->GetAStringUsingPythonCode(vi, a_number, (ViChar*)a_string.data());
     response->set_status(status);
     if (status == 0) {
       response->set_a_string(a_string);
@@ -38,7 +38,21 @@ namespace internal = ni::hardware::grpc::internal;
 ::grpc::Status NiFakeService::GetArrayForPythonCodeDouble(::grpc::ServerContext* context, const GetArrayForPythonCodeDoubleRequest* request, GetArrayForPythonCodeDoubleResponse* response)
 {
   try {
-    return ::grpc::Status(::grpc::UNIMPLEMENTED, "TODO: This server handler has not been implemented.");
+    auto session = request->vi();
+    ViSession vi = session_repository_->access_session(session.id(), session.name());
+
+    ViInt32 number_of_elements;
+    ViStatus status = library_->GetArraySizeForPythonCode(vi, &number_of_elements);
+    if (status != 0) {
+      response->set_status(status);
+      return ::grpc::Status::OK;
+    }
+
+    response->mutable_array_out()->Reserve(number_of_elements);
+    ViReal64* array_out = response->mutable_array_out()->AddNAlreadyReserved(number_of_elements);
+    status = library_->GetArrayForPythonCodeDouble(vi, number_of_elements, array_out);
+    response->set_status(status);
+    return ::grpc::Status::OK;
   }
   catch (internal::LibraryLoadException& ex) {
     return ::grpc::Status(::grpc::NOT_FOUND, ex.what());
@@ -50,7 +64,24 @@ namespace internal = ni::hardware::grpc::internal;
 ::grpc::Status NiFakeService::GetArrayForPythonCodeCustomType(::grpc::ServerContext* context, const GetArrayForPythonCodeCustomTypeRequest* request, GetArrayForPythonCodeCustomTypeResponse* response)
 {
   try {
-    return ::grpc::Status(::grpc::UNIMPLEMENTED, "TODO: This server handler has not been implemented.");
+    auto session = request->vi();
+    ViSession vi = session_repository_->access_session(session.id(), session.name());
+
+    ViInt32 number_of_elements;
+    ViStatus status = library_->GetArraySizeForPythonCode(vi, &number_of_elements);
+    if (status != 0) {
+      response->set_status(status);
+      return ::grpc::Status::OK;
+    }
+
+    std::vector<CustomStruct> array_out(number_of_elements);
+    status = library_->GetArrayForPythonCodeCustomType(vi, number_of_elements, array_out.data());
+    response->set_status(status);
+    if (status == 0) {
+      // TODO: Figure out how to link to Copy defined in nifake_service.cpp. Possibly add to header file for NiFakeService as private function.
+      // Copy(array_out, response->mutable_array_out());
+    }
+    return ::grpc::Status::OK;
   }
   catch (internal::LibraryLoadException& ex) {
     return ::grpc::Status(::grpc::NOT_FOUND, ex.what());
