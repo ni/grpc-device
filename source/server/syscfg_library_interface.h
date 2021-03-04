954907c016dc10b8055d43aeffd5478c37eec5f3
Kruthi M <kruthi.m@ni.com>
2021-03-04 19:31:27 +0530

    Minor

--- source/server/syscfg_library_interface.h
+++ source/server/syscfg_library_interface.h
@@ -34,24 +34,24 @@ class SysCfgLibraryInterface {
     NISysCfgSessionHandle                  session_handle,
     NISysCfgFilterMode                     filter_mode,
     NISysCfgFilterHandle                   filter_handle,
-    const char *                           expert_names,
-    NISysCfgEnumResourceHandle *           resource_enum_handle
+    const char*                            expert_names,
+    NISysCfgEnumResourceHandle*            resource_enum_handle
     ) = 0;
   virtual NISysCfgStatus NextResource(
     NISysCfgSessionHandle                  session_handle,
     NISysCfgEnumResourceHandle             resource_enum_handle,
-    NISysCfgResourceHandle *               resource_handle
+    NISysCfgResourceHandle*                resource_handle
     ) = 0;
   virtual NISysCfgStatus GetResourceIndexedProperty(
     NISysCfgResourceHandle                 resource_handle,
     NISysCfgIndexedProperty                property_ID,
     unsigned int                           index,
-    void *                                 value
+    void*                                  value
     ) = 0;
   virtual NISysCfgStatus GetResourceProperty(
     NISysCfgResourceHandle                 resource_handle,
     NISysCfgResourceProperty               property_ID,
-    void *                                 value
+    void*                                  value
     ) = 0;
 };
 
