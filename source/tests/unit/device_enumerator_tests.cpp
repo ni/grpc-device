792d61ee22f9d51e4fef27dfe4c183ae725c9cd5
Kruthi M <kruthi.m@ni.com>
2021-03-05 20:28:55 +0530

    adding 3 important corner test cases

--- source/tests/unit/device_enumerator_tests.cpp
+++ source/tests/unit/device_enumerator_tests.cpp
@@ -97,6 +97,71 @@ TEST(DeviceEnumeratorTests, InitializeSessionSetsSessionHandle_EnumerateDevices_
   EXPECT_EQ(::grpc::StatusCode::OK, status.error_code());
 }
 
+TEST(DeviceEnumerationTests, SysCfgApiInstalledAndNoDevicesPresent_EnumerateDevices_ListOfDevicesIsEmpty)
+{
+  NiceMock<ni::tests::utilities::SysCfgMockLibrary> mock_library;
+  internal::DeviceEnumerator device_enumerator(&mock_library);
+  google::protobuf::RepeatedPtrField<ni::hardware::grpc::DeviceProperties> devices;
+  EXPECT_CALL(mock_library, NextResource)
+      .WillOnce(Return(NISysCfg_EndOfEnum));
+
+  ::grpc::Status status = device_enumerator.enumerate_devices(&devices);
+
+  EXPECT_EQ(::grpc::StatusCode::OK, status.error_code());
+  EXPECT_EQ(0, devices.size());
+}
+
+NISysCfgStatus SetExpertNameToNetwork(void* value)
+{
+  char* expert_name = (char*)value;
+  strcpy(expert_name, internal::kNetworkExpertName);
+  return NISysCfg_OK; 
+}
+
+TEST(DeviceEnumerationTests, SysCfgApiInstalled_EnumerateDevices_ListOfDevicesDoesNotContainRemoteDevices)
+{
+  NiceMock<ni::tests::utilities::SysCfgMockLibrary> mock_library;
+  internal::DeviceEnumerator device_enumerator(&mock_library);
+  google::protobuf::RepeatedPtrField<ni::hardware::grpc::DeviceProperties> devices;
+  EXPECT_CALL(mock_library, NextResource)
+      .WillOnce(Return(NISysCfg_OK))
+      .WillOnce(Return(NISysCfg_EndOfEnum));
+  EXPECT_CALL(mock_library, GetResourceIndexedProperty)
+      .WillOnce(WithArg<3>(Invoke(SetExpertNameToNetwork)));
+  EXPECT_CALL(mock_library, GetResourceIndexedProperty(_, NISysCfgIndexedPropertyExpertUserAlias, _, _))
+      .Times(0);
+
+  ::grpc::Status status = device_enumerator.enumerate_devices(&devices);
+
+  EXPECT_EQ(::grpc::StatusCode::OK, status.error_code());
+  EXPECT_EQ(0, devices.size());
+}
+
+TEST(DeviceEnumerationTests, GetResourcePropertyApisReturnError_EnumerateDevices_DevicePropertiesAreSetToEmptyString)
+{
+  NiceMock<ni::tests::utilities::SysCfgMockLibrary> mock_library;
+  internal::DeviceEnumerator device_enumerator(&mock_library);
+  google::protobuf::RepeatedPtrField<ni::hardware::grpc::DeviceProperties> devices;
+  EXPECT_CALL(mock_library, NextResource)
+      .WillOnce(Return(NISysCfg_OK))
+      .WillOnce(Return(NISysCfg_EndOfEnum));
+  EXPECT_CALL(mock_library, GetResourceIndexedProperty)
+      .WillRepeatedly(Return(NISysCfg_PropDoesNotExist));
+  EXPECT_CALL(mock_library, GetResourceProperty)
+      .WillRepeatedly(Return(NISysCfg_PropDoesNotExist));
+
+  ::grpc::Status status = device_enumerator.enumerate_devices(&devices);
+
+  EXPECT_EQ(::grpc::StatusCode::OK, status.error_code());
+  EXPECT_EQ(1, devices.size());
+  for (auto it : devices) {
+    EXPECT_EQ("", it.name());
+    EXPECT_EQ("", it.model());
+    EXPECT_EQ("", it.vendor());
+    EXPECT_EQ("", it.serial_number());
+  }
+}
+
 }  // namespace unit
 }  // namespace tests
 }  // namespace ni
