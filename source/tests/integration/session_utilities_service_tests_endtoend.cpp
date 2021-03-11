55b2d033aa1222d3933a537941aa30e498ce979a
kruthi-NI <77618357+kruthi-NI@users.noreply.github.com>
2021-03-12 03:07:35 +0530

    [Owners] [Tiny] [Refactoring] Renaming session_utilities_service_tests to session_utilities_service_tests_endtoend in integration folder (#114)
    * Renaming device management to device enumerator
    
    * implementing wrapper for device enumeration
    
    * self-review
    
    * improving diff
    
    * avoiding conflicts
    
    * Minor
    
    * review suggestions by Jonathan
    
    * Updating syscfg lirbary to throw exception
    
    * Minor
    
    * Changing FindHardware to InitializeSession
    
    * Modifying error message
    
    * Making error string as static const char*
    
    * Clean up
    
    * Removing extra whitespace
    
    * Renaming integration test to in_process_server_client_tests
    
    * Review comments to make sessoin_utilites_service_tests as integration tests
    
    * Removing extra whitespace
    
    * Reverting chnages to making it integration test. Also asserting error message
    
    * Removed the test for libary loaded case of production library
    
    * [Owners] Implementing Initialize and Close session using delay loading of nisysfg.dll (#80)
    
    * Linking lib and added intiailize and close
    
    * Remove check on session being NULL
    
    * Delay load on windows
    
    * Adding function pointers
    
    * Minor
    
    * Minor
    
    * Adding device_enumerator_tests
    
    * Minor
    
    * Added syscfg_library_tests
    
    * minor
    
    * Minor
    
    * use streq
    
    * Clean up
    
    * Using action
    
    * Fix naming
    
    * Fix naming
    
    * Syscfg library tests
    
    * Reveiw comments
    
    * Not linking with nisyscfg.lib
    
    * Making unit tests use mock library
    
    * Mkaing integration tests use mock library
    
    * Making parameters little closer
    
    * Removing extra space
    
    * Minor
    
    * Minor
    
    * Comments
    
    * Removing space
    
    * Minor
    
    * Modification suggested by dan
    
    * MInor renaming
    
    * Owners comments to use nicemock and modify constant name
    
    * Using mock lirbary in integration tests of niscope
    
    * Minor
    
    * Removing syscfg_library.cpp from integration tests
    
    * Updating niscope_driver_api_tests to use the new device enumerator constructor
    
    * Comments by jonathan
    
    * renaming test file name
    
    * Updating correct namespace
    
    * Running clang-format
    
    * Running clang format
    
    * new name suggested by Jonathan

--- source/tests/integration/session_utilities_service_tests_endtoend.cpp
+++ source/tests/integration/session_utilities_service_tests_endtoend.cpp
@@ -13,9 +13,9 @@ namespace integration {
 using ::testing::NiceMock;
 using ::testing::Throw;
 
-class InProcessServerClientTest : public ::testing::Test {
+class SessionUtilitiesServiceTests_EndToEnd : public ::testing::Test {
  public:
-  virtual ~InProcessServerClientTest() {}
+  virtual ~SessionUtilitiesServiceTests_EndToEnd() {}
 
   void SetUp() override
   {
@@ -89,7 +89,7 @@ class InProcessServerClientTest : public ::testing::Test {
   }
 
  protected:
-  InProcessServerClientTest() {}
+  SessionUtilitiesServiceTests_EndToEnd() {}
 
  private:
   std::shared_ptr<::grpc::Channel> channel_;
@@ -101,7 +101,7 @@ class InProcessServerClientTest : public ::testing::Test {
   std::unique_ptr<::grpc::Server> server_;
 };
 
-TEST_F(InProcessServerClientTest, SessionUtilitiesServiceClient_RequestIsServerRunning_ResponseIsTrue)
+TEST_F(SessionUtilitiesServiceTests_EndToEnd, SessionUtilitiesServiceClient_RequestIsServerRunning_ResponseIsTrue)
 {
   grpc::nidevice::IsReservedByClientRequest request;
   grpc::nidevice::IsReservedByClientResponse response;
@@ -113,7 +113,7 @@ TEST_F(InProcessServerClientTest, SessionUtilitiesServiceClient_RequestIsServerR
   EXPECT_TRUE(s.ok());
 }
 
-TEST_F(InProcessServerClientTest, ClientTimesOutWaitingForReservation_FreeReservation_DoesNotReserve)
+TEST_F(SessionUtilitiesServiceTests_EndToEnd, ClientTimesOutWaitingForReservation_FreeReservation_DoesNotReserve)
 {
   auto status_a = call_reserve("foo", "a");
   auto status_b = call_reserve("foo", "b", std::chrono::system_clock::now() + std::chrono::milliseconds(5));
@@ -126,7 +126,7 @@ TEST_F(InProcessServerClientTest, ClientTimesOutWaitingForReservation_FreeReserv
   EXPECT_EQ(status_b.error_code(), ::grpc::DEADLINE_EXCEEDED);
 }
 
-TEST_F(InProcessServerClientTest, MultipleClientsTimeOutWaitingForReservation_FreeReservation_DoNotReserve)
+TEST_F(SessionUtilitiesServiceTests_EndToEnd, MultipleClientsTimeOutWaitingForReservation_FreeReservation_DoNotReserve)
 {
   call_reserve("foo", "a");
   call_reserve("foo", "b", std::chrono::system_clock::now() + std::chrono::milliseconds(5));
@@ -138,7 +138,7 @@ TEST_F(InProcessServerClientTest, MultipleClientsTimeOutWaitingForReservation_Fr
   EXPECT_FALSE(call_is_reserved("foo", "c"));
 }
 
-TEST_F(InProcessServerClientTest, ClientTimesOutWaitingForReservationWithOtherClientWaitingBehind_FreeReservation_ReservesLastClient)
+TEST_F(SessionUtilitiesServiceTests_EndToEnd, ClientTimesOutWaitingForReservationWithOtherClientWaitingBehind_FreeReservation_ReservesLastClient)
 {
   call_reserve("foo", "a");
   call_reserve("foo", "b", std::chrono::system_clock::now() + std::chrono::milliseconds(5));
@@ -158,7 +158,7 @@ TEST_F(InProcessServerClientTest, ClientTimesOutWaitingForReservationWithOtherCl
   EXPECT_TRUE(call_is_reserved("foo", "c"));
 }
 
-TEST_F(InProcessServerClientTest, SysCfgLibraryNotPresent_ClientCallsEnumerateDevices_ReturnsNotFoundGrpcStatusError)
+TEST_F(SessionUtilitiesServiceTests_EndToEnd, SysCfgLibraryNotPresent_ClientCallsEnumerateDevices_ReturnsNotFoundGrpcStatusError)
 {
   grpc::nidevice::EnumerateDevicesRequest request;
   grpc::nidevice::EnumerateDevicesResponse response;
