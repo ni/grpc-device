c55425b8c8671aba8f196a5bcc2988011026cfae
Palash Khare <palash.khare@ni.com>
2021-03-16 10:14:38 +0530

    added string interpolation , changed variable and function names to snake_case

--- examples/device_enumerator/EnumerateDevice.py
+++ examples/device_enumerator/EnumerateDevice.py
@@ -20,27 +20,27 @@ import session_pb2 as sessionType
 import session_pb2_grpc as gRPCSession
 
 # Helper to print the devices 
-def printDevices() :
-    print("Devices: " + "\n") 
-    for it in enumerateDevicesResponse.devices :
-        print(it.name + ":")
-        print("   " + "Model: " + it.model)
-        print("   " + "Vendor: " + it.vendor)
-        print("   " + "Serial Number: " + it.serial_number +"\n")
+def print_devices() :
+    print("Devices: \n") 
+    for device in enumerate_devices_response.devices :
+        print(f"{device.name}")
+        print(f"    Model: {device.model}")
+        print(f"    Vendor: {device.vendor}")
+        print(f"    Serial Number: {device.serial_number} \n")
 
 # Create communication with the server using gRPC APIs
-serverAddress = "localhost:31763"
-channel = grpc.insecure_channel(serverAddress)
+server_address = "localhost:31763"
+channel = grpc.insecure_channel(server_address)
 server = gRPCSession.SessionUtilitiesStub(channel)
 
 try :
     # EnumerateDevices API gives a list of devices (simulated and physical) connected to the server machine.
-    enumerateDevicesResponse = server.EnumerateDevices(sessionType.EnumerateDevicesRequest())
+    enumerate_devices_response = server.EnumerateDevices(sessionType.EnumerateDevicesRequest())
 
     # Display devices connected to the server machine
-    printDevices()     
+    print_devices()     
 
 # If EnumerateDevices API throws an exception, print the error message
 except grpc.RpcError as e:
-    errorMessage = e.details()
-    print(errorMessage)
+    error_message = e.details()
+    print(f"{error_message}")
