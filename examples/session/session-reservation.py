9abc5e9fc79f7a542cbfa5b67f92c61a1c972d40
Ryan Eckenrode <ryan.eckenrode@ni.com>
2021-03-18 12:30:17 -0500

    Address PR comments.

--- examples/session/session-reservation.py
+++ examples/session/session-reservation.py
@@ -1,5 +1,5 @@
 # 
-# Copyright 2020 National Instruments Corp
+# Copyright 2021 National Instruments Corp
 # Licensed under the MIT license
 #
 # This example initiates a session and uses the session API to exercise some common reservation operations on the session.
@@ -10,12 +10,12 @@
 # To run this example, install "NI-SCOPE Driver" on the server machine.
 # Link : https://www.ni.com/en-us/support/downloads/drivers/download.ni-scope.html
 #
-# Install the gRPC tools for Python
+# Install the gRPC tools for Python.
 #     > pip install grpcio-tools
 #   if you are using anaconda
 #     > conda install grpcio-tools
 #
-# Generate the python API from the gRPC definition (.proto) files
+# Generate the python API from the gRPC definition (.proto) files.
 # Note: The snippets below assume you are executing from the examples/session folder in the repo directory. 
 # If not, you will need to adjust the -I arguments so the compiler knows where to find the proto files.
 #   > py -m grpc_tools.protoc -I="../../source/protobuf" --python_out=. --grpc_python_out=. session.proto
@@ -39,7 +39,7 @@ def CheckForError (vi, status) :
         any_error = True
         ThrowOnError (vi, status)
 
-# Converts an error code returned by NI-SCOPE into a user-readable string
+# Converts an error code returned by NI-SCOPE into a user-readable string.
 def ThrowOnError (vi, error_code):
     error_message_request = niscope_types.GetErrorMessageRequest(
         vi = vi,
@@ -48,15 +48,14 @@ def ThrowOnError (vi, error_code):
     error_message_response = niscope_client.GetErrorMessage(error_message_request)
     raise Exception (error_message_response)
 
-# Server machine's IP address and port number have to be passed as two separate command line arguments.
-#   > python session-reservation.py localhost 31763
-# If not passed as command line arguments, then by default server address would be "localhost:31763"
+# Server machine's IP address and port number can be passed as two separate command line arguments.
+#   > python session-reservation.py <server_address> <port_number>
+# If they are not passed in as command line arguments, then by default the server address will be "localhost:31763".
 server_address = "localhost:31763"
 if len(sys.argv) == 3 :
     server_address = f"{sys.argv[1]}:{sys.argv[2]}"
 
-# Create the communication channel for the remote host (in this case we are connecting to a local server)
-# and create connections to the NI-SCOPE and session services
+# Create the communication channel for the remote host and create connections to the NI-SCOPE and session services.
 channel = grpc.insecure_channel(server_address)
 niscope_client = grpc_niscope.NiScopeStub(channel)
 session_client = grpc_session.SessionUtilitiesStub(channel)
@@ -72,8 +71,8 @@ try :
     reset_server_response = session_client.ResetServer(session_types.ResetServerRequest())
     assert(reset_server_response.is_server_reset)
 
-    # Open session to NI-SCOPE module with options
-    session_name = "Ni-Scope-Session-1"
+    # Open session to NI-SCOPE module with options.
+    session_name = "NI-Scope-Session-1"
     print('\nInitializing session...')
     init_with_options_response = niscope_client.InitWithOptions(niscope_types.InitWithOptionsRequest(
         session_name = session_name,
@@ -85,7 +84,7 @@ try :
     CheckForError(vi, init_with_options_response.status)
     print(f'Session initialized with name {session_name} and id {vi.id}.\n')
 
-    # Check if session is reserved by client 1
+    # Check if session is reserved by client 1.
     # Note: The reservation_id is defined by and has meaning only for the client + Session Reservation API.
     print(f'Checking if {session_name} is reserved by {client_1_id}...')
     is_reserved_response = session_client.IsReservedByClient(session_types.IsReservedByClientRequest(
@@ -95,7 +94,7 @@ try :
     assert(not is_reserved_response.is_reserved)
     print(f'{session_name} is not reserved by {client_1_id}.\n')
 
-    # Reserve session for client 1
+    # Reserve session for client 1.
     print(f'Reserving {session_name} for {client_1_id}...')
     reserve_response = session_client.Reserve(session_types.ReserveRequest(
         reservation_id = session_name,
@@ -110,7 +109,7 @@ try :
     assert(is_reserved_by_client1_response.is_reserved)
     print(f'{session_name} is reserved by {client_1_id}.\n')
 
-    # Check reservation on client 2
+    # Check reservation on client 2.
     print(f'Checking if {session_name} is reserved by {client_2_id}...')
     is_reserved_by_client2_response = session_client.IsReservedByClient(session_types.IsReservedByClientRequest(
         reservation_id = session_name,
@@ -119,7 +118,7 @@ try :
     assert(not is_reserved_by_client2_response.is_reserved)
     print(f'{session_name} is not reserved by {client_2_id}.\n')
 
-    # Unreserve on client 1
+    # Unreserve on client 1.
     print(f'Unreserving {session_name} reserved by {client_1_id}...')
     is_unreserved_response = session_client.Unreserve(session_types.UnreserveRequest(
         reservation_id = session_name,
@@ -133,13 +132,32 @@ try :
     assert(not is_reserved_by_client1_response.is_reserved)
     print(f'{session_name} is no longer reserved by {client_1_id}.\n')
 
-    # Reset server
+    print(f'Reserving {session_name} for {client_2_id}...')
+    reserve_response = session_client.Reserve(session_types.ReserveRequest(
+        reservation_id = session_name,
+        client_id = client_2_id
+        ))
+    is_reserved = reserve_response.is_reserved
+    assert(is_reserved)
+    is_reserved_by_client2_response = session_client.IsReservedByClient(session_types.IsReservedByClientRequest(
+        reservation_id = session_name,
+        client_id = client_2_id
+        ))
+    assert(is_reserved_by_client2_response.is_reserved)
+    print(f'{session_name} is reserved by {client_2_id}.\n')
+
+    # Reset server.
     print(f'Resetting the server...')
     reset_server_response = session_client.ResetServer(session_types.ResetServerRequest())
     assert(reset_server_response.is_server_reset)
-    print(f'Server has been reset.\n')
+    is_reserved_by_client2_response = session_client.IsReservedByClient(session_types.IsReservedByClientRequest(
+        reservation_id = session_name,
+        client_id = client_2_id
+        ))
+    assert(not is_reserved_by_client2_response.is_reserved)
+    print(f'All sessions have been closed and reservations have been cleared.\n')
 
-# If NI-SCOPE API or Session API throws an exception, print the error message  
+# If NI-SCOPE API or Session API throws an exception, print the error message.
 except grpc.RpcError as e:
     error_message = e.details()
     print(error_message)
