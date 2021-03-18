bff6b2e314ab197854607efd71ac619e762610cc
Akshata K <akshata.k@ni.com>
2021-03-18 23:03:04 +0530

    Incorporate review comments 1.

--- examples/niswitch/software-scanning.py
+++ examples/niswitch/software-scanning.py
@@ -1,4 +1,4 @@
-# Example communication with niSwitch over gRPC
+# Example communication with NI-SWITCH over gRPC
 #
 # Getting Started:
 #
@@ -8,40 +8,34 @@
 #     > conda install grpcio-tools
 #
 # Generate the python API from the gRPC definition (.proto) files
-#   > py -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. session.proto
-#   > py -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. niswitch.proto 
+# Note: The snippets below assume you are executing from the examples/session folder in the repo directory.
+#   > python -m grpc_tools.protoc -I../../source/protobuf --python_out=. --grpc_python_out=. session.proto
+#   > python -m grpc_tools.protoc -I../../generated/niswitch --python_out=. --grpc_python_out=. niswitch.proto 
 #
-# Run the code to initialize a session with Switch. This example demonstrates how to scan a series of channels on a switch using software scanning.
+# Run the code to initialize a session with NI-SWITCH. This example demonstrates how to scan a series of channels on a module using software scanning.
 #
-# Refer to the NI Switches Help to determine if your switch supports scanning, the scan list syntax, 
-# and the valid channel names and valid resource names for your switch module.
+# Refer to the NI-SWITCH Help to determine if your module supports scanning, the scan list syntax, valid channel names and valid resource names  
 
 import grpc
 import sys
 import time
-import niswitch_pb2 as switchTypes
-import niswitch_pb2_grpc as gRPCSwitch
+import niswitch_pb2 as niswitch_types
+import niswitch_pb2_grpc as grpc_niswitch
 
 # Server machine's IP address and port number have to be passed as two separate command line arguments.
-#   > python software-scanning.py localhost 31763
+#   > python software-scanning.py 10.20.30.40 31763
 # If not passed as command line arguments, then by default server address would be "localhost:31763"
 server_address = "localhost:31763"
 if len(sys.argv) == 3 :
     server_address = f"{sys.argv[1]}:{sys.argv[2]}"
 
-# Resource name and topology string for a simulated 2529 switch. Refer to NI-SWITCH help to find valid values for the device being used currently.
-resource = "Switch2"
+# Resource name and topology string for a simulated 2529 module. Refer to NI-SWITCH help to find valid values for the device being used currently.
+resource = "Switch1"
 topology_string = "2529/2-Wire Dual 4x16 Matrix"
 
-# Or you can use real hardware
-# resource = "2529"
-# channelName="0"
-# options = ""
-
-# Create the communcation channel for the remote host (in this case we are connecting to a local server)
-# and create a connection to the niSwitch service
+# Create the communcation channel for the remote host and create a connection to the NI-SWITCH service
 channel = grpc.insecure_channel(server_address)
-switch = gRPCSwitch.NiSwitchStub(channel)
+niswitch_client = grpc_niswitch.NiSwitchStub(channel)
 number_of_triggers = 5
 anyError = False
 
@@ -54,15 +48,15 @@ def CheckForError (vi, status) :
 
 # Converts an error code returned by NI-SWITCH into a user-readable string
 def ThrowOnError (vi, errorCode):
-    error_message_request = switchTypes.ErrorMessageRequest(
+    error_message_request = niswitch_types.ErrorMessageRequest(
         vi=vi,
         error_code = errorCode
         )
-    error_message_response = switch.ErrorMessage(error_message_request)
+    error_message_response = niswitch_client.ErrorMessage(error_message_request)
     raise Exception (error_message_response.error_message)
 try :
-    # Open session to switch module and set topology. Set simulate to false if actual device is being used.
-    init_with_topology_response = switch.InitWithTopology(switchTypes.InitWithTopologyRequest(
+    # Open session to NI-SWITCH and set topology. Set simulate parameter to false if real hardware device is being used.
+    init_with_topology_response = niswitch_client.InitWithTopology(niswitch_types.InitWithTopologyRequest(
         resource_name=resource,
         topology = topology_string,
         simulate=True,
@@ -71,51 +65,51 @@ try :
     vi = init_with_topology_response.vi
     CheckForError(vi,init_with_topology_response.status)
 
-    # Specify scan list. Use values that are valid for the switch model being used.
-    CheckForError(vi, (switch.ConfigureScanList(switchTypes.ConfigureScanListRequest(
+    # Specify scan list. Use values that are valid for the model being used.
+    CheckForError(vi, (niswitch_client.ConfigureScanList(niswitch_types.ConfigureScanListRequest(
         vi=vi,
         scanlist = "b0r1->b0c1;b0r1->b0c2;b0r2->b0c3",
-        scan_mode = switchTypes.ScanMode.SCAN_MODE_NISWITCH_VAL_BREAK_BEFORE_MAKE
+        scan_mode = niswitch_types.ScanMode.SCAN_MODE_NISWITCH_VAL_BREAK_BEFORE_MAKE
         ))).status)
 
     # Configures the trigger to be software trigger.
-    CheckForError(vi, (switch.ConfigureScanTrigger(switchTypes.ConfigureScanTriggerRequest(
+    CheckForError(vi, (niswitch_client.ConfigureScanTrigger(niswitch_types.ConfigureScanTriggerRequest(
         vi=vi,
-        trigger_input = switchTypes.TriggerInput.TRIGGER_INPUT_NISWITCH_VAL_SOFTWARE_TRIG,
-        scan_advanced_output = switchTypes.ScanAdvancedOutput.SCAN_ADVANCED_OUTPUT_NISWITCH_VAL_NONE
+        trigger_input = niswitch_types.TriggerInput.TRIGGER_INPUT_NISWITCH_VAL_SOFTWARE_TRIG,
+        scan_advanced_output = niswitch_types.ScanAdvancedOutput.SCAN_ADVANCED_OUTPUT_NISWITCH_VAL_NONE
         ))).status)
 
     # Loop through scan list continuously
-    CheckForError(vi, (switch.SetContinuousScan(switchTypes.SetContinuousScanRequest(
+    CheckForError(vi, (niswitch_client.SetContinuousScan(niswitch_types.SetContinuousScanRequest(
         vi=vi,
         continuous_scan = True
         ))).status)
 
     #Initiate scanning
-    CheckForError(vi, (switch.InitiateScan(switchTypes.InitiateScanRequest(
+    CheckForError(vi, (niswitch_client.InitiateScan(niswitch_types.InitiateScanRequest(
         vi=vi
         ))).status)
 
-    #Send software trigger to switch module in a loop
+    #Send software trigger to module in a loop
     for x in range(number_of_triggers):
         #Wait for 500 ms
         time.sleep(0.5)
-        CheckForError(vi, (switch.SendSoftwareTrigger(switchTypes.SendSoftwareTriggerRequest(
+        CheckForError(vi, (niswitch_client.SendSoftwareTrigger(niswitch_types.SendSoftwareTriggerRequest(
             vi=vi
             ))).status)
         number_of_triggers = number_of_triggers - 1    
 
     #Abort Scanning
-    CheckForError(vi, (switch.AbortScan(switchTypes.AbortScanRequest(
+    CheckForError(vi, (niswitch_client.AbortScan(niswitch_types.AbortScanRequest(
         vi=vi
         ))).status)
 
-    # Close session to switch module.
-    CheckForError(vi, (switch.Close(switchTypes.CloseRequest(
+    # Close NI-SWITCH session.
+    CheckForError(vi, (niswitch_client.Close(niswitch_types.CloseRequest(
         vi = vi
         ))).status)
 
-# If NiSwitch API throws an exception, print the error message  
+# If NI-SWITCH API throws an exception, print the error message  
 except grpc.RpcError as rpc_error:
     error_message = rpc_error.details()
     if rpc_error.code() == grpc.StatusCode.UNAVAILABLE :
