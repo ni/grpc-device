bc26997af5f0b9afb26d5ef56ddac1d0f0692b3b
Palash Khare <palash.khare@ni.com>
2021-04-10 18:19:52 +0530

    example in python

---
+++ examples/nidcpower/test_api.py
@@ -0,0 +1,51 @@
+import grpc
+import nidcpower_pb2 as nidcpower_types
+import nidcpower_pb2_grpc as grpc_nidcpower
+import session_pb2 as session_types
+import session_pb2_grpc as grpc_session
+
+server_address = "localhost"
+server_port = "31763"
+
+resource = "SimulatedDCPower"
+options = "Simulate=1, DriverSetup=Model:4110; BoardType:PXI"
+
+any_error = False
+def CheckForError (vi, status) :
+    global any_error
+    if(status != 0 and not any_error):
+        any_error = True
+        ThrowOnError (vi, status)
+
+def ThrowOnError (vi, error_code):
+    error_message_request = nidcpower_types.ErrorMessageRequest(
+        vi = vi,
+        error_code = error_code
+        )
+    error_message_response = client.ErrorMessage(error_message_request)
+    raise Exception (error_message_response)
+
+channel = grpc.insecure_channel(f"{server_address}:{server_port}")
+client = grpc_nidcpower.NiDCPowerStub(channel)
+
+try :
+
+    # Initialise with channels API
+    initialize_with_independent_channels_response = client.InitializeWithIndependentChannels(nidcpower_types.InitializeWithIndependentChannelsRequest(
+        resource_name = resource,
+        reset = False,
+        option_string = options
+        ))
+    vi = initialize_with_independent_channels_response.vi
+    CheckForError(vi, initialize_with_independent_channels_response.status)
+    print("Initialize_with_indepenedent_Channels works fine")
+
+    # close API
+    CheckForError(vi, (client.Close(nidcpower_types.CloseRequest(
+        vi = vi
+        ))).status)
+    channel.close()
+
+except grpc.RpcError as e:
+    error_message = e.details()
+    print(error_message)
