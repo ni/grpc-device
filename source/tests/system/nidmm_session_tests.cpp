ea73f8693a9c5bd2a4d43cc28894dc4e257f35b7
Shantanu Shinde <32991453+ShantanuShinde@users.noreply.github.com>
2021-04-21 15:23:04 +0530

    [Owner Review] Added Session Tests for NI-DMM (#12)
    * autogenerated dmm files
    
    * removed session.proto from the generated
    
    * fixed InitWithOptions
    
    * Init update
    
    * changes
    
    * added more APIs
    
    * service.mako update
    
    * Added APIs
    
    * ni dmm session tests added
    
    * service.mako fix
    
    * added updated generated
    
    * added api tests
    
    * added api tests
    
    * resolved the errors in API generation
    
    * test update
    
    * cmakelists update
    
    * service helper fix
    
    * nisync
    
    * added back nisync
    
    * removed obselete apis
    
    * Update common_helpers.py
    
    * Update nifake_service.cpp
    
    * reverted change
    
    * reverted change
    
    * build
    
    * removed APIs with size issue
    
    * added GetErrorMessage
    
    * removed api test for PR
    
    * Updated cmake
    
    * reverted files
    
    * reverted files
    
    * reverted files
    
    * Added cname tag, removed documentation tags
    
    * PR comment changes
    
    * PR comment changes
    
    * PR comment changes
    
    * core server name NiDmm name update
    
    * NiDMM -> NiDmm

---
+++ source/tests/system/nidmm_session_tests.cpp
@@ -0,0 +1,150 @@
+#include <gtest/gtest.h>
+
+#include "nidmm/nidmm_library.h"
+#include "nidmm/nidmm_service.h"
+
+namespace ni {
+namespace tests {
+namespace system {
+
+namespace dmm = nidmm_grpc;
+
+const int kViErrorRsrcNFound = -1074118656;
+const char* kViErrorResourceNotFoundMessage = "Device was not recognized. The device is not supported with this driver or version.";
+const char* kResourceName = "FakeDevice";
+const char* kOptionsString = "Simulate=1, DriverSetup=Model:4065; BoardType:PCI";
+const char* kSessionName = "SessionName";
+const char* kInvalidRsrc = "";
+
+class NiDmmSessionTest : public ::testing::Test{
+    protected:
+        NiDmmSessionTest()
+        {
+            ::grpc::ServerBuilder builder;
+            session_repository_ = std::make_unique<nidevice_grpc::SessionRepository>();
+            nidmm_library_ = std::make_unique<dmm::NiDmmLibrary>();
+            nidmm_service_ = std::make_unique<dmm::NiDmmService>(nidmm_library_.get(), session_repository_.get());
+            builder.RegisterService(nidmm_service_.get());
+
+            server_ = builder.BuildAndStart();
+            ResetStubs();
+        }
+
+        virtual ~NiDmmSessionTest() {}
+
+        void ResetStubs()
+        {
+            channel_ = server_->InProcessChannel(::grpc::ChannelArguments());
+            nidmm_stub_ = dmm::NiDmm::NewStub(channel_);
+        }
+
+        std::unique_ptr<dmm::NiDmm::Stub>& GetStub()
+        {
+            return nidmm_stub_;
+        }
+
+        ::grpc::Status call_init_with_options(const char* resource_name, const char* option_string, const char* session_name, dmm::InitWithOptionsResponse* response)
+        {
+            ::grpc::ClientContext context;
+            dmm::InitWithOptionsRequest request;
+            request.set_resource_name(resource_name);
+            request.set_option_string(option_string);
+            request.set_session_name(session_name);
+            request.set_reset_device(false);
+            request.set_id_query(false);
+
+            ::grpc::Status status = GetStub()->InitWithOptions(&context, request, response);
+            return status;
+        }
+
+    private:
+        std::shared_ptr<::grpc::Channel> channel_;
+        std::unique_ptr<dmm::NiDmm::Stub> nidmm_stub_;
+        std::unique_ptr<nidevice_grpc::SessionRepository> session_repository_;
+        std::unique_ptr<dmm::NiDmmLibrary> nidmm_library_;
+        std::unique_ptr<dmm::NiDmmService> nidmm_service_;
+        std::unique_ptr<::grpc::Server> server_;
+};
+
+TEST_F(NiDmmSessionTest, InitializeSessionWithDeviceAndSessionName_CreatesDriverSession)
+{
+  dmm::InitWithOptionsResponse response;
+  ::grpc::Status status = call_init_with_options(kResourceName, kOptionsString, kSessionName, &response);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(0, response.status());
+  EXPECT_NE(0, response.vi().id());
+}
+
+TEST_F(NiDmmSessionTest, InitializeSessionWithDeviceAndNoSessionName_CreatesDriverSession)
+{
+  dmm::InitWithOptionsResponse response;
+  ::grpc::Status status = call_init_with_options(kResourceName, kOptionsString, "", &response);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(0, response.status());
+  EXPECT_NE(0, response.vi().id());
+}
+
+TEST_F(NiDmmSessionTest, InitializeSessionWithoutDevice_ReturnsDriverError)
+{
+  dmm::InitWithOptionsResponse response;
+  ::grpc::Status status = call_init_with_options(kInvalidRsrc, "", "", &response);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(kViErrorRsrcNFound, response.status());
+  EXPECT_EQ(0, response.vi().id());
+}
+
+TEST_F(NiDmmSessionTest, InitializedSession_CloseSession_ClosesDriverSession)
+{
+  dmm::InitWithOptionsResponse init_response;
+  call_init_with_options(kResourceName, kOptionsString, kSessionName, &init_response);
+  nidevice_grpc::Session session = init_response.vi();
+
+  ::grpc::ClientContext context;
+  dmm::CloseRequest close_request;
+  close_request.mutable_vi()->set_id(session.id());
+  dmm::CloseResponse close_response;
+  ::grpc::Status status = GetStub()->Close(&context, close_request, &close_response);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(0, close_response.status());
+}
+
+TEST_F(NiDmmSessionTest, InvalidSession_CloseSession_NoErrorReported)
+{
+  nidevice_grpc::Session session;
+  session.set_id(NULL);
+
+  ::grpc::ClientContext context;
+  dmm::CloseRequest request;
+  request.mutable_vi()->set_id(session.id());
+  dmm::CloseResponse response;
+  ::grpc::Status status = GetStub()->Close(&context, request, &response);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(0, response.status());
+}
+
+TEST_F(NiDmmSessionTest, ErrorFromDriver_GetErrorMessage_ReturnsUserErrorMessage)
+{
+  dmm::InitWithOptionsResponse init_response;
+  call_init_with_options(kInvalidRsrc, "", "", &init_response);
+  EXPECT_EQ(kViErrorRsrcNFound, init_response.status());
+
+  nidevice_grpc::Session session = init_response.vi();
+  ::grpc::ClientContext context;
+  dmm::GetErrorMessageRequest error_request;
+  error_request.mutable_vi()->set_id(session.id());
+  error_request.set_error_code(kViErrorRsrcNFound);
+  dmm::GetErrorMessageResponse error_response;
+  ::grpc::Status status = GetStub()->GetErrorMessage(&context, error_request, &error_response);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_STREQ(kViErrorResourceNotFoundMessage, error_response.error_message().c_str());
+}
+
+} // namespace system
+} // namespace tests 
+} // namespace ni
