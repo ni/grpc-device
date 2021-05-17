516fd4da47260ec4bd4bdbae7ed89f3e3ba156a6
ni-pkhare <78592845+ni-pkhare@users.noreply.github.com>
2021-05-17 09:21:16 +0530

    minor (#77)

--- generated/nidigital/nidigital_service.cpp
+++ generated/nidigital/nidigital_service.cpp
@@ -224,6 +224,8 @@ namespace nidigital_grpc {
       auto vi_grpc_session = request->vi();
       ViSession vi = session_repository_->access_session(vi_grpc_session.id(), vi_grpc_session.name());
       session_repository_->remove_session(vi);
+      auto status = library_->Close(vi);
+      response->set_status(status);
       return ::grpc::Status::OK;
     }
     catch (nidevice_grpc::LibraryLoadException& ex) {
