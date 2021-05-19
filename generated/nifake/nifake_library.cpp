c2db98b0119c1bbe3255582f9462f36cf42d228e
Shantanu Shinde <32991453+ShantanuShinde@users.noreply.github.com>
2021-05-19 05:30:41 +0530

    [Owner Review] Added Handling of ViInt32 and ViUInt32 output arrays as they are used in Digital Pattern APIs (#80)
    * added int32 out handle
    
    * added handling of int32 and uint32 output array
    
    * updated CHANGES.md

--- generated/nifake/nifake_library.cpp
+++ generated/nifake/nifake_library.cpp
@@ -48,6 +48,8 @@ NiFakeLibrary::NiFakeLibrary() : shared_library_(kLibraryName)
   function_pointers_.GetCustomTypeArray = reinterpret_cast<GetCustomTypeArrayPtr>(shared_library_.get_function_pointer("niFake_GetCustomTypeArray"));
   function_pointers_.GetEnumValue = reinterpret_cast<GetEnumValuePtr>(shared_library_.get_function_pointer("niFake_GetEnumValue"));
   function_pointers_.GetError = reinterpret_cast<GetErrorPtr>(shared_library_.get_function_pointer("niFake_GetError"));
+  function_pointers_.GetViInt32Array = reinterpret_cast<GetViInt32ArrayPtr>(shared_library_.get_function_pointer("niFake_GetViInt32Array"));
+  function_pointers_.GetViUInt32Array = reinterpret_cast<GetViUInt32ArrayPtr>(shared_library_.get_function_pointer("niFake_GetViUInt32Array"));
   function_pointers_.ImportAttributeConfigurationBuffer = reinterpret_cast<ImportAttributeConfigurationBufferPtr>(shared_library_.get_function_pointer("niFake_ImportAttributeConfigurationBuffer"));
   function_pointers_.InitWithOptions = reinterpret_cast<InitWithOptionsPtr>(shared_library_.get_function_pointer("niFake_InitWithOptions"));
   function_pointers_.Initiate = reinterpret_cast<InitiatePtr>(shared_library_.get_function_pointer("niFake_Initiate"));
@@ -407,6 +409,30 @@ ViStatus NiFakeLibrary::GetError(ViSession vi, ViStatus* errorCode, ViInt32 buff
     throw nidevice_grpc::LibraryLoadException("Could not find niFake_GetError.");
   }
   return function_pointers_.GetError(vi, errorCode, bufferSize, description);
+}
+
+ViStatus NiFakeLibrary::GetViInt32Array(ViSession vi, ViInt32 arrayLen, ViInt32 int32Array[])
+{
+  if (!function_pointers_.GetViInt32Array) {
+    throw nidevice_grpc::LibraryLoadException("Could not find niFake_GetViInt32Array.");
+  }
+#if defined(_MSC_VER)
+  return niFake_GetViInt32Array(vi, arrayLen, int32Array);
+#else
+  return function_pointers_.GetViInt32Array(vi, arrayLen, int32Array);
+#endif
+}
+
+ViStatus NiFakeLibrary::GetViUInt32Array(ViSession vi, ViInt32 arrayLen, ViUInt32 uInt32Array[])
+{
+  if (!function_pointers_.GetViUInt32Array) {
+    throw nidevice_grpc::LibraryLoadException("Could not find niFake_GetViUInt32Array.");
+  }
+#if defined(_MSC_VER)
+  return niFake_GetViUInt32Array(vi, arrayLen, uInt32Array);
+#else
+  return function_pointers_.GetViUInt32Array(vi, arrayLen, uInt32Array);
+#endif
 }
 
 ViStatus NiFakeLibrary::ImportAttributeConfigurationBuffer(ViSession vi, ViInt32 sizeInBytes, ViInt8 configuration[])
--- source/codegen/metadata/nifake/CHANGES.md
+++ source/codegen/metadata/nifake/CHANGES.md
@@ -7,6 +7,12 @@ The following functions, not originally in nimi-python metadata, were newly adde
 - `'CloseExtCal'`
 - `'BoolArrayInputFunction'`
 	- This function allows testing of ViBoolean[] input parameter
+- `'AcceptViUInt32Array'`
+	- This function allows testing of ViUInt32[] input parameter
+- `'GetViInt32Array'`
+	- This function allows testing of ViInt32[] output parameter
+- `'GetViUInt32Array'`
+	- This function allows testing of ViUInt32[] output parameter
  
 The following functions were tagged with `'init_method': True,` to ensure their generated service handlers register the new session
 with the session_repository.
