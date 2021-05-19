894a99d68145f23cefa523abdfc6a759fbe7ad58
Shantanu Shinde <32991453+ShantanuShinde@users.noreply.github.com>
2021-05-19 05:32:02 +0530

    added metadata and generated apis with uint32 input array (#81)

--- generated/nidigital/nidigital_mock_library.h
+++ generated/nidigital/nidigital_mock_library.h
@@ -131,7 +131,9 @@ class NiDigitalMockLibrary : public nidigital_grpc::NiDigitalLibraryInterface {
   MOCK_METHOD(ViStatus, WaitUntilDone, (ViSession vi, ViReal64 timeout), (override));
   MOCK_METHOD(ViStatus, WriteSequencerFlag, (ViSession vi, ViConstString flag, ViBoolean value), (override));
   MOCK_METHOD(ViStatus, WriteSequencerRegister, (ViSession vi, ViConstString reg, ViInt32 value), (override));
+  MOCK_METHOD(ViStatus, WriteSourceWaveformBroadcastU32, (ViSession vi, ViConstString waveformName, ViInt32 waveformSize, ViUInt32 waveformData[]), (override));
   MOCK_METHOD(ViStatus, WriteSourceWaveformDataFromFileTDMS, (ViSession vi, ViConstString waveformName, ViConstString waveformFilePath), (override));
+  MOCK_METHOD(ViStatus, WriteSourceWaveformSiteUniqueU32, (ViSession vi, ViConstString siteList, ViConstString waveformName, ViInt32 numWaveforms, ViInt32 samplesPerWaveform, ViUInt32 waveformData[1]), (override));
 };
 
 }  // namespace unit
