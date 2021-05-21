01457de1abc000ad8d5587a48d80d596347b1dc2
ni-pkhare <78592845+ni-pkhare@users.noreply.github.com>
2021-05-21 09:20:30 +0530

    [Owners Review] Added metadata for function using ViUInt8  (#75)
    * mako file changes
    
    * metadata function addition and documentation
    
    * tests
    
    * mako script modification
    
    * new generated files
    
    * changing testing values
    
    * cleanup in mako script
    
    * minor
    
    * minor
    
    * Function added in metadata and documented
    
    * generated files
    
    * test values changed.
    
    * minor
    
    * minor
    
    * minor cleanup
    
    * T2* inplace of T2
    
    * modifications
    
    * minor

--- source/codegen/metadata/nidigital/functions.py
+++ source/codegen/metadata/nidigital/functions.py
@@ -2752,6 +2752,27 @@ functions = {
         ],
         "returns": "ViStatus"
     },
+    'WriteStatic': {
+        'parameters': [
+            {
+                'direction': 'in',
+                'name': 'vi',
+                'type': 'ViSession'
+            },
+            {
+                'direction': 'in',
+                'name': 'channelList',
+                'type': 'ViConstString'
+            },
+            {
+                'direction': 'in',
+                'enum': 'WriteStaticPinState',
+                'name': 'state',
+                'type': 'ViUInt8'
+            }
+        ],
+        'returns': 'ViStatus'
+    },
     'WriteSourceWaveformSiteUniqueU32': {
         'codegen_method': 'public',
         'parameters': [
