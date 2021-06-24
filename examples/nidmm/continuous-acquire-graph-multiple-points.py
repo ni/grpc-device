b6518ed10d2c008ab458d1a936740059e58fdaca
ni-pkhare <78592845+ni-pkhare@users.noreply.github.com>
2021-06-24 13:27:37 +0530

    [Owners Review] Display Port Number in addition to server_address in exception handling (#230)
    * examples modified for consistency
    
    * minor
    
    * server port added in message

--- examples/nidmm/continuous-acquire-graph-multiple-points.py
+++ examples/nidmm/continuous-acquire-graph-multiple-points.py
@@ -201,7 +201,7 @@ try:
 except grpc.RpcError as rpc_error:
     error_message = rpc_error.details()
     if rpc_error.code() == grpc.StatusCode.UNAVAILABLE :
-        error_message = f"Failed to connect to server on {server_address}"
+        error_message = f"Failed to connect to server on {server_address}:{server_port}"
     elif rpc_error.code() == grpc.StatusCode.UNIMPLEMENTED:
         error_message = "The operation is not implemented or is not supported/enabled in this service"
     print(f"{error_message}") 
