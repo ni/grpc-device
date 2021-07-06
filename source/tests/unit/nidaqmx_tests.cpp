644a1248f439061426f4c74b86e8c062310c1811
Greg Stoll <greg.stoll@ni.com>
2021-07-06 15:27:16 -0500

    working test

--- source/tests/unit/nidaqmx_tests.cpp
+++ source/tests/unit/nidaqmx_tests.cpp
@@ -8,18 +8,34 @@ namespace ni {
 namespace tests {
 namespace unit {
 
+namespace daqmx = nidaqmx_grpc;
+
+using ::std::get;
+MATCHER(CustomU16Data, "")
+{
+  uInt16 const* write_data_array = std::get<5>(arg);
+  return write_data_array[0] == 0 && write_data_array[1] == UINT16_MAX && write_data_array[2] == 16;
+}
+
 TEST(NIDAQMX_TESTS, WriteDigitalU16_U32DataGetsConverted)
 {
+  using ::testing::_;
   NiceMock<ni::tests::unit::NiDAQmxMockLibrary> mock_library;
+  EXPECT_CALL(mock_library, WriteDigitalU16(_, _, _, _, _, _, _, _))
+      .With(CustomU16Data())
+      .Times(1);
   nidevice_grpc::SessionRepository session_repository;
   nidaqmx_grpc::NiDAQmxService nidaqmx_service(&mock_library, &session_repository);
+  ::grpc::ServerContext context;
+  daqmx::WriteDigitalU16Request request;
+  request.add_write_array(0);
+  request.add_write_array(UINT16_MAX);
+  request.add_write_array(16);
+  daqmx::WriteDigitalU16Response response;
 
-  EXPECT_CALL(mock_library, WriteDigitalU16);
-  //nidevice_grpc::DeviceEnumerator device_enumerator(&mock_library);
-  //nidevice_grpc::SysCfgLibrary syscfg_library;
-  //std::string shared_library_name = syscfg_library.get_library_name();
+  nidaqmx_service.WriteDigitalU16(&context, &request, &response);
 
-  //EXPECT_STREQ(nidevice_grpc::kSysCfgApiLibraryName, shared_library_name.c_str());
+  ASSERT_EQ(0, response.status());
 }
 
 }  // namespace unit
