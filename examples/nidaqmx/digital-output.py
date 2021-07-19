db033a8477a53031fcc9bd71a1ce756ac137db06
Greg Stoll <greg.stoll@ni.com>
2021-07-18 20:34:38 -0500

    add DO example

---
+++ examples/nidaqmx/digital-output.py
@@ -0,0 +1,49 @@
+import grpc
+import nidaqmx_pb2 as nidaqmx_types
+import nidaqmx_pb2_grpc as grpc_nidaqmx
+
+server_address = "localhost"
+server_port = "31763"
+
+# Create a gRPC channel + client.
+channel = grpc.insecure_channel(f"{server_address}:{server_port}")
+client = grpc_nidaqmx.NiDAQmxStub(channel)
+
+# Raise an exception if an error was returned
+
+
+def RaiseIfError(response):
+    if response.status != 0:
+        response = client.GetErrorString(nidaqmx_types.GetErrorStringRequest(
+            error_code=response.status, buffer_size=2048))
+        error_string = response.error_string.strip(' \0')
+        raise Exception(f"Error: {error_string}")
+
+
+response = client.CreateTask(
+    nidaqmx_types.CreateTaskRequest(session_name="my task"))
+RaiseIfError(response)
+task = response.task
+
+try:
+    RaiseIfError(client.CreateDOChan(nidaqmx_types.CreateDOChanRequest(
+        task=task,
+        lines="Dev1/port0",
+        line_grouping=nidaqmx_types.LINE_GROUPING_CHAN_FOR_ALL_LINES
+    )))
+
+    RaiseIfError(client.StartTask(nidaqmx_types.StartTaskRequest(task=task)))
+
+    # TODO - write U32 instead
+    RaiseIfError(client.WriteDigitalU16(nidaqmx_types.WriteDigitalU16Request(
+        task=task,
+        num_samps_per_chan=1,
+        auto_start=True,
+        timeout=10.0,
+        data_layout=nidaqmx_types.GROUP_BY_GROUP_BY_CHANNEL,
+        write_array=[0xffff]
+    )))
+
+finally:
+    client.StopTask(nidaqmx_types.StopTaskRequest(task=task))
+    client.ClearTask(nidaqmx_types.ClearTaskRequest(task=task))
