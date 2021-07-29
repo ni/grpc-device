4268b68eb105d99a112be248236387eb0270272f
Alex Starche <alex.starche@ni.com>
2021-07-29 09:30:42 -0500

    Fix examples

--- examples/nidaqmx/counter-output.py
+++ examples/nidaqmx/counter-output.py
@@ -26,9 +26,17 @@ server_address = "localhost"
 server_port = "31763"
 counter_name = "Dev1/ctr0"
 
+if len(sys.argv) >= 2:
+    server_address = sys.argv[1]
+if len(sys.argv) >= 3:
+    server_port = sys.argv[2]
+if len(sys.argv) >= 4:
+    counter_name = sys.argv[3]
+
 # Create a gRPC channel + client.
 channel = grpc.insecure_channel(f"{server_address}:{server_port}")
 client = grpc_nidaqmx.NiDAQmxStub(channel)
+
 
 # Raise an exception if an error was returned
 def RaiseIfError(response):
@@ -37,14 +45,6 @@ def RaiseIfError(response):
             error_code=response.status, buffer_size=2048))
         error_string = response.error_string.strip(' \0')
         raise Exception(f"Error: {error_string}")
-
-
-if len(sys.argv) >= 2:
-    server_address = sys.argv[1]
-if len(sys.argv) >= 3:
-    server_port = sys.argv[2]
-if len(sys.argv) >= 4:
-    counter_name = sys.argv[3]
 
 
 response = client.CreateTask(
@@ -67,6 +67,15 @@ try:
     RaiseIfError(client.WaitUntilTaskDone(nidaqmx_types.WaitUntilTaskDoneRequest(
         task=task,
         time_to_wait=10.0)))
+
+    print(f"Output was successfully written to {counter_name}.")
+except grpc.RpcError as rpc_error:
+    error_message = rpc_error.details()
+    if rpc_error.code() == grpc.StatusCode.UNAVAILABLE :
+        error_message = f"Failed to connect to server on {server_address}:{server_port}"
+    elif rpc_error.code() == grpc.StatusCode.UNIMPLEMENTED:
+        error_message = "The operation is not implemented or is not supported/enabled in this service"
+    print(f"{error_message}") 
 finally:
     client.StopTask(nidaqmx_types.StopTaskRequest(task=task))
     client.ClearTask(nidaqmx_types.ClearTaskRequest(task=task))
