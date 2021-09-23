a0e6f68d6f96fc17d1546dd1589f0a7edaacfbf9
Alex Starche <alex.starche@ni.com>
2021-09-23 11:23:10 -0500

    Tests for string enums

--- source/tests/system/nirfsa_driver_api_tests.cpp
+++ source/tests/system/nirfsa_driver_api_tests.cpp
@@ -3,6 +3,7 @@
 
 #include "device_server.h"
 #include "ivi.h"  // VI_SUCCESS
+#include "niRFSA.h"
 #include "niRFSAErrors.h"
 #include "nirfsa/nirfsa_client.h"
 
@@ -17,6 +18,13 @@ namespace system {
 
 const auto PXI_5663E = "5663E";
 const auto PXI_5603 = "5603";
+
+template <typename TResponse>
+void EXPECT_SUCCESS(const TResponse& response)
+{
+  EXPECT_EQ(VI_SUCCESS, response.status());
+}
+
 class NiRFSADriverApiTests : public Test {
  protected:
   NiRFSADriverApiTests()
@@ -27,7 +35,7 @@ class NiRFSADriverApiTests : public Test {
 
   virtual ~NiRFSADriverApiTests() {}
 
-  void SetUp() override
+  void TearDown() override
   {
     device_server_->ResetServer();
   }
@@ -37,23 +45,23 @@ class NiRFSADriverApiTests : public Test {
     return stub_;
   }
 
+  void check_error(const nidevice_grpc::Session& session)
+  {
+    auto response = client::get_error(stub(), session, 2048);
+    EXPECT_EQ("", std::string(response.error_description().c_str()));
+  }
+
+  template <typename TResponse>
+  void EXPECT_SUCCESS(const nidevice_grpc::Session& session, const TResponse& response)
+  {
+    ni::tests::system::EXPECT_SUCCESS(response);
+    check_error(session);
+  }
+
  private:
   DeviceServerInterface* device_server_;
   std::unique_ptr<NiRFSA::Stub> stub_;
 };
-
-template <typename TResponse>
-void EXPECT_SUCCESS(const TResponse& response)
-{
-  EXPECT_EQ(VI_SUCCESS, response.status());
-}
-
-template <typename TResponse>
-void EXPECT_SUCCESS(const ::grpc::Status& status, const TResponse& response)
-{
-  EXPECT_SUCCESS(response);
-  EXPECT_EQ(::grpc::Status::OK.error_code(), status.error_code());
-}
 
 InitWithOptionsResponse init(const client::StubPtr& stub, const std::string& model)
 {
@@ -64,18 +72,20 @@ InitWithOptionsResponse init(const client::StubPtr& stub, const std::string& mod
 nidevice_grpc::Session init_session(const client::StubPtr& stub, const std::string& model)
 {
   auto response = init(stub, model);
+  auto session = response.vi();
   EXPECT_SUCCESS(response);
-  return response.vi();
+  return session;
 }
 
 TEST_F(NiRFSADriverApiTests, Init_Close_Succeeds)
 {
   auto init_response = init(stub(), PXI_5663E);
+  auto session = init_response.vi();
+  EXPECT_SUCCESS(session, init_response);
 
-  auto close_response = client::close(stub(), init_response.vi());
+  auto close_response = client::close(stub(), session);
 
-  EXPECT_SUCCESS(init_response);
-  EXPECT_SUCCESS(close_response);
+  EXPECT_SUCCESS(session, close_response);
 }
 
 TEST_F(NiRFSADriverApiTests, ConfigureGettingStartedIQ_Succeeds)
@@ -87,11 +97,11 @@ TEST_F(NiRFSADriverApiTests, ConfigureGettingStartedIQ_Succeeds)
   auto configure_number_of_samples = client::configure_number_of_samples(stub(), session, "", true, 1000);
   auto configure_iq_rate = client::configure_iq_rate(stub(), session, "", 1e6);
 
-  EXPECT_SUCCESS(configure_clock);
-  EXPECT_SUCCESS(configure_reference_level);
-  EXPECT_SUCCESS(configure_acquisition_type);
-  EXPECT_SUCCESS(configure_number_of_samples);
-  EXPECT_SUCCESS(configure_iq_rate);
+  EXPECT_SUCCESS(session, configure_clock);
+  EXPECT_SUCCESS(session, configure_reference_level);
+  EXPECT_SUCCESS(session, configure_acquisition_type);
+  EXPECT_SUCCESS(session, configure_number_of_samples);
+  EXPECT_SUCCESS(session, configure_iq_rate);
 }
 
 TEST_F(NiRFSADriverApiTests, GetDeviceResponse_ReturnsResponseData)
@@ -100,7 +110,7 @@ TEST_F(NiRFSADriverApiTests, GetDeviceResponse_ReturnsResponseData)
   auto response = client::get_device_response(stub(), session, "", DeviceResponse::DEVICE_RESPONSE_DOWNCONVERTER_IF_RESPONSE);
 
   const auto EXPECTED_SIZE = 81;
-  EXPECT_SUCCESS(response);
+  EXPECT_SUCCESS(session, response);
   EXPECT_THAT(response.frequencies(), Each(Not(Eq(0))));
   EXPECT_THAT(response.phase_response(), Each(Eq(0)));
   EXPECT_THAT(response.magnitude_response(), Each(Eq(0)));
@@ -115,7 +125,7 @@ TEST_F(NiRFSADriverApiTests, GetRelayName_ReturnsRelayName)
   auto session = init_session(stub(), PXI_5603);
   auto response = client::get_relay_name(stub(), session, "", 1);
 
-  EXPECT_SUCCESS(response);
+  EXPECT_SUCCESS(session, response);
   EXPECT_EQ("Cal Tone Path Switch", response.name());
 }
 
@@ -125,10 +135,50 @@ TEST_F(NiRFSADriverApiTests, GetRelayOperationsCount_ReturnsOperationCounts)
   auto session = init_session(stub(), PXI_5603);
   auto response = client::get_relay_operations_count(stub(), session, "");
 
-  EXPECT_SUCCESS(response);
+  EXPECT_SUCCESS(session, response);
   EXPECT_THAT(
       response.operations_count(),
       ElementsAreArray(EXPECTED.cbegin(), EXPECTED.cend()));
+}
+
+TEST_F(NiRFSADriverApiTests, ConfigureDigitalEdgeRefTrigger_Succeeds)
+{
+  auto session = init_session(stub(), PXI_5663E);
+  auto response = client::configure_digital_edge_ref_trigger(
+      stub(),
+      session,
+      DigitalEdgeTriggerSource::DIGITAL_EDGE_TRIGGER_SOURCE_PXI_TRIG0_STR,
+      DigitalEdgeRangeTable::DIGITAL_EDGE_RANGE_TABLE_RISING_EDGE,
+      10);
+
+  EXPECT_SUCCESS(session, response);
+}
+
+TEST_F(NiRFSADriverApiTests, ReconfigureExportedRefClockOutTerminal_UpdatesRefClockSuccessfully)
+{
+  auto session = init_session(stub(), PXI_5663E);
+  auto initial_response = client::get_attribute_vi_string(
+      stub(),
+      session,
+      "",
+      NiRFSAAttributes::NIRFSA_ATTRIBUTE_EXPORTED_REF_CLOCK_OUTPUT_TERMINAL);
+  auto set_response = client::set_attribute_vi_string(
+      stub(),
+      session,
+      "",
+      NiRFSAAttributes::NIRFSA_ATTRIBUTE_EXPORTED_REF_CLOCK_OUTPUT_TERMINAL,
+      NiRFSAStringAttributeValuesMapped::NIRFSA_STRING_REF_CLOCK_OUT_TERMINAL_RANGE_TABLE_REF_OUT_STR);
+  auto get_response = client::get_attribute_vi_string(
+      stub(),
+      session,
+      "",
+      NiRFSAAttributes::NIRFSA_ATTRIBUTE_EXPORTED_REF_CLOCK_OUTPUT_TERMINAL);
+
+  EXPECT_SUCCESS(session, initial_response);
+  EXPECT_SUCCESS(session, set_response);
+  EXPECT_SUCCESS(session, get_response);
+  EXPECT_NE(initial_response.value(), get_response.value());
+  EXPECT_EQ(NIRFSA_VAL_REF_OUT_STR, get_response.value());
 }
 
 }  // namespace system
