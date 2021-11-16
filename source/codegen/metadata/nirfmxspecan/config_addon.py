41f16b0040f9a0424a77bf01fc4ecfecef7f2d0d
Greg Stoll <greg.stoll@ni.com>
2021-11-16 10:46:41 -0600

    remove TODO

--- source/codegen/metadata/nirfmxspecan/config_addon.py
+++ source/codegen/metadata/nirfmxspecan/config_addon.py
@@ -1,5 +1,4 @@
 # We need to maintain the version here since it needs to be updated by the build process on GitHub
-# TODO
 config_additional_config = {
     'module_version': '1.3.3.dev0',
     'latest_runtime_version_tested_against': '21.3.0',
