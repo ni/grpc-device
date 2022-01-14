9b77ca63ddb734703324bb22383f362c9a69f32f
astarche <56981669+astarche@users.noreply.github.com>
2022-01-14 09:36:03 -0600

    Filter RFmx drivers from the build on Linux (#467)
    * Filter RFmx drivers from the build on Linux
    
    * Fixup register_all_services
    
    * Remove tests on linux
    
    * Fixup cmakelists
    
    * fixup instr handl repository
    
    * Switch to windows_only flag for register_all_services
    
    * remove messages
    
    * minor cleanup

--- generated/register_all_services.cpp
+++ generated/register_all_services.cpp
@@ -16,8 +16,12 @@
 #include "nidigitalpattern/nidigitalpattern_service_registrar.h"
 #include "nidmm/nidmm_service_registrar.h"
 #include "nifgen/nifgen_service_registrar.h"
+#if defined(_MSC_VER)
 #include "nirfmxinstr/nirfmxinstr_service_registrar.h"
+#endif // defined(_MSC_VER)
+#if defined(_MSC_VER)
 #include "nirfmxspecan/nirfmxspecan_service_registrar.h"
+#endif // defined(_MSC_VER)
 #include "nirfsa/nirfsa_service_registrar.h"
 #include "nirfsg/nirfsg_service_registrar.h"
 #include "niscope/niscope_service_registrar.h"
@@ -41,7 +45,9 @@ std::shared_ptr<void> register_all_services(
 
   auto task_handle_repository = std::make_shared<nidevice_grpc::SessionResourceRepository<TaskHandle>>(session_repository.get());
   auto vi_session_repository = std::make_shared<nidevice_grpc::SessionResourceRepository<ViSession>>(session_repository.get());
+#if defined(_MSC_VER)
   auto ni_r_fmx_instr_handle_repository = std::make_shared<nidevice_grpc::SessionResourceRepository<niRFmxInstrHandle>>(session_repository.get());
+#endif // defined(_MSC_VER)
 
   service_vector->push_back(
     nidaqmx_grpc::register_service(
@@ -68,17 +74,21 @@ std::shared_ptr<void> register_all_services(
       server_builder, 
       vi_session_repository,
       feature_toggles));
+#if defined(_MSC_VER)
   service_vector->push_back(
     nirfmxinstr_grpc::register_service(
       server_builder, 
       ni_r_fmx_instr_handle_repository,
       vi_session_repository,
       feature_toggles));
+#endif // defined(_MSC_VER)
+#if defined(_MSC_VER)
   service_vector->push_back(
     nirfmxspecan_grpc::register_service(
       server_builder, 
       ni_r_fmx_instr_handle_repository,
       feature_toggles));
+#endif // defined(_MSC_VER)
   service_vector->push_back(
     nirfsa_grpc::register_service(
       server_builder, 
