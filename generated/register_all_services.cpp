dbe68af74ac06a2e1dbda4fbaa99674b9707b80f
Dane Stull <dane.stull@ni.com>
2022-01-18 10:54:49 -0600

    Update register_all_services.cpp

--- generated/register_all_services.cpp
+++ generated/register_all_services.cpp
@@ -25,6 +25,7 @@
 #if defined(_MSC_VER)
 #include "nirfmxspecan/nirfmxspecan_service_registrar.h"
 #endif // defined(_MSC_VER)
+#include "nirfmxwlan/nirfmxwlan_service_registrar.h"
 #include "nirfsa/nirfsa_service_registrar.h"
 #include "nirfsg/nirfsg_service_registrar.h"
 #include "niscope/niscope_service_registrar.h"
@@ -48,9 +49,7 @@ std::shared_ptr<void> register_all_services(
 
   auto task_handle_repository = std::make_shared<nidevice_grpc::SessionResourceRepository<TaskHandle>>(session_repository.get());
   auto vi_session_repository = std::make_shared<nidevice_grpc::SessionResourceRepository<ViSession>>(session_repository.get());
-#if defined(_MSC_VER)
   auto ni_r_fmx_instr_handle_repository = std::make_shared<nidevice_grpc::SessionResourceRepository<niRFmxInstrHandle>>(session_repository.get());
-#endif // defined(_MSC_VER)
 
   service_vector->push_back(
     nidaqmx_grpc::register_service(
@@ -99,6 +98,11 @@ std::shared_ptr<void> register_all_services(
       ni_r_fmx_instr_handle_repository,
       feature_toggles));
 #endif // defined(_MSC_VER)
+  service_vector->push_back(
+    nirfmxwlan_grpc::register_service(
+      server_builder, 
+      ni_r_fmx_instr_handle_repository,
+      feature_toggles));
   service_vector->push_back(
     nirfsa_grpc::register_service(
       server_builder, 
