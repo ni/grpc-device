a74bea3304a1c86e45a60a447ba7eebb647955c1
Ryan Eckenrode <ryan.eckenrode@ni.com>
2022-01-20 16:21:31 -0600

    Add LTE tests for Init/Close

---
+++ source/tests/system/nirfmxlte_session_tests.cpp
@@ -0,0 +1,153 @@
+#include <gtest/gtest.h>
+
+#include "device_server.h"
+#include "nirfmxlte/nirfmxlte_service.h"
+
+namespace ni {
+namespace tests {
+namespace system {
+namespace {
+
+namespace rfmxlte = nirfmxlte_grpc;
+
+const int kInvalidRsrc = -200220;
+const int kInvalidRFmxLTESession = -380598;
+const char* kRFmxLTETestRsrc = "FakeDevice";
+const char* kRFmxLTEOptionsString = "Simulate=1, DriverSetup=Model:5663E";
+const char* kRFmxLTETestSession = "SessionName";
+const char* kRFmxLTETestSessionTwo = "SessionTwoName";
+const char* kRFmxLTETestInvalidRsrc = "";
+
+class NiRFmxLTESessionTest : public ::testing::Test {
+ protected:
+  NiRFmxLTESessionTest()
+      : device_server_(DeviceServerInterface::Singleton()),
+        nirfmxlte_stub_(rfmxlte::NiRFmxLTE::NewStub(device_server_->InProcessChannel()))
+  {
+    device_server_->ResetServer();
+  }
+
+  virtual ~NiRFmxLTESessionTest() {}
+
+  std::unique_ptr<rfmxlte::NiRFmxLTE::Stub>& GetStub()
+  {
+    return nirfmxlte_stub_;
+  }
+
+  ::grpc::Status call_initialize(const char* resource_name, const char* option_string, const char* session_name, rfmxlte::InitializeResponse* response)
+  {
+    ::grpc::ClientContext context;
+    rfmxlte::InitializeRequest request;
+    request.set_resource_name(resource_name);
+    request.set_option_string(option_string);
+    request.set_session_name(session_name);
+
+    ::grpc::Status status = GetStub()->Initialize(&context, request, response);
+    return status;
+  }
+
+  ::grpc::Status call_close(nidevice_grpc::Session session, nirfmxlte_grpc::Boolean force_destroy, rfmxlte::CloseResponse* response)
+  {
+    ::grpc::ClientContext context;
+    rfmxlte::CloseRequest request;
+    request.mutable_instrument()->set_id(session.id());
+    request.set_force_destroy(force_destroy);
+
+    ::grpc::Status status = GetStub()->Close(&context, request, response);
+    return status;
+  }
+
+ private:
+  DeviceServerInterface* device_server_;
+  std::unique_ptr<rfmxlte::NiRFmxLTE::Stub> nirfmxlte_stub_;
+};
+
+TEST_F(NiRFmxLTESessionTest, InitializeSessionWithDeviceAndSessionName_CreatesDriverSession)
+{
+  rfmxlte::InitializeResponse response;
+  ::grpc::Status status = call_initialize(kRFmxLTETestRsrc, kRFmxLTEOptionsString, kRFmxLTETestSession, &response);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(0, response.status());
+  EXPECT_NE(0, response.instrument().id());
+}
+
+TEST_F(NiRFmxLTESessionTest, InitializeSessionWithDeviceAndNoSessionName_CreatesDriverSession)
+{
+  rfmxlte::InitializeResponse response;
+  ::grpc::Status status = call_initialize(kRFmxLTETestRsrc, kRFmxLTEOptionsString, "", &response);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(0, response.status());
+  EXPECT_NE(0, response.instrument().id());
+}
+
+TEST_F(NiRFmxLTESessionTest, InitializeSessionWithoutDevice_ReturnsDriverError)
+{
+  rfmxlte::InitializeResponse response;
+  ::grpc::Status status = call_initialize(kRFmxLTETestInvalidRsrc, "", "", &response);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(kInvalidRsrc, response.status());
+  EXPECT_EQ(0, response.instrument().id());
+}
+
+TEST_F(NiRFmxLTESessionTest, InitializedSession_CloseSession_ClosesDriverSession)
+{
+  rfmxlte::InitializeResponse init_response;
+  call_initialize(kRFmxLTETestRsrc, kRFmxLTEOptionsString, kRFmxLTETestSession, &init_response);
+
+  nidevice_grpc::Session session = init_response.instrument();
+  ::grpc::ClientContext context;
+  rfmxlte::CloseRequest close_request;
+  close_request.mutable_instrument()->set_id(session.id());
+  close_request.set_force_destroy(nirfmxlte_grpc::Boolean::BOOLEAN_FALSE);
+  rfmxlte::CloseResponse close_response;
+  ::grpc::Status status = GetStub()->Close(&context, close_request, &close_response);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(0, close_response.status());
+}
+
+TEST_F(NiRFmxLTESessionTest, TwoInitializedSessionsOnSameDevice_CloseSessions_ClosesDriverSessions)
+{
+  rfmxlte::InitializeResponse init_response_one, init_response_two;
+  ::grpc::Status status_one = call_initialize(kRFmxLTETestRsrc, kRFmxLTEOptionsString, kRFmxLTETestSession, &init_response_one);
+  ::grpc::Status status_two = call_initialize(kRFmxLTETestRsrc, kRFmxLTEOptionsString, kRFmxLTETestSessionTwo, &init_response_two);
+  EXPECT_TRUE(status_one.ok());
+  EXPECT_EQ(0, init_response_one.status());
+  EXPECT_TRUE(status_two.ok());
+  EXPECT_EQ(0, init_response_two.status());
+
+  nidevice_grpc::Session session_one = init_response_one.instrument();
+  nidevice_grpc::Session session_two = init_response_two.instrument();
+  rfmxlte::CloseResponse close_response_one, close_response_two;
+  status_one = call_close(session_one, nirfmxlte_grpc::Boolean::BOOLEAN_FALSE, &close_response_one);
+  status_two = call_close(session_two, nirfmxlte_grpc::Boolean::BOOLEAN_FALSE, &close_response_two);
+
+  EXPECT_TRUE(status_one.ok());
+  EXPECT_EQ(0, close_response_one.status());
+  EXPECT_TRUE(status_two.ok());
+  EXPECT_EQ(0, close_response_two.status());
+}
+
+TEST_F(NiRFmxLTESessionTest, InvalidSession_CloseSession_ReturnsInvalidSessionError)
+{
+  nidevice_grpc::Session session;
+  session.set_id(0UL);
+
+  ::grpc::ClientContext context;
+  rfmxlte::CloseRequest request;
+  request.mutable_instrument()->set_id(session.id());
+  request.set_force_destroy(nirfmxlte_grpc::Boolean::BOOLEAN_FALSE);
+  rfmxlte::CloseResponse response;
+  ::grpc::Status status = GetStub()->Close(&context, request, &response);
+
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(kInvalidRFmxLTESession, response.status());
+}
+
+}  // namespace
+}  // namespace system
+}  // namespace tests
+}  // namespace ni
