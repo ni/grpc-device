44a156a0d6b96e287bc607bc8a29cb1e2efbd9fb
Ryan Eckenrode <ryan.eckenrode@ni.com>
2022-01-26 14:24:54 -0600

    Add broken InitializeFromNIRFSA test

--- source/tests/system/nirfmxnr_driver_api_tests.cpp
+++ source/tests/system/nirfmxnr_driver_api_tests.cpp
@@ -3,10 +3,12 @@
 #include "device_server.h"
 #include "niRFmxNR.h"
 #include "nirfmxnr/nirfmxnr_client.h"
+#include "nirfsa/nirfsa_client.h"
 
 using namespace ::testing;
 using namespace nirfmxnr_grpc;
 namespace client = nirfmxnr_grpc::experimental::client;
+namespace nirfsa_client = nirfsa_grpc::experimental::client;
 
 namespace ni {
 namespace tests {
@@ -65,6 +67,11 @@ InitializeResponse init(const client::StubPtr& stub, const std::string& model)
   return client::initialize(stub, "FakeDevice", options);
 }
 
+nirfsa_grpc::InitWithOptionsResponse init_rfsa(const nirfsa_client::StubPtr& stub, const std::string& resource_name)
+{
+  return nirfsa_client::init_with_options(stub, resource_name, false, false, "Simulate=1, DriverSetup=Model:5663E");
+}
+
 TEST_F(NiRFmxNRDriverApiTests, Init_Close_Succeeds)
 {
   auto init_response = init(stub(), PXI_5663E);
@@ -74,6 +81,18 @@ TEST_F(NiRFmxNRDriverApiTests, Init_Close_Succeeds)
   auto close_response = client::close(stub(), session, 0);
 
   ni::tests::system::EXPECT_SUCCESS(close_response);
+}
+
+TEST_F(NiRFmxNRDriverApiTests, InitializeFromNIRFSA_SelfCalibrate_Succeeds)
+{
+  auto rfsa_stub = create_stub<nirfsa_grpc::NiRFSA>();
+  auto init_rfsa_response = init_rfsa(rfsa_stub, "Sim");
+  ni::tests::system::EXPECT_SUCCESS(init_rfsa_response);
+  auto init_response = client::initialize_from_nirfsa_session(stub(), init_rfsa_response.vi());
+  auto session = init_response.instrument();
+  EXPECT_SUCCESS(session, init_response);
+
+  EXPECT_SUCCESS(session, client::close(stub(), session, false));
 }
 
 }  // namespace
