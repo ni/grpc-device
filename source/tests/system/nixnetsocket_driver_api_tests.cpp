878e46c74d811711a0e1e845625d4c9b96fdfbf6
Alex Starche <alex.starche@ni.com>
2022-03-04 15:11:40 -0600

    Tests fixup

--- source/tests/system/nixnetsocket_driver_api_tests.cpp
+++ source/tests/system/nixnetsocket_driver_api_tests.cpp
@@ -16,8 +16,9 @@ namespace system {
 namespace {
 
 constexpr auto INVALID_XNET_SOCKET = -1;
-constexpr auto FAILED_INIT = -1;
+constexpr auto GENERIC_NXSOCKET_ERROR = -1;
 constexpr auto SOCKET_COULD_NOT_BE_FOUND = -13009;
+constexpr auto SOCKET_COULD_NOT_BE_FOUND_MESSAGE = "The specified socket could not be found.";
 
 class NiXnetDriverApiTests : public ::testing::Test {
  protected:
@@ -60,9 +61,14 @@ class NiXnetDriverApiTests : public ::testing::Test {
     EXPECT_EQ(error, (response).status()); \
   }
 
+SocketResponse socket(client::StubPtr& stub)
+{
+  return client::socket(stub, 2 /* nxAF_INET */, 1 /* STREAM */, 6 /* TCP */);
+}
+
 TEST_F(NiXnetDriverApiTests, InitWithInvalidIpStack_Close_ReturnsAndSetsExpectedErrors)
 {
-  auto socket_response = client::socket(stub(), 2 /* nxAF_INET */, 1 /* STREAM */, 6 /* TCP */);
+  auto socket_response = socket(stub());
   auto socket_get_last_error = client::get_last_error_num(stub());
   auto socket_get_last_error_str = client::get_last_error_str(stub(), 512);
 
@@ -70,7 +76,7 @@ TEST_F(NiXnetDriverApiTests, InitWithInvalidIpStack_Close_ReturnsAndSetsExpected
   auto close_get_last_error = client::get_last_error_num(stub());
   auto close_get_last_error_str = client::get_last_error_str(stub(), 512);
 
-  EXPECT_XNET_ERROR(FAILED_INIT, socket_response);
+  EXPECT_XNET_ERROR(GENERIC_NXSOCKET_ERROR, socket_response);
   EXPECT_SUCCESS(socket_get_last_error);
   EXPECT_SUCCESS(socket_get_last_error_str);
   EXPECT_EQ("The specified IP Stack could not be found.", socket_get_last_error_str.error());
@@ -85,29 +91,29 @@ TEST_F(NiXnetDriverApiTests, InitWithInvalidIpStack_Bind_ReturnsAndSetsExpectedE
   auto sock_addr = SockAddr{};
   sock_addr.mutable_ipv4()->set_addr(0x7F000001);
   sock_addr.mutable_ipv4()->set_port(31764);
-  auto socket_response = client::socket(stub(), 2 /* nxAF_INET */, 1 /* STREAM */, 6 /* TCP */);
+  auto socket_response = socket(stub());
   auto bind_response = client::bind(stub(), socket_response.socket(), sock_addr);
   auto bind_get_last_error = client::get_last_error_num(stub());
   auto bind_get_last_error_str = client::get_last_error_str(stub(), 512);
 
-  EXPECT_XNET_ERROR(FAILED_INIT, socket_response);
-  EXPECT_XNET_ERROR(FAILED_INIT, bind_response);
-  EXPECT_XNET_ERROR(-13009, bind_get_last_error);
-  EXPECT_THAT("The specified socket could not be found.", bind_get_last_error_str.error());
+  EXPECT_XNET_ERROR(GENERIC_NXSOCKET_ERROR, socket_response);
+  EXPECT_XNET_ERROR(GENERIC_NXSOCKET_ERROR, bind_response);
+  EXPECT_XNET_ERROR(SOCKET_COULD_NOT_BE_FOUND, bind_get_last_error);
+  EXPECT_THAT(SOCKET_COULD_NOT_BE_FOUND_MESSAGE, bind_get_last_error_str.error());
 }
 
-TEST_F(NiXnetDriverApiTests, IsSet)
+TEST_F(NiXnetDriverApiTests, SocketAndEmptySet_IsSet_ReturnsFalse)
 {
-  auto socket_response = client::socket(stub(), 2 /* nxAF_INET */, 1 /* STREAM */, 6 /* TCP */);
+  auto socket_response = socket(stub());
   auto is_set_response = client::is_set(stub(), socket_response.socket(), {});
 
   EXPECT_SUCCESS(is_set_response);
   EXPECT_EQ(FALSE, is_set_response.is_set());
 }
 
-TEST_F(NiXnetDriverApiTests, IsSet_True)
+TEST_F(NiXnetDriverApiTests, SocketAndSetContainingSocket_IsSet_ReturnsTrue)
 {
-  auto socket_response = client::socket(stub(), 2 /* nxAF_INET */, 1 /* STREAM */, 6 /* TCP */);
+  auto socket_response = socket(stub());
   auto is_set_response = client::is_set(stub(), socket_response.socket(), {socket_response.socket()});
 
   EXPECT_SUCCESS(is_set_response);
@@ -116,7 +122,7 @@ TEST_F(NiXnetDriverApiTests, IsSet_True)
 
 TEST_F(NiXnetDriverApiTests, Select)
 {
-  auto socket_response = client::socket(stub(), 2 /* nxAF_INET */, 1 /* STREAM */, 6 /* TCP */);
+  auto socket_response = socket(stub());
   auto duration = pb::Duration{};
   duration.set_seconds(1);
   duration.set_nanos(500000);
@@ -124,9 +130,9 @@ TEST_F(NiXnetDriverApiTests, Select)
   auto select_last_error = client::get_last_error_num(stub());
   auto select_last_error_str = client::get_last_error_str(stub(), 512);
 
-  EXPECT_SUCCESS(select_response);
-  EXPECT_XNET_ERROR(0, select_last_error);
-  EXPECT_THAT("", select_last_error_str.error());
+  EXPECT_XNET_ERROR(GENERIC_NXSOCKET_ERROR, select_response);
+  EXPECT_XNET_ERROR(SOCKET_COULD_NOT_BE_FOUND, select_last_error);
+  EXPECT_THAT(SOCKET_COULD_NOT_BE_FOUND_MESSAGE, select_last_error_str.error());
 }
 }  // namespace
 }  // namespace system
