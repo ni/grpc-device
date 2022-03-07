84fccdffd207b1accba58a51c9b1aee034452054
astarche <56981669+astarche@users.noreply.github.com>
2022-03-07 11:32:25 -0600

    Add raise_if_initialization_error to rfmx examples (#586)

--- examples/nirfmxbluetooth/acp-basic.py
+++ examples/nirfmxbluetooth/acp-basic.py
@@ -69,6 +69,15 @@ client = grpc_nirfmxbluetooth.NiRFmxBluetoothStub(channel)
 instr = None
 
 
+def raise_if_initialization_error(response):
+    """Raise an exception if an error was returned from Initialize."""
+    if response.status < 0:
+        raise RuntimeError(f"Error: {response.error_message or response.status}")
+    if response.status > 0:
+        sys.stderr.write(f"Warning: {response.error_message or response.status}\n")
+    return response
+
+
 def raise_if_error(response):
     """Raise an exception if an error was returned."""
     if response.status != 0:
@@ -88,7 +97,7 @@ def raise_if_error(response):
 try:
     offset_channel_mode = nirfmxbluetooth_types.ACP_OFFSET_CHANNEL_MODE_SYMMETRIC
 
-    initialize_response = raise_if_error(
+    initialize_response = raise_if_initialization_error(
         client.Initialize(
             nirfmxbluetooth_types.InitializeRequest(
                 session_name=SESSION_NAME, resource_name=RESOURCE, option_string=OPTIONS
