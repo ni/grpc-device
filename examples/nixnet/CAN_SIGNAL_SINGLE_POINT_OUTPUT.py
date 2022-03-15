bc92def7e7e3a40224387091b8328303fa710baa
Aman Kothari <aman.kothari@ni.com>
2022-03-15 21:25:12 +0530

    Reformatted code with black

--- examples/nixnet/CAN_SIGNAL_SINGLE_POINT_OUTPUT.py
+++ examples/nixnet/CAN_SIGNAL_SINGLE_POINT_OUTPUT.py
@@ -1,4 +1,4 @@
-r""" Write Signal Data 
+r""" Write Signal Data.
 
  This example writes a signal value when a keyboard character is pressed.
  This is used to demonstrate a signal single point output session. 
@@ -54,14 +54,12 @@ SUCCESS = 0
 # Display Error Function
 def display_error_andexit(status, source):
     """Display error message and exit."""
-    response = client.StatusToString(
-        nixnet_types.StatusToStringRequest(
-            status_id = status
-        ))
+    response = client.StatusToString(nixnet_types.StatusToStringRequest(status_id=status))
     print("\n\nERROR at {}!\n{}\n", source, response.status_description)
     print("\nExecution stopped.\nPress any key to quit\n")
-    client.Clear(nixnet_types.ClearRequest(session_ref = session_reference))
+    client.Clear(nixnet_types.ClearRequest(session_ref=session_reference))
     exit(1)
+
 
 i = 0
 session_reference = 0
@@ -73,50 +71,50 @@ channel = grpc.insecure_channel(f"{SERVER_ADDRESS}:{SERVER_PORT}")
 client = grpc_nixnet.NiXnetStub(channel)
 
 # Display parameters that will be used for the example.
-print("Interface: "+ INTERFACE, "Database: " + DATABASE, "Signal List: " + SIGNAL_LIST, sep="\n")
+print("Interface: " + INTERFACE, "Database: " + DATABASE, "Signal List: " + SIGNAL_LIST, sep="\n")
 
 # Create an XNET session in SignalOutSinglePoint mode
 CreateSession_Response = client.CreateSession(
     nixnet_types.CreateSessionRequest(
-        database_name = DATABASE,
-        cluster_name = CLUSTER,
-        list = SIGNAL_LIST,
-        interface = INTERFACE, 
-        mode = nixnet_types.CREATE_SESSION_MODE_MODE_SIGNAL_OUT_SINGLE_POINT
-    ))
+        database_name=DATABASE,
+        cluster_name=CLUSTER,
+        list=SIGNAL_LIST,
+        interface=INTERFACE,
+        mode=nixnet_types.CREATE_SESSION_MODE_MODE_SIGNAL_OUT_SINGLE_POINT,
+    )
+)
 
 session_reference = CreateSession_Response.session_ref
 
 if CreateSession_Response.status == SUCCESS:
     print("Session Created Successfully.\n")
 else:
-    display_error_andexit(CreateSession_Response.status,"CreateSession")
-    
+    display_error_andexit(CreateSession_Response.status, "CreateSession")
+
 print("\nPress any key to transmit new signal values or q to quit\n")
 
-while not (getch.getch()).decode('UTF-8') == 'q':
+while not (getch.getch()).decode("UTF-8") == "q":
     value_buffer[0] = float(i)
-    value_buffer[1] = float(i*10)
-    
+    value_buffer[1] = float(i * 10)
+
     # Update the signal data
     response = client.WriteSignalSinglePoint(
         nixnet_types.WriteSignalSinglePointRequest(
-            session_ref = session_reference,
-            value_buffer = value_buffer
-        ))
-    
-    if response.status == SUCCESS: 
+            session_ref=session_reference, value_buffer=value_buffer
+        )
+    )
+
+    if response.status == SUCCESS:
         print("Signals sent:")
         print(f"Signal 1: {value_buffer[0]}")
         print(f"Signal 2: {value_buffer[1]}", end="\n\n")
-        i = i+1 if i<10 else 0
+        i = i + 1 if i < 10 else 0
     else:
-        display_error_andexit(response.status,"WriteSignalSinglePoint")
-            
+        display_error_andexit(response.status, "WriteSignalSinglePoint")
+
 # Clears the X-NET Session
-response = client.Clear(nixnet_types.ClearRequest(session_ref = session_reference))
+response = client.Clear(nixnet_types.ClearRequest(session_ref=session_reference))
 if response.status == SUCCESS:
     print("Session cleared successfully!\n")
 else:
-    display_error_andexit(response.status,"Clear")
-
+    display_error_andexit(response.status, "Clear")
