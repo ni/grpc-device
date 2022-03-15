06d3921158beef510d68504c235484603f5c5718
Ryan Eckenrode <ryan.eckenrode@ni.com>
2022-03-15 11:06:36 -0500

    Update comment for avoiding size for optval.

--- source/codegen/metadata/nixnetsocket/functions_addon.py
+++ source/codegen/metadata/nixnetsocket/functions_addon.py
@@ -5,7 +5,7 @@ functions_validation_suppressions = {
     name: {"parameters": {"mem": ["ARRAY_PARAMETER_NEEDS_SIZE"]}} for name in ["Recv", "RecvFrom"]
 }
 
-# Void* param output size is allocated differently.
+# Allocated with supports_standard_output_allocation.
 functions_validation_suppressions["GetSockOpt"] = {
     "parameters": {"optval": ["ARRAY_PARAMETER_NEEDS_SIZE"]}
 }
