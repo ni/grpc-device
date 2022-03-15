62c00f8526ba5806f2011259c8cff70147b2e721
Aman Kothari <aman.kothari@ni.com>
2022-03-15 21:14:04 +0530

    Updated StatusToString

--- examples/nixnet/CAN_SIGNAL_SINGLE_POINT_OUTPUT.py
+++ examples/nixnet/CAN_SIGNAL_SINGLE_POINT_OUTPUT.py
@@ -1,4 +1,5 @@
-r"""
+r""" Write Signal Data 
+
  This example writes a signal value when a keyboard character is pressed.
  This is used to demonstrate a signal single point output session. 
  This example uses hardcoded signal names that use the NIXNET_example database. 
@@ -7,21 +8,21 @@ The gRPC API is built from the C API. NI-XNET documentation is installed with th
   C:\Program Files (x86)\IVI Foundation\IVI\Drivers\niXNET\Documentation\NIXNETCref.chm
 Getting Started:
 
-To run this example, install "NI-DCPower Driver" on the server machine:
+To run this example, install "NI-XNET Driver" on the server machine:
   https://www.ni.com/en-in/support/downloads/drivers/download.ni-xnet.html
 
 For instructions on how to use protoc to generate gRPC client interfaces, see our "Creating a gRPC
 Client" wiki page:
   https://github.com/ni/grpc-device/wiki/Creating-a-gRPC-Client
 
-Refer to the NI DCPOWER gRPC Wiki for the latest C Function Reference:
+Refer to the NI XNET gRPC Wiki for the latest C Function Reference:
   https://github.com/ni/grpc-device/wiki/NI-XNET-C-Function-Reference
 
  Running from command line:
 
 Server machine's IP address, port number, and resource name can be passed as separate command line
 arguments.
-  > python measure-record.py <server_address> <port_number>
+  > python CAN_SIGNAL_SINGLE_POINT_OUTPUT.py <server_address> <port_number>
 If they are not passed in as command line arguments, then by default the server address will be
 "localhost:31763"
 """
@@ -53,9 +54,11 @@ SUCCESS = 0
 # Display Error Function
 def display_error_andexit(status, source):
     """Display error message and exit."""
-    status_string = [None] * 1024
-    nixnet_types.StatustoString(status, status_string.__sizeof__(), source) 
-    print("\n\nERROR at {}!\n{}\n", source, status_string)
+    response = client.StatusToString(
+        nixnet_types.StatusToStringRequest(
+            status_id = status
+        ))
+    print("\n\nERROR at {}!\n{}\n", source, response.status_description)
     print("\nExecution stopped.\nPress any key to quit\n")
     client.Clear(nixnet_types.ClearRequest(session_ref = session_reference))
     exit(1)
