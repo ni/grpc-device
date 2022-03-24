8b69414af7a8bd40a35cd5773702ee71dfa8c65d
Rohan Doshi <rohan.doshi@ni.com>
2022-03-24 10:39:47 +0530

    Update XNET Frame input APIs to remove buffer size parameters (#625)

--- generated/nixnet/nixnet_client.cpp
+++ generated/nixnet/nixnet_client.cpp
@@ -90,14 +90,15 @@ connect_terminals(const StubPtr& stub, const nidevice_grpc::Session& session_ref
 }
 
 ConvertByteArrayToFramesSinglePointResponse
-convert_byte_array_to_frames_single_point(const StubPtr& stub, const nidevice_grpc::Session& session_ref, const pb::string& value_buffer, const pb::uint32& size_of_buffer, const simple_variant<FrameType, pb::uint32>& frame_type)
+convert_byte_array_to_frames_single_point(const StubPtr& stub, const nidevice_grpc::Session& session_ref, const pb::string& value_buffer, const pb::uint32& number_of_frames, const pb::uint32& max_payload_per_frame, const simple_variant<FrameType, pb::uint32>& frame_type)
 {
   ::grpc::ClientContext context;
 
   auto request = ConvertByteArrayToFramesSinglePointRequest{};
   request.mutable_session_ref()->CopyFrom(session_ref);
   request.set_value_buffer(value_buffer);
-  request.set_size_of_buffer(size_of_buffer);
+  request.set_number_of_frames(number_of_frames);
+  request.set_max_payload_per_frame(max_payload_per_frame);
   const auto frame_type_ptr = frame_type.get_if<FrameType>();
   const auto frame_type_raw_ptr = frame_type.get_if<pb::uint32>();
   if (frame_type_ptr) {
@@ -152,14 +153,15 @@ convert_frames_to_signals_single_point(const StubPtr& stub, const nidevice_grpc:
 }
 
 ConvertSignalsToFramesSinglePointResponse
-convert_signals_to_frames_single_point(const StubPtr& stub, const nidevice_grpc::Session& session_ref, const std::vector<double>& value_buffer, const pb::uint32& size_of_buffer, const simple_variant<FrameType, pb::uint32>& frame_type)
+convert_signals_to_frames_single_point(const StubPtr& stub, const nidevice_grpc::Session& session_ref, const std::vector<double>& value_buffer, const pb::uint32& number_of_frames, const pb::uint32& max_payload_per_frame, const simple_variant<FrameType, pb::uint32>& frame_type)
 {
   ::grpc::ClientContext context;
 
   auto request = ConvertSignalsToFramesSinglePointRequest{};
   request.mutable_session_ref()->CopyFrom(session_ref);
   copy_array(value_buffer, request.mutable_value_buffer());
-  request.set_size_of_buffer(size_of_buffer);
+  request.set_number_of_frames(number_of_frames);
+  request.set_max_payload_per_frame(max_payload_per_frame);
   const auto frame_type_ptr = frame_type.get_if<FrameType>();
   const auto frame_type_raw_ptr = frame_type.get_if<pb::uint32>();
   if (frame_type_ptr) {
@@ -690,21 +692,14 @@ get_sub_property_size(const StubPtr& stub, const nidevice_grpc::Session& session
 }
 
 ReadFrameResponse
-read_frame(const StubPtr& stub, const nidevice_grpc::Session& session_ref, const pb::uint32& size_of_buffer, const simple_variant<TimeOut, double>& timeout, const simple_variant<FrameType, pb::uint32>& frame_type)
+read_frame(const StubPtr& stub, const nidevice_grpc::Session& session_ref, const pb::int32& number_of_frames, const pb::uint32& max_payload_per_frame, const simple_variant<FrameType, pb::uint32>& frame_type, const simple_variant<TimeOut, double>& timeout)
 {
   ::grpc::ClientContext context;
 
   auto request = ReadFrameRequest{};
   request.mutable_session_ref()->CopyFrom(session_ref);
-  request.set_size_of_buffer(size_of_buffer);
-  const auto timeout_ptr = timeout.get_if<TimeOut>();
-  const auto timeout_raw_ptr = timeout.get_if<double>();
-  if (timeout_ptr) {
-    request.set_timeout(*timeout_ptr);
-  }
-  else if (timeout_raw_ptr) {
-    request.set_timeout_raw(*timeout_raw_ptr);
-  }
+  request.set_number_of_frames(number_of_frames);
+  request.set_max_payload_per_frame(max_payload_per_frame);
   const auto frame_type_ptr = frame_type.get_if<FrameType>();
   const auto frame_type_raw_ptr = frame_type.get_if<pb::uint32>();
   if (frame_type_ptr) {
@@ -712,6 +707,14 @@ read_frame(const StubPtr& stub, const nidevice_grpc::Session& session_ref, const
   }
   else if (frame_type_raw_ptr) {
     request.set_frame_type_raw(*frame_type_raw_ptr);
+  }
+  const auto timeout_ptr = timeout.get_if<TimeOut>();
+  const auto timeout_raw_ptr = timeout.get_if<double>();
+  if (timeout_ptr) {
+    request.set_timeout(*timeout_ptr);
+  }
+  else if (timeout_raw_ptr) {
+    request.set_timeout_raw(*timeout_raw_ptr);
   }
 
   auto response = ReadFrameResponse{};
--- source/custom/nixnet_service.custom.cpp
+++ source/custom/nixnet_service.custom.cpp
@@ -1211,6 +1211,16 @@ void convert_to_grpc(std::vector<f64>& input, google::protobuf::RepeatedField<do
       return x;
     });
 }
+
+u32 get_frame_buffer_size(int32 number_of_frames, u32 max_payload_per_frame, u32 frame_type)
+{
+  if(frame_type == FrameType::FRAME_TYPE_ENET) {
+    return number_of_frames * (ENET_FRAME_HEADER_LENGTH + max_payload_per_frame);
+  }
+  else {
+    return number_of_frames * nxFrameSize(max_payload_per_frame);
+  }
+}
 }  // namespace nixnet_grpc
 
 namespace nidevice_grpc {
