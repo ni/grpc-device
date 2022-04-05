5e7504923fd6d87db073a88092212c24545011a4
ni-pkhare <78592845+ni-pkhare@users.noreply.github.com>
2022-04-05 12:46:07 +0530

    Add system tests for XNET LIN (#643)
    * minor
    
    * minor
    
    * minor
    
    * removed anonymous namespace and made some internal functions inline
    
    * hit n trial for linux build
    
    * build failure resolution
    
    * minor
    
    * making pb and client namespace as global
    
    * added helper for setting frame

---
+++ source/tests/system/nixnet_lin_driver_api_tests.cpp
@@ -0,0 +1,184 @@
+#include <gmock/gmock.h>
+#include <google/protobuf/util/time_util.h>
+#include <gtest/gtest.h>
+#undef interface
+#include <nixnet/nixnet_client.h>
+
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <vector>
+
+#include "device_server.h"
+#include "enumerate_devices.h"
+#include "nixnet_utilities.h"
+
+using namespace nixnet_grpc;
+namespace client = nixnet_grpc::experimental::client;
+namespace pb = google::protobuf;
+using namespace ::testing;
+using namespace ::nixnet_utilities;
+using nlohmann::json;
+
+namespace ni {
+namespace tests {
+namespace system {
+namespace {
+
+using u8 = pb::uint8;
+using u32 = pb::uint32;
+using u64 = pb::uint64;
+using f64 = double;
+
+class NiXnetLINDriverApiTests : public ::testing::Test {
+ protected:
+  NiXnetLINDriverApiTests()
+      : device_server_(DeviceServerInterface::Singleton()),
+        stub_(NiXnet::NewStub(device_server_->InProcessChannel()))
+  {
+    device_server_->ResetServer();
+  }
+  virtual ~NiXnetLINDriverApiTests() {}
+
+  void TearDown() override
+  {
+    device_server_->ResetServer();
+  }
+
+  template <typename TService>
+  std::unique_ptr<typename TService::Stub> create_stub()
+  {
+    return TService::NewStub(device_server_->InProcessChannel());
+  }
+
+  std::unique_ptr<NiXnet::Stub>& stub()
+  {
+    return stub_;
+  }
+
+  void assert_lin_frames_are_equal(nixnet_grpc::FrameRequest* frame1, nixnet_grpc::FrameResponse* frame2)
+  {
+    int frame1_flags = calculate_bitwise_or_of_flags(frame1->flags());
+    int frame2_flags = calculate_bitwise_or_of_flags(frame2->flags());
+    EXPECT_EQ(frame1_flags, frame2_flags);
+
+    EXPECT_EQ(frame1->type(), frame2->type());
+    EXPECT_EQ(frame1->identifier(), frame2->identifier());
+    EXPECT_EQ(frame1->payload(), frame2->payload());
+  }
+
+  void set_frame_data(nixnet_grpc::FrameRequest* frame, u64 timestamp, nixnet_grpc::FrameFlags frame_flag, u32 identifier, nixnet_grpc::FrameType frame_type, const char* payload)
+  {
+    frame->set_timestamp(timestamp);
+    frame->add_flags(frame_flag);
+    frame->set_identifier(identifier);
+    frame->set_type(frame_type);
+    frame->set_payload(payload);
+  }
+
+ private:
+  DeviceServerInterface* device_server_;
+  std::unique_ptr<NiXnet::Stub> stub_;
+};
+
+TEST_F(NiXnetLINDriverApiTests, ConvertFramesToFromSignalsFromExample_FetchData_DataLooksReasonable)
+{
+  constexpr auto NUM_FRAMES = 2;
+  constexpr auto NUM_SIGNALS = 2;
+  std::vector<nixnet_grpc::FrameBufferRequest> frames;
+  auto session = EXPECT_SUCCESS(client::create_session(stub(), "NIXNET_exampleLDF", "Cluster", "SlaveSignal1_U16,MasterSignal1_U16", "", CREATE_SESSION_MODE_SIGNAL_CONVERSION_SINGLE_POINT)).session();
+  nixnet_grpc::FrameRequest* frame1 = new nixnet_grpc::FrameRequest();
+  nixnet_grpc::FrameRequest* frame2 = new nixnet_grpc::FrameRequest();
+  set_frame_data(frame1, 0, FrameFlags::FRAME_FLAGS_UNSPECIFIED, 4, FrameType::FRAME_TYPE_LIN_DATA, "\2\1\2\3\4\5\6\7");
+  set_frame_data(frame2, 0, FrameFlags::FRAME_FLAGS_UNSPECIFIED, 2, FrameType::FRAME_TYPE_LIN_DATA, "\4\1");
+  frames.push_back(nixnet_grpc::FrameBufferRequest());
+  frames.back().set_allocated_lin(frame1);
+  frames.push_back(nixnet_grpc::FrameBufferRequest());
+  frames.back().set_allocated_lin(frame2);
+
+  auto convert_frames_to_signals_single_point_response = EXPECT_SUCCESS(client::convert_frames_to_signals_single_point(stub(), session, NUM_SIGNALS, frames));
+  std::vector<f64> value_buffer_copy(convert_frames_to_signals_single_point_response.value_buffer().begin(), convert_frames_to_signals_single_point_response.value_buffer().end());
+  auto convert_signals_to_frames_single_point_response = EXPECT_SUCCESS(client::convert_signals_to_frames_single_point(stub(), session, value_buffer_copy, NUM_FRAMES, 8, Protocol::PROTOCOL_LIN));
+  auto frame_out_1 = convert_signals_to_frames_single_point_response.buffer()[0];
+  auto frame_out_2 = convert_signals_to_frames_single_point_response.buffer()[1];
+  auto lin_1 = frame_out_1.lin();
+  auto lin_2 = frame_out_2.lin();
+
+  EXPECT_EQ(2, convert_frames_to_signals_single_point_response.timestamp_buffer().size());
+  EXPECT_EQ(2, convert_frames_to_signals_single_point_response.value_buffer().size());
+  EXPECT_EQ(2, convert_signals_to_frames_single_point_response.buffer().size());
+  assert_lin_frames_are_equal(frame1, &lin_1);
+  assert_lin_frames_are_equal(frame2, &lin_2);
+  EXPECT_SUCCESS(client::clear(stub(), session));
+  frames.clear();
+}
+
+TEST_F(NiXnetLINDriverApiTests, FrameStreamInputFromExample_FetchData_DataLooksReasonable)
+{
+  constexpr auto NUM_FRAMES = 2;
+  auto session = EXPECT_SUCCESS(client::create_session(stub(), "NIXNET_exampleLDF", "Cluster", "", "LIN2", CREATE_SESSION_MODE_FRAME_IN_STREAM)).session();
+  EXPECT_SUCCESS(set_property(stub(), session, PROPERTY_SESSION_INTF_LIN_NO_RESPONSE_TO_IN_STRM, true));
+
+  auto read_frame_response = EXPECT_SUCCESS(client::read_frame(stub(), session, NUM_FRAMES, 8, Protocol::PROTOCOL_LIN, TimeOut::TIME_OUT_NONE));
+  EXPECT_SUCCESS(client::clear(stub(), session));
+}
+
+TEST_F(NiXnetLINDriverApiTests, LoopbackTestFromExample_FetchData_DataLooksReasonable)
+{
+  constexpr auto NUM_SIGNALS_OUT = 2;
+  constexpr auto NUM_SIGNALS_IN = 2;
+  std::vector<f64> output_value_vtr(NUM_SIGNALS_OUT);
+  auto input_session = EXPECT_SUCCESS(client::create_session(stub(), "NIXNET_exampleLDF", "Cluster", "MasterSignal1_U16,MasterSignal2_U16", "LIN1", CREATE_SESSION_MODE_SIGNAL_IN_SINGLE_POINT)).session();
+  auto output_session = EXPECT_SUCCESS(client::create_session(stub(), "NIXNET_exampleLDF", "Cluster", "MasterSignal1_U16,MasterSignal2_U16", "LIN2", CREATE_SESSION_MODE_SIGNAL_OUT_SINGLE_POINT)).session();
+  WriteStateValue state_value;
+  state_value.set_lin_schedule_change(0);
+  EXPECT_SUCCESS(client::write_state(stub(), output_session, WRITE_STATE_LIN_SCHEDULE_CHANGE, state_value));
+  EXPECT_SUCCESS(client::start(stub(), input_session, START_STOP_SCOPE_NORMAL));
+  output_value_vtr[0] = (f64)(5 % 10);
+  output_value_vtr[1] = (f64)((5 % 10) * 10);
+  auto write_signal_single_point_response = EXPECT_SUCCESS(client::write_signal_single_point(stub(), output_session, output_value_vtr));
+
+  auto read_signal_single_point_response = EXPECT_SUCCESS(client::read_signal_single_point(stub(), input_session, NUM_SIGNALS_IN));
+  EXPECT_SUCCESS(client::clear(stub(), output_session));
+  EXPECT_SUCCESS(client::clear(stub(), input_session));
+
+  EXPECT_EQ(output_value_vtr[0], read_signal_single_point_response.value_buffer()[0]) << "LIN1 and LIN2 must be connected together.";
+  EXPECT_EQ(output_value_vtr[1], read_signal_single_point_response.value_buffer()[1]) << "LIN1 and LIN2 must be connected together.";
+}
+
+TEST_F(NiXnetLINDriverApiTests, WriteSignalXYFromExample_ReadSignalXYFromExample_SignalMatches)
+{
+  constexpr auto NUM_SAMPLES = 3;
+  constexpr auto NUM_SIGNALS = 2;
+  f64 increment_out = 0.0;
+  f64 min_out = 0.0;
+  f64 max_out = 40.0;
+  std::vector<u32> num_pairs_buffer(NUM_SIGNALS);
+  std::vector<f64> value_buffer(NUM_SAMPLES * NUM_SIGNALS);
+  auto session_for_writing = EXPECT_SUCCESS(client::create_session(stub(), "NIXNET_exampleLDF", "Cluster", "MasterSignal1_U16", "LIN1", CREATE_SESSION_MODE_SIGNAL_OUT_XY)).session();
+  WriteStateValue state_value;
+  state_value.set_lin_schedule_change(0);
+  EXPECT_SUCCESS(client::write_state(stub(), session_for_writing, WRITE_STATE_LIN_SCHEDULE_CHANGE, state_value));
+  EXPECT_SUCCESS(client::start(stub(), session_for_writing, START_STOP_SCOPE_NORMAL));
+  increment_out = (max_out - min_out) / NUM_SAMPLES;
+  for (int i = 0; i < NUM_SAMPLES; ++i) {
+    value_buffer[i] = min_out + (increment_out * i);
+    value_buffer[i + NUM_SAMPLES] = min_out + (increment_out * ((i + (NUM_SAMPLES / 2)) % NUM_SAMPLES));
+  }
+  auto write_signal_xy_response = EXPECT_SUCCESS(client::write_signal_xy(stub(), session_for_writing, 1.0, value_buffer, std::vector<u64>(), num_pairs_buffer));
+
+  auto session_for_reading = EXPECT_SUCCESS(client::create_session(stub(), "NIXNET_exampleLDF", "Cluster", "MasterSignal1_U16", "LIN2", CREATE_SESSION_MODE_SIGNAL_IN_XY)).session();
+  auto read_signal_xy_response = EXPECT_SUCCESS(client::read_signal_xy(stub(), session_for_reading, 1.0, NUM_SAMPLES, NUM_SIGNALS));
+  EXPECT_SUCCESS(client::clear(stub(), session_for_reading));
+  EXPECT_SUCCESS(client::clear(stub(), session_for_writing));
+
+  for (int i = 0; i < NUM_SAMPLES; ++i) {
+    EXPECT_EQ(value_buffer[i], read_signal_xy_response.value_buffer()[i]) << "LIN1 and LIN2 must be connected together.";
+    EXPECT_EQ(value_buffer[i + NUM_SAMPLES], read_signal_xy_response.value_buffer()[i + NUM_SAMPLES]) << "LIN1 and LIN2 must be connected together.";
+  }
+}
+
+}  // namespace
+}  // namespace system
+}  // namespace tests
+}  // namespace ni
---
+++ source/tests/system/nixnet_utilities.h
@@ -0,0 +1,196 @@
+#include <gtest/gtest.h>
+#undef interface
+#include <nixnet/nixnet_client.h>
+
+#include <string>
+#include <vector>
+
+#define EXPECT_SUCCESS(response_)    \
+  ([](const auto& response) {              \
+    EXPECT_EQ(0, response.status()); \
+    return response;                 \
+  })(response_)
+
+#define EXPECT_XNET_ERROR(error, response) \
+  if (1) {                                 \
+    EXPECT_EQ(error, (response).status()); \
+  }
+
+using namespace nixnet_grpc;
+namespace client = nixnet_grpc::experimental::client;
+namespace pb = google::protobuf;
+
+namespace nixnet_utilities {
+  
+using u32 = pb::uint32;
+
+GetPropertyResponse get_property(const client::StubPtr& stub, const nidevice_grpc::Session& session_ref, const client::simple_variant<nixnet_grpc::Property, pb::uint32>& property_id)
+{
+  ::grpc::ClientContext context;
+
+  auto request = GetPropertyRequest{};
+  request.mutable_session()->CopyFrom(session_ref);
+  const auto property_id_ptr = property_id.get_if<nixnet_grpc::Property>();
+  const auto property_id_raw_ptr = property_id.get_if<pb::uint32>();
+  if (property_id_ptr) {
+    request.set_property_id(*property_id_ptr);
+  }
+  else if (property_id_raw_ptr) {
+    request.set_property_id_raw(*property_id_raw_ptr);
+  }
+
+  auto response = GetPropertyResponse{};
+
+  client::raise_if_error(
+      stub->GetProperty(&context, request, &response));
+
+  return response;
+}
+
+inline void _set_property_value(SetPropertyRequest& request, const pb::uint32& value)
+{
+  request.set_u32_scalar(value);
+}
+
+inline void _set_property_value(SetPropertyRequest& request, const bool& value)
+{
+  request.set_bool_scalar(value);
+}
+
+inline void _set_property_value(SetPropertyRequest& request, const std::string& value)
+{
+  request.set_str(value);
+}
+
+inline void _set_property_value(SetPropertyRequest& request, const pb::uint64& value)
+{
+  request.set_u64_scalar(value);
+}
+
+inline void _set_property_value(SetPropertyRequest& request, const pb::int32& value)
+{
+  request.set_i32_scalar(value);
+}
+
+inline void _set_property_value(SetPropertyRequest& request, const double& value)
+{
+  request.set_f64_scalar(value);
+}
+
+inline void _set_property_value(SetPropertyRequest& request, const std::vector<pb::uint32>& value)
+{
+  request.mutable_u32_array()->clear_u32_array();
+  request.mutable_u32_array()->mutable_u32_array()->Add(value.begin(), value.end());
+}
+
+inline void _set_property_value(SetPropertyRequest& request, const nidevice_grpc::Session& value)
+{
+  request.mutable_db_ref()->CopyFrom(value);
+}
+
+inline void _set_property_value(SetPropertyRequest& request, const std::vector<nidevice_grpc::Session>& value)
+{
+  request.mutable_db_ref_array()->clear_db_ref();
+  request.mutable_db_ref_array()->mutable_db_ref()->Reserve(static_cast<int>(value.size()));
+  for (int i = 0; i < value.size(); ++i) {
+    request.mutable_db_ref_array()->mutable_db_ref()->at(i) = value[i];
+  }
+}
+
+inline void _set_property_value(SetPropertyRequest& request, const std::vector<EptRxFilter>& value)
+{
+  request.mutable_ept_rx_filter_array()->clear_ept_rx_filter();
+  request.mutable_ept_rx_filter_array()->mutable_ept_rx_filter()->Reserve(static_cast<int>(value.size()));
+  for (int i = 0; i < value.size(); ++i) {
+    request.mutable_ept_rx_filter_array()->mutable_ept_rx_filter()->at(i) = value[i];
+  }
+}
+
+
+template <typename T>
+SetPropertyResponse set_property(
+    const client::StubPtr& stub,
+    const nidevice_grpc::Session& session,
+    const client::simple_variant<nixnet_grpc::Property, pb::uint32>& property_id,
+    const T& value)
+{
+  ::grpc::ClientContext context;
+
+  auto request = SetPropertyRequest{};
+  request.mutable_session()->CopyFrom(session);
+  const auto property_id_ptr = property_id.get_if<nixnet_grpc::Property>();
+  const auto property_id_raw_ptr = property_id.get_if<pb::uint32>();
+  if (property_id_ptr) {
+    request.set_property_id(*property_id_ptr);
+  }
+  else if (property_id_raw_ptr) {
+    request.set_property_id_raw(*property_id_raw_ptr);
+  }
+  _set_property_value(request, value);
+
+  auto response = SetPropertyResponse{};
+
+  client::raise_if_error(
+      stub->SetProperty(&context, request, &response));
+
+  return response;
+}
+
+DbGetPropertyResponse db_get_property(const client::StubPtr& stub, const nidevice_grpc::Session& dbobject_ref, const client::simple_variant<nixnet_grpc::DBProperty, pb::uint32>& property_id)
+{
+  ::grpc::ClientContext context;
+
+  auto request = DbGetPropertyRequest{};
+  request.mutable_dbobject()->CopyFrom(dbobject_ref);
+  const auto property_id_ptr = property_id.get_if<nixnet_grpc::DBProperty>();
+  const auto property_id_raw_ptr = property_id.get_if<pb::uint32>();
+  if (property_id_ptr) {
+    request.set_property_id(*property_id_ptr);
+  }
+  else if (property_id_raw_ptr) {
+    request.set_property_id_raw(*property_id_raw_ptr);
+  }
+
+  auto response = DbGetPropertyResponse{};
+
+  client::raise_if_error(
+      stub->DbGetProperty(&context, request, &response));
+
+  return response;
+}
+
+std::vector<u32> get_property_u32_vtr(const client::StubPtr& stub, const nidevice_grpc::Session& session_ref, const client::simple_variant<nixnet_grpc::Property, pb::uint32>& property_id)
+{
+  auto array = EXPECT_SUCCESS(get_property(stub, session_ref, property_id)).u32_array().u32_array();
+  return std::vector<u32>(array.begin(), array.end());
+}
+
+std::vector<nidevice_grpc::Session> get_property_db_ref_vtr(const client::StubPtr& stub, const nidevice_grpc::Session& session_ref, const client::simple_variant<nixnet_grpc::Property, pb::uint32>& property_id)
+{
+  auto array = EXPECT_SUCCESS(get_property(stub, session_ref, property_id)).db_ref_array().db_ref();
+  return std::vector<nidevice_grpc::Session>(array.begin(), array.end());
+}
+
+std::vector<u32> db_get_property_u32_vtr(const client::StubPtr& stub, const nidevice_grpc::Session& database_ref, const client::simple_variant<nixnet_grpc::DBProperty, pb::uint32>& property_id)
+{
+  auto array = EXPECT_SUCCESS(db_get_property(stub, database_ref, property_id)).u32_array().u32_array();
+  return std::vector<u32>(array.begin(), array.end());
+}
+
+std::vector<nidevice_grpc::Session> db_get_property_db_ref_vtr(const client::StubPtr& stub, const nidevice_grpc::Session& database_ref, const client::simple_variant<nixnet_grpc::DBProperty, pb::uint32>& property_id)
+{
+  auto array = EXPECT_SUCCESS(db_get_property(stub, database_ref, property_id)).db_ref_array().db_ref();
+  return std::vector<nidevice_grpc::Session>(array.begin(), array.end());
+}
+
+int calculate_bitwise_or_of_flags(google::protobuf::RepeatedField<google::protobuf::int32> flags)
+{
+  int bitwise_or_of_flags = 0;
+  for(int i = 0; i < flags.size(); i++)
+  {
+    bitwise_or_of_flags = bitwise_or_of_flags | flags[i];
+  }
+  return bitwise_or_of_flags;
+}
+
+}  // namespace nixnet_utilities
