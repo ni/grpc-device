f0e1dd03cd8f48395bab23660cfe88f1f88cc97a
Ryan Eckenrode <77176215+reckenro@users.noreply.github.com>
2022-06-22 09:08:40 -0700

    Create script to check if Linux RT feed needs updating. (#671)
    * Create script to check if linux rt feed needs updating.
    
    * Make validate python style happy.
    
    * Put summary doc string on first line.
    
    * Blank line between summary line and description.
    
    * Rename methods.
    
    * Rework the script to be more accurate and honest.
    
    * Remove temp import.
    
    * Also ignore test changes along with codegen changes.
    
    * Simplify remaining_changes check against set.

---
+++ source/codegen/validate-linux-rt.py
@@ -0,0 +1,71 @@
+"""Script for validating if a set of changed files warrants updating the Linux RT Feed.
+
+Expects output of git diff --name-only <last-release-commit> <current-commit> 
+to be piped in so it can be read from sys.stdin
+"""
+
+import argparse
+import fnmatch
+import os
+import sys
+from typing import Set
+
+from template_helpers import load_metadata
+
+
+def _get_codegen_changes(changed_files: Set[str]) -> Set[str]:
+    return set(fnmatch.filter(changed_files, "source/codegen/*"))
+
+
+def _get_test_changes(changed_files: Set[str]) -> Set[str]:
+    return set(fnmatch.filter(changed_files, "source/tests/*"))
+
+
+def _get_non_rt_driver_changes(driver_name: str, changed_files: Set[str]) -> Set[str]:
+    files_affecting_linux_rt = [
+        f"generated/{driver_name}/*",
+        f"examples/{driver_name}/*",
+        f"source/custom/{driver_name}*",
+    ]
+    non_rt_driver_changes = set()
+
+    for pattern in files_affecting_linux_rt:
+        non_rt_driver_changes |= set(fnmatch.filter(changed_files, pattern))
+    return non_rt_driver_changes
+
+
+def _need_linux_rt_feed_update(metadata_dir: str, changed_files: Set[str]) -> bool:
+    codegen_changes = _get_codegen_changes(changed_files)
+    remaining_changes = changed_files - codegen_changes
+    test_changes = _get_test_changes(changed_files)
+    remaining_changes -= test_changes
+    non_rt_drivers = [
+        driver
+        for driver in os.listdir(metadata_dir)
+        if not load_metadata(f"{metadata_dir}/{driver}/")["config"]["linux_rt_support"]
+    ]
+    non_rt_driver_changes = set()
+    for non_rt_driver in non_rt_drivers:
+        non_rt_driver_changes |= _get_non_rt_driver_changes(non_rt_driver, remaining_changes)
+    remaining_changes = remaining_changes - non_rt_driver_changes
+    if non_rt_driver_changes and remaining_changes == {"source/CMakeLists.txt"}:
+        return False
+    return len(remaining_changes) > 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Check if any file has changed that affects the Linux RT Feed."
+    )
+    parser.add_argument(
+        "metadata",
+        help="The path to the directory containing all of the metadata.",
+    )
+    args = parser.parse_args()
+    changed_files = set()
+    for line in sys.stdin:
+        changed_files.add(line.strip())
+    if _need_linux_rt_feed_update(args.metadata, changed_files):
+        print("\nLinux RT Feed likely needs updating.")
+    else:
+        print("\nLinux RT Feed should not need updating.")
