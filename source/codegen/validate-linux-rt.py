344d6e715509205ba4b08bbd9fc5d5f248426b1e
Ryan Eckenrode <ryan.eckenrode@ni.com>
2022-06-22 17:01:38 -0500

    Update the validation script to output more info on changed files.

--- source/codegen/validate-linux-rt.py
+++ source/codegen/validate-linux-rt.py
@@ -9,6 +9,7 @@ import fnmatch
 import os
 import sys
 from typing import Set
+from pprint import pprint
 
 from template_helpers import load_metadata
 
@@ -36,9 +37,12 @@ def _get_non_rt_driver_changes(driver_name: str, changed_files: Set[str]) -> Set
 
 def _need_linux_rt_feed_update(metadata_dir: str, changed_files: Set[str]) -> bool:
     codegen_changes = _get_codegen_changes(changed_files)
+    _print_changed_files("Codegen changes not affecting RT:", codegen_changes)
     remaining_changes = changed_files - codegen_changes
     test_changes = _get_test_changes(changed_files)
+    _print_changed_files("Test related changes not affecting RT:", test_changes)
     remaining_changes -= test_changes
+
     non_rt_drivers = [
         driver
         for driver in os.listdir(metadata_dir)
@@ -47,10 +51,19 @@ def _need_linux_rt_feed_update(metadata_dir: str, changed_files: Set[str]) -> bo
     non_rt_driver_changes = set()
     for non_rt_driver in non_rt_drivers:
         non_rt_driver_changes |= _get_non_rt_driver_changes(non_rt_driver, remaining_changes)
+    _print_changed_files("Changes related to drivers not supported on RT:", non_rt_driver_changes)
     remaining_changes = remaining_changes - non_rt_driver_changes
     if non_rt_driver_changes and remaining_changes == {"source/CMakeLists.txt"}:
         return False
-    return len(remaining_changes) > 0
+    if len(remaining_changes) > 0:
+        _print_changed_files("Remaining changes likely affecting RT:", remaining_changes)
+        return True
+    return False
+
+
+def _print_changed_files(title: str, changed_files: Set[str]):
+    print(f"\n{title}\n")
+    pprint(changed_files)
 
 
 if __name__ == "__main__":
