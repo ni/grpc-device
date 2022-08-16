771c6bddba14b16f72b95614d23a4eda90a85e61
Ryan Eckenrode <77176215+reckenro@users.noreply.github.com>
2022-08-16 09:13:56 -0700

    Generate some old hand-written methods. (#703)
    * Generate two ivi-dance-with-a-twist ni-scope methods.
    
    * Generate fgen method using lengths of inputs.
    
    * Generate ivi-dance method in ni-tclk.
    
    * Generate ni sync method
    
    * Don't need the 'include_in_proto' tag I added.
    
    * Revert "Generate ni sync method"
    
    This reverts commit c001b5e7b7324b68434e6ccb1d7f5b50a6d68398.
    
    * Fix discrepancy in generated nitclk method.
    
    * Revert "Generate fgen method using lengths of inputs."
    
    This reverts commit 06861981d2ca42a8d2496f522c3daf0333dd91bb.
    
    * Revert format change in niscope_service.custom.cpp

--- source/custom/nitclk_service.custom.cpp
+++ source/custom/nitclk_service.custom.cpp
@@ -5,45 +5,6 @@ namespace nitclk_grpc {
 const auto kErrorReadBufferTooSmall = -200229;
 const auto kWarningCAPIStringTruncatedToFitBuffer = 200026;
 
-//---------------------------------------------------------------------
-//---------------------------------------------------------------------
-::grpc::Status NiTClkService::GetAttributeViString(::grpc::ServerContext* context, const GetAttributeViStringRequest* request, GetAttributeViStringResponse* response)
-  {
-    if (context->IsCancelled()) {
-      return ::grpc::Status::CANCELLED;
-    }
-    try {
-      auto session_grpc_session = request->session();
-      ViSession session = session_repository_->access_session(session_grpc_session.id(), session_grpc_session.name());
-      ViConstString channel_name = request->channel_name().c_str();
-      ViAttr attribute_id = request->attribute_id();
-
-      while (true) {
-        auto status = library_->GetAttributeViString(session, channel_name, attribute_id, 0, nullptr);
-        if (status < VI_SUCCESS) {
-          return ConvertApiErrorStatusForViSession(status, session);
-        }
-        ViInt32 buffer_size = status;
-
-        std::string value(buffer_size, '\0');
-        status = library_->GetAttributeViString(session, channel_name, attribute_id, buffer_size, (ViChar*)value.data());
-        if (status == kErrorReadBufferTooSmall || status == kWarningCAPIStringTruncatedToFitBuffer) {
-          // buffer is now too small, try again
-          continue;
-        }
-        if (status < VI_SUCCESS) {
-          return ConvertApiErrorStatusForViSession(status, session);
-        }
-        response->set_status(status);
-        response->set_value(value);
-        return ::grpc::Status::OK;
-      }
-    }
-    catch (nidevice_grpc::LibraryLoadException& ex) {
-      return ::grpc::Status(::grpc::NOT_FOUND, ex.what());
-    }
-  }
-
 ::grpc::Status NiTClkService::ConvertApiErrorStatusForViSession(google::protobuf::int32 status, ViSession session_number)
 {
     std::string description(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
