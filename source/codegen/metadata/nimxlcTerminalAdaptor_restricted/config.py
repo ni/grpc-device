5da77ad0a3cb6171a9af62428c497bfadac26c9f
Ariane Chan-You <ariane.chan-you@ni.com>
2022-08-18 20:26:37 -0500

    Add headers and try to fix codegen.

--- source/codegen/metadata/nimxlcTerminalAdaptor_restricted/config.py
+++ source/codegen/metadata/nimxlcTerminalAdaptor_restricted/config.py
@@ -2,74 +2,39 @@
 config = {
     "code_readiness": "RestrictedNextRelease",
     'api_version': '21.0.0',
-    'c_header': 'MxlcTerminalAdaptor.h',
+    'c_header': 'TerminalAdaptorTypes.h',
     'c_function_prefix': 'nimxlc_ta_nimxlc_',
     'service_class_prefix': 'NimxlcTerminalAdaptorRestricted',
     'java_package': 'com.ni.grpc.nimxlcterminaladaptorrestricted',
     'csharp_namespace': 'NationalInstruments.Grpc.NimxlcTerminalAdaptorRestricted',
     'namespace_component': 'nimxlcterminaladaptor_restricted',
-    'close_function': None,
-    'custom_types': [
-        {
-            'name': 'nierr_Status',
-            'grpc_name': 'nierrStatus',
-            'fields': [
-                {
-                    'type': 'int32',
-                    'name': 'code'
-                },
-                {
-                    'type': 'uint32',
-                    'name': 'capacity'
-                },
-                {
-                    'type': 'nierr_ReallocJson*',
-                    'name': 'reallocJson'
-                },
-                {
-                    'type': 'char*',
-                    'name': 'json'
-                }
-            ]
-        },
-        {
-            'name': 'nimxlc_DeviceContainer',
-            'grpc_name': 'DeviceContainer',
-            'fields':[]
-        },
-        {
-            'name': 'nimxlc_DeviceIterator',
-            'grpc_name': 'DeviceIterator',
-            'fields':[]
-        },
-        {
-            'name': 'nimxlc_TerminalContainer',
-            'grpc_name': 'TerminalContainer',
-            'fields':[]
-        },
-        {
-            'name': 'nimxlc_TerminalIterator',
-            'grpc_name': 'TerminalIterator',
-            'fields':[]
-        }
-    ],
+    'close_function': 'destroySession',
+    'additional_headers': { 
+        "custom/nimxlcTerminalAdaptor_restricted_converters.h": ["service.cpp"],
+    },
+    'custom_types': [],
     "type_to_grpc_type": {
-        "char[]": "string",
-        "float32": "float",
-        "float64": "double",
-        "int16": "int32",
-        "int32": "int32",
-        "int64": "int64",
-        "nimxlc_Session": "nidevice_grpc.Session",
-        "int8": "int32",
-        "uInt16": "uint32",
-        "uInt32": "uint32",
-        "uInt64": "uint64",
-        "uInt8": "uint32",
-        "uInt8[]": "bytes",
-        "NIComplexSingle": "nidevice_grpc.NIComplexNumberF32",
-        "NIComplexDouble": "nidevice_grpc.NIComplexNumber",
-        "nierr_ReallocJson": 'void'
+        'char[]': 'string',
+        'float32': 'float',
+        'float64': 'double',
+        'int16': 'int32',
+        'int32': 'int32',
+        'int64': 'int64',
+        'nimxlc_Session': 'nidevice_grpc.Session',
+        'int8': 'int32',
+        'uInt16': 'uint32',
+        'uInt32': 'uint32',
+        'uint32_t': 'uint32',
+        'uInt64': 'uint64',
+        'uInt8': 'uint32',
+        'uInt8[]': 'bytes',
+        'NIComplexSingle': 'nidevice_grpc.NIComplexNumberF32',
+        'NIComplexDouble': 'nidevice_grpc.NIComplexNumber',
+        'nierr_Status': 'nierrStatus',
+        'nimxlc_DeviceContainer': 'bytes',
+        'nimxlc_DeviceIterator': 'bytes',
+        'nimxlc_TerminalContainer': 'bytes',
+        'nimxlc_TerminalIterator': 'bytes'
     },
     'driver_name': 'NI-MXLCTERMINALADAPTOR-RESTRICTED',
     "resource_handle_type": "nimxlc_Session",
@@ -78,7 +43,7 @@ config = {
     'library_info': {
         'Linux': {
             '64bit': {
-                'name': 'nimxlcterminaladaptor',
+                'name': 'libnimxlcTerminalAdaptor',
                 'type': 'cdll',
                 'abi_version': '1'
             }
