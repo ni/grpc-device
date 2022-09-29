5cd301a1680a74b5e8812856473a3cefa7b8d0ee
Ryan Eckenrode <ryan.eckenrode@ni.com>
2022-09-29 16:38:04 -0500

    Get black and linting working again locally.

--- source/codegen/validate_imported_protos.py
+++ source/codegen/validate_imported_protos.py
@@ -5,29 +5,34 @@ Expects import path as source/codegen/metadata and generated folder path.
 Background: 
 All MI driver metadata is being imported from hapigen. We're also creating .proto files
 based on the metadata in that component. So when an MI driver is updated, the export will have
-a metadata folder with [attributes.py, config.py, enums.py, functions.py, <driver-name>.proto]. Someone
-will import those five files to grpc-device's source/codegen/metadata folder.
+a metadata folder with [attributes.py, config.py, enums.py, functions.py, <driver-name>.proto].
+Someone will import those five files to grpc-device's source/codegen/metadata folder.
 
 Purpose: 
-Since, at the moment, we have both grpc-device and hapigen generating the driver .proto files we want a way to validate
-that they're creating equivalent ones. This script will check imported .proto files (copied out of hapigen-based driver exports)
-against their corresponding .proto file that gets generated in grpc-device to make sure they match.
+Since, at the moment, we have both grpc-device and hapigen generating the driver .proto files 
+we want a way to validate that they're creating equivalent ones. This script will check imported
+.proto files (copied out of hapigen-based driver exports) against their corresponding .proto file
+that gets generated in grpc-device to make sure they match.
 
 On Mismatch:
 1. Make sure .proto from driver export is copied over to grpc-device
-2. Make sure you built grpc-device locally to get any generated folder changes (specifically .proto files there)
-3. The proto generators (mako and helper python files) likely changed between grpc-device and hapigen and need to be synced up.
+2. Make sure you built grpc-device locally to get any generated folder changes
+   (specifically .proto files there)
+3. The proto generators (mako and helper python files) likely changed between grpc-device and
+   hapigen and need to be synced up.
 """
 
-import click
 import filecmp
 import glob
 import os
 import sys
 
+import click
+
+
 @click.command()
-@click.option('--imported', '-i', help="Directory where imported .proto files reside")
-@click.option('--generated', '-g', help='Directory where generated .proto files reside')
+@click.option("--imported", "-i", help="Directory where imported .proto files reside")
+@click.option("--generated", "-g", help="Directory where generated .proto files reside")
 def _check_mismatched_protos(imported: str, generated: str):
     any_mismatches = False
     file_paths = glob.glob(f"{imported}/**/*.proto", recursive=True)
