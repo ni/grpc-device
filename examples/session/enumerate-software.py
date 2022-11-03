11a0819dfb9c65b7f6fb978ed04982739cc4e165
KC Hawes-Domingue 🦜 <kc.hawesdomingue@ni.com>
2022-11-03 15:19:30 -0500

    Add --show_hidden argument to enumerate-software example

--- examples/session/enumerate-software.py
+++ examples/session/enumerate-software.py
@@ -17,20 +17,23 @@ If they are not passed in as command line arguments, then by default the server 
 "localhost:31763".
 """
 
-import sys
+import argparse
 
 import grpc
 import session_pb2 as session_types
 import session_pb2_grpc as grpc_session
 
-SERVER_ADDRESS = "localhost"
-SERVER_PORT = "31763"
+# Parser for command-line options
+parser = argparse.ArgumentParser(description='Lists out NI software installed on the server machine.')
+parser.add_argument("server_address", help="The IP address or machine name of the server. The default is localhost.", nargs='?', default="localhost")
+parser.add_argument("port_number", help="The port number of the server. The default is 31763.", nargs='?', default="31763")
+parser.add_argument("--show-hidden", help="Include hidden packages in the list of software.", action="store_true", default=False)
+args = parser.parse_args()
 
 # Read in cmd args
-if len(sys.argv) >= 2:
-    SERVER_ADDRESS = sys.argv[1]
-if len(sys.argv) >= 3:
-    SERVER_PORT = sys.argv[2]
+SERVER_ADDRESS = args.server_address
+SERVER_PORT = args.port_number
+SHOW_HIDDEN_PACKAGES = args.show_hidden
 
 # Open communication with the server using gRPC APIs.
 channel = grpc.insecure_channel(f"{SERVER_ADDRESS}:{SERVER_PORT}")
@@ -52,12 +55,15 @@ def print_software(software):
     for package in software:
         print(f"    Package ID: {package.package_id}")
         print(f"    Product Name: {package.product_name}")
-        print(f"    Version: {package.version} \n")
+        print(f"    Version: {package.package_version} \n")
 
 
 try:
     # The EnumerateInstalledSoftware API gives a list of NI packages installed on the server machine.
-    enumerate_software_response = client.EnumerateInstalledSoftware(session_types.EnumerateInstalledSoftwareRequest())
+    if (SHOW_HIDDEN_PACKAGES):
+        enumerate_software_response = client.EnumerateInstalledSoftwareIncludeHiddenPackages(session_types.EnumerateInstalledSoftwareIncludeHiddenPackagesRequest())
+    else:
+        enumerate_software_response = client.EnumerateInstalledSoftware(session_types.EnumerateInstalledSoftwareRequest())
 
     # Display software installed on the server machine.
     print_software(enumerate_software_response.software)
