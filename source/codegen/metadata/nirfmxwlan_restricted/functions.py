ccf86959ba7aa8d37228a26b79b98d330d28a8cc
Wesley Tang <wesley.tang@ni.com>
2023-01-05 12:42:22 -0600

    Initial changes for nirfmxwlan_restricted

---
+++ source/codegen/metadata/nirfmxwlan_restricted/functions.py
@@ -0,0 +1,164 @@
+functions = {
+    'BuildResultString': {
+        'parameters': [
+            {
+                'direction': 'in',
+                'name': 'selectorString',
+                'type': 'char[]'
+            },
+            {
+                'direction': 'in',
+                'name': 'resultNumber',
+                'type': 'int32'
+            },
+            {
+                'direction': 'in',
+                'name': 'selectorStringOutLength',
+                'type': 'int32'
+            },
+            {
+                'direction': 'out',
+                'name': 'selectorStringOut',
+                'size': {
+                    'mechanism': 'ivi-dance',
+                    'value': 'selectorStringOutLength'
+                },
+                'type': 'char[]'
+            }
+        ],
+        'returns': 'int32'
+    },
+    'GetChannelList': {
+        'parameters': [
+            {
+                'direction': 'in',
+                'grpc_name': 'instrument',
+                'name': 'instrumentHandle',
+                'type': 'niRFmxInstrHandle'
+            },
+            {
+                'direction': 'in',
+                'name': 'selectorString',
+                'type': 'char[]'
+            },
+            {
+                'direction': 'in',
+                'name': 'WLANBand',
+                'type': 'int32'
+            },
+            {
+                'direction': 'out',
+                'name': 'centerFrequencies',
+                'size': {
+                    'mechanism': 'ivi-dance-with-a-twist',
+                    'value': 'arraySize',
+                    'value_twist': 'actualArraySize'
+                },
+                'type': 'float64[]'
+            },
+            {
+                'direction': 'out',
+                'name': 'channelBandwidths',
+                'size': {
+                    'mechanism': 'ivi-dance-with-a-twist',
+                    'value': 'arraySize',
+                    'value_twist': 'actualArraySize'
+                },
+                'type': 'float64[]'
+            },
+            {
+                'direction': 'in',
+                'name': 'arraySize',
+                'type': 'int32'
+            },
+            {
+                'direction': 'out',
+                'name': 'actualArraySize',
+                'type': 'int32'
+            },
+        ],
+        'returns': 'int32'
+    },
+    'OFDMModAccFetchCommonPilotErrorTraceIndB': {
+        'parameters': [
+            {
+                'direction': 'in',
+                'grpc_name': 'instrument',
+                'name': 'instrumentHandle',
+                'type': 'niRFmxInstrHandle'
+            },
+            {
+                'direction': 'in',
+                'name': 'selectorString',
+                'type': 'char[]'
+            },
+            {
+                'direction': 'in',
+                'name': 'timeout',
+                'type': 'float64'
+            },
+            {
+                'direction': 'out',
+                'name': 'x0',
+                'type': 'float64'
+            },
+            {
+                'direction': 'out',
+                'name': 'dx',
+                'type': 'float64'
+            },
+            {
+                'direction': 'out',
+                'name': 'commonPilotErrorMagnitude',
+                'size': {
+                    'mechanism': 'ivi-dance-with-a-twist',
+                    'value': 'arraySize',
+                    'value_twist': 'actualArraySize'
+                },
+                'type': 'float32[]'
+            },
+            {
+                'direction': 'out',
+                'name': 'commonPilotErrorPhase',
+                'size': {
+                    'mechanism': 'ivi-dance-with-a-twist',
+                    'value': 'arraySize',
+                    'value_twist': 'actualArraySize'
+                },
+                'type': 'float32[]'
+            },
+            {
+                'direction': 'in',
+                'name': 'arraySize',
+                'type': 'int32'
+            },
+            {
+                'direction': 'out',
+                'name': 'actualArraySize',
+                'type': 'int32'
+            }
+        ],
+        'returns': 'int32'
+    },
+    'OFDMModAccCNoiseCalibrate': {
+        'parameters': [
+            {
+                'direction': 'in',
+                'grpc_name': 'instrument',
+                'name': 'instrumentHandle',
+                'type': 'niRFmxInstrHandle'
+            },
+            {
+                'direction': 'in',
+                'name': 'selectorString',
+                'type': 'char[]'
+            },
+            {
+                'direction': 'in',
+                'name': 'sharedLOConnection',
+                'type': 'int32'
+            },
+        ],
+        'returns': 'int32'
+    }
+}
---
+++ source/custom/nirfmxwlan_restricted_service.custom.cpp
@@ -0,0 +1,18 @@
+#include <nirfmxwlan_restricted/nirfmxwlan_restricted_service.h>
+
+namespace nirfmxwlan_restricted_grpc {
+
+::grpc::Status NiRFmxWLANRestrictedService::ConvertApiErrorStatusForNiRFmxInstrHandle(::grpc::ServerContext* context, int32_t status, niRFmxInstrHandle instrumentHandle)
+{
+  ViStatus error_code{};
+  std::string description(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
+  // Try first to get the most recent error with a dynamic message.
+  library_->GetError(instrumentHandle, &error_code, nidevice_grpc::kMaxGrpcErrorDescriptionSize, &description[0]);
+  if (error_code != status) {
+    // Since another thread has changed the status, fall back to the static message lookup.
+    library_->GetErrorString(instrumentHandle, status, nidevice_grpc::kMaxGrpcErrorDescriptionSize, &description[0]);
+  }
+  return nidevice_grpc::ApiErrorAndDescriptionToStatus(context, status, description);
+}
+
+}  // namespace nirfmxwlan_restricted_grpc
