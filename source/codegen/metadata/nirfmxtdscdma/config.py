94afa993b7f08711c4051be28a297de502fda5a8
Phil Hindman <77023184+phindman@users.noreply.github.com>
2023-02-01 16:40:21 -0600

    Add source and imports files for RFmxTDSCDMA

---
+++ source/codegen/metadata/nirfmxtdscdma/config.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+config = {
+    'api_version': '23.0.0',
+    'c_header': 'niRFmxTDSCDMA.h',
+    'c_function_prefix': 'RFmxTDSCDMA_',
+    'service_class_prefix': 'NiRFmxTDSCDMA',
+    'java_package': 'com.ni.grpc.nirfmxtdscdma',
+    'csharp_namespace': 'NationalInstruments.Grpc.NiRFmxTDSCDMA',
+    'namespace_component': 'nirfmxtdscdma',
+    'close_function': 'Close',
+    'custom_types': [],
+    'additional_headers': {},
+    'type_to_grpc_type': {
+        "char[]": "string",
+        "float32": "float",
+        "float64": "double",
+        "int16": "int32",
+        "int32": "int32",
+        "int64": "int64",
+        "niRFmxInstrHandle": "nidevice_grpc.Session",
+        "int8": "int32",
+        "uInt16": "uint32",
+        "uInt32": "uint32",
+        "uInt64": "uint64",
+        "uInt8": "uint32",
+        "uInt8[]": "bytes",
+        "NIComplexSingle": "nidevice_grpc.NIComplexNumberF32",
+        "NIComplexDouble": "nidevice_grpc.NIComplexNumber",
+    },
+    'code_readiness': 'NextRelease',
+    'feature_toggles': {},
+    'driver_name': 'NI-rfmxtdscdma',
+    'extra_errors_used': [
+    ],
+    'init_function': 'Initialize',
+    'resource_handle_type': 'niRFmxInstrHandle',
+    'status_ok': 'status >= 0',
+    'windows_only': True,
+    'library_info': {
+        'Linux': {
+            '64bit': {
+                'name': 'nirfmxtdscdma',
+                'type': 'cdll',
+                'abi_version': '1'
+            }
+        },
+        'Windows': {
+            '32bit': {
+                'name': 'niRFmxTDSCDMA.dll',
+                'type': 'windll'
+            },
+            '64bit': {
+                'name': 'niRFmxTDSCDMA.dll',
+                'type': 'cdll'
+            }
+        }
+    },
+    'metadata_version': '0.1',
+    'module_name': 'nirfmxtdscdma',
+    'session_class_description': 'An NI-RFmxTDSCDMA instrument handle',
+    'session_handle_parameter_name': 'instrumentHandle',
+    'duplicate_resource_handles_allowed': True
+}
