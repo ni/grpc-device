888d28ec40e24341c1968e9deddf4901c236ab5c
Phil Hindman <77023184+phindman@users.noreply.github.com>
2023-02-02 10:49:23 -0600

    PR feedback: Update api_version
    Signed-off-by: Phil Hindman <77023184+phindman@users.noreply.github.com>

--- source/codegen/metadata/nirfmxgsm/config.py
+++ source/codegen/metadata/nirfmxgsm/config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 config = {
-    'api_version': '21.0.0',
+    'api_version': '23.0.0',
     'c_header': 'niRFmxGSM.h',
     'c_function_prefix': 'RFmxGSM_',
     'service_class_prefix': 'NiRFmxGSM',
