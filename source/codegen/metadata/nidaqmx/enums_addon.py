2375cedee7c307a27bc41d3c2b131e1b8998773a
Vikram Avudaiappan <vikram.avudaiappan@ni.com>
2023-03-14 00:15:29 +0530

    Add comments for validation suppression in enums for DAQmx metadata

--- source/codegen/metadata/nidaqmx/enums_addon.py
+++ source/codegen/metadata/nidaqmx/enums_addon.py
@@ -1,10 +1,9 @@
 # These dictionaries are applied to the generated enums dictionary at build time
 # Any changes to the API should be made here. enums.py is code generated
 
-enums_override_metadata = {
-}
+enums_override_metadata = {}
 
 enums_validation_suppressions = {
-    "DAQmxErrors": ["ENUMS_SHOULD_BE_USED"],
-    "DAQmxWarnings": ["ENUMS_SHOULD_BE_USED"]
+    "DAQmxErrors": ["ENUMS_SHOULD_BE_USED"],  # Used by nidaqmx-python
+    "DAQmxWarnings": ["ENUMS_SHOULD_BE_USED"],  # Used by nidaqmx-python
 }
