1d0cecd7a9b56314c512799469a592fe573b02cf
Gerardo Orozco <gorozco@ni.com>
2023-04-14 17:44:36 -0500

    Black with 96 line lenght

--- examples/nirf/specan-nr-txp-example.py
+++ examples/nirf/specan-nr-txp-example.py
@@ -326,16 +326,14 @@ except grpc.RpcError as rpc_error:
     error_message = rpc_error.details()
     for entry in rpc_error.trailing_metadata() or []:
         if entry.key == "ni-error":
-            value = (
-                entry.value
-                if isinstance(entry.value, str)
-                else entry.value.decode("utf-8")
-            )
+            value = entry.value if isinstance(entry.value, str) else entry.value.decode("utf-8")
             error_message += f"\nError status: {value}"
     if rpc_error.code() == grpc.StatusCode.UNAVAILABLE:
         error_message = f"Failed to connect to server on {SERVER_ADDRESS}:{SERVER_PORT}"
     elif rpc_error.code() == grpc.StatusCode.UNIMPLEMENTED:
-        error_message = "The operation is not implemented or is not supported/enabled in this service"
+        error_message = (
+            "The operation is not implemented or is not supported/enabled in this service"
+        )
     sys.stderr.write(f"{error_message}\n")
 finally:
     if instr:
