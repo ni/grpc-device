aa44ba91ca52d3cf3257c66ece02d395bb58360b
Jay Fitzgerald <34140133+ni-jfitzger@users.noreply.github.com>
2023-07-26 13:51:01 -0500

    Fix mock_shared_library mock methods

--- source/server/mock_shared_library.h
+++ source/server/mock_shared_library.h
@@ -17,15 +17,15 @@ namespace unit {
 
 class MockSharedLibrary : public nidevice_grpc::SharedLibrary {
  public:
-  MOCK_METHOD(void, swap, (SharedLibrary& other), (override));
-  MOCK_METHOD(bool, is_loaded, (), (const, override));
-  MOCK_METHOD(nidevice_grpc::LibraryHandle, get_handle, (), (const, override));
-  MOCK_METHOD(void, load, (), (override));
-  MOCK_METHOD(void, unload, (), (override));
-  MOCK_METHOD(const void*, get_function_pointer, (const char* name), (const, override));
-  MOCK_METHOD(bool, function_exists, (const char* name), (const, override));
-  MOCK_METHOD(void, set_library_name, (const char* library_name), (override));
-  MOCK_METHOD(std::string, get_library_name, (), (const, override));
+  MOCK_METHOD(void, swap, (nidevice_grpc::SharedLibrary& other), ());
+  MOCK_METHOD(bool, is_loaded, (), (const));
+  MOCK_METHOD(nidevice_grpc::LibraryHandle, get_handle, (), (const));
+  MOCK_METHOD(void, load, (), ());
+  MOCK_METHOD(void, unload, (), ());
+  MOCK_METHOD(const void*, get_function_pointer, (const char* name), (const));
+  MOCK_METHOD(bool, function_exists, (const char* name), (const));
+  MOCK_METHOD(void, set_library_name, (const char* library_name), ());
+  MOCK_METHOD(std::string, get_library_name, (), (const));
 };
 
 }  // namespace unit
