a3791ca4b08cd87e951f91c05a3620421316108d
Dan Mondrik <dan.mondrik@ni.com>
2023-08-25 13:48:08 -0500

    Fix VISA error lookup signature to match

--- source/custom/visa_service.custom.cpp
+++ source/custom/visa_service.custom.cpp
@@ -2,7 +2,14 @@
 
 namespace visa_grpc {
 
-::grpc::Status VisaService::ConvertApiErrorStatusForNxSOCKET(::grpc::ServerContextBase* context, int32_t status, ViSession vi)
+::grpc::Status VisaService::ConvertApiErrorStatusForViSession(::grpc::ServerContextBase* context, int32_t status, ViSession vi)
+{
+  std::string description(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
+  library_->GetStatusDescription(vi, &description[0]);
+  return nidevice_grpc::ApiErrorAndDescriptionToStatus(context, status, description);
+}
+
+::grpc::Status VisaService::ConvertApiErrorStatusForViObject(::grpc::ServerContextBase* context, int32_t status, ViObject vi)
 {
   std::string description(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
   library_->GetStatusDescription(vi, &description[0]);
