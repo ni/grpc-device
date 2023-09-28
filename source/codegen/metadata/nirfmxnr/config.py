c74771744da4ad1ac5839f8a5534c29c148d7782
alenkani <ashutosh.lenka@ni.com>
2023-09-28 12:11:08 +0530

    version number changes

--- source/codegen/metadata/nirfmxnr/config.py
+++ source/codegen/metadata/nirfmxnr/config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 config = {
-    'api_version': '23.5.0',
+    'api_version': '23.8.0',
     'c_header': 'niRFmxNR.h',
     'c_function_prefix': 'RFmxNR_',
     'service_class_prefix': 'NiRFmxNR',
