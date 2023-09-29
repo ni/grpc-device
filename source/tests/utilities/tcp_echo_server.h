d1f8d3d6773ce673b0d0c01ae2db8ad7fe6ff345
Daniel Hua <daniel.hua@ni.com>
2023-09-29 14:00:50 -0500

    Add tcp_echo_server

---
+++ source/tests/utilities/tcp_echo_server.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <memory>
+#include <cstring>
+#ifdef _WIN32
+#include <winsock2.h>
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#endif
+
+#ifndef _WIN32
+typedef int SOCKET;
+#endif
+
+class TcpEchoSession : public std::enable_shared_from_this<TcpEchoSession> {
+public:
+    TcpEchoSession(SOCKET socket_fd)
+    : socket_fd_(socket_fd)
+    {
+    }
+
+    void start() {
+        do_read();
+    }
+
+private:
+    void do_read() {
+        auto self(shared_from_this());
+        char buffer[max_length] = {0};
+        int n = recv(socket_fd_, buffer, max_length, 0);
+        if (n > 0) {
+            do_write(buffer, n);
+        }
+    }
+
+    void do_write(const char* data, int length) {
+        auto self(shared_from_this());
+        int n = send(socket_fd_, data, length, 0);
+        if (n > 0) {
+            do_read();
+        }
+    }
+
+    static const int max_length = 256;
+    SOCKET socket_fd_;
+};
