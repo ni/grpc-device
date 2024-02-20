17852a46e6f5d89fb7dfc91a2ef53b0df0bba043
Ryan Eckenrode <ryan.eckenrode@ni.com>
2024-02-20 10:55:37 -0600

    Fix runtime environment test failing in some situations.

--- source/tests/unit/library_set_runtime_environment_tests.cpp
+++ source/tests/unit/library_set_runtime_environment_tests.cpp
@@ -2,6 +2,7 @@
 // Use NI-SWITCH instead of NI-FAKE because nifake.h is missing a bunch of functions specified in metadata
 // and they're difficult to properly add in source.
 #include <niswitch/niswitch_library.h>
+
 #include "mock_shared_library.h"
 #include "version.h"
 
@@ -20,15 +21,15 @@ TEST(LibrarySetRuntimeEnvironmentTests, Library_SetRuntimeEnvironmentFound_Calls
   EXPECT_CALL(*p_shared_library, set_library_name(_));
   EXPECT_CALL(*p_shared_library, load());
   EXPECT_CALL(*p_shared_library, is_loaded())
-    .WillOnce(Return(true)); // the constructor will bail out early if false
+      .WillOnce(Return(true));  // the constructor will bail out early if false
 
   const char* func_name = "niSwitch_SetRuntimeEnvironment";
 
   EXPECT_CALL(*p_shared_library, get_function_pointer(_))
-    .WillRepeatedly(Return(nullptr));
+      .WillRepeatedly(Return(nullptr));
   // We must return an address for SetRuntimeEnvironment or we won't call it.
-  EXPECT_CALL(*p_shared_library, get_function_pointer(func_name))
-    .WillOnce(Return(const_cast<const void*>(reinterpret_cast<void*>(&FunctionToPointAt))));
+  EXPECT_CALL(*p_shared_library, get_function_pointer(::testing::StrEq(func_name)))
+      .WillOnce(Return(const_cast<const void*>(reinterpret_cast<void*>(&FunctionToPointAt))));
 
   auto library = std::make_shared<niswitch_grpc::NiSwitchLibrary>(p_shared_library);
   EXPECT_TRUE(library->is_runtime_environment_set());
@@ -40,9 +41,9 @@ TEST(LibrarySetRuntimeEnvironmentTests, Library_SetRuntimeEnvironmentNotFound_Do
   EXPECT_CALL(*p_shared_library, set_library_name(_));
   EXPECT_CALL(*p_shared_library, load());
   EXPECT_CALL(*p_shared_library, is_loaded())
-    .WillOnce(Return(true)); // the constructor will bail out early if false
+      .WillOnce(Return(true));  // the constructor will bail out early if false
   EXPECT_CALL(*p_shared_library, get_function_pointer(_))
-    .WillRepeatedly(Return(nullptr)); // means a function wasn't found
+      .WillRepeatedly(Return(nullptr));  // means a function wasn't found
 
   auto library = std::make_shared<niswitch_grpc::NiSwitchLibrary>(p_shared_library);
   EXPECT_FALSE(library->is_runtime_environment_set());
