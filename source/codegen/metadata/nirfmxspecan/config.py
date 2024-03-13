4662e3eedc9319c02c7466407d143425a566cfb8
rnagaraj <rahul.nagaraju@ni.com>
2024-03-13 14:09:58 +0530

    Bumping the version for SpecAn personality to 24.3.0

--- source/codegen/metadata/nirfmxspecan/config.py
+++ source/codegen/metadata/nirfmxspecan/config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 config = {
-    'api_version': '23.8.0',
+    'api_version': '24.3.0',
     'c_header': 'niRFmxSpecAn.h',
     'c_function_prefix': 'RFmxSpecAn_',
     'service_class_prefix': 'NiRFmxSpecAn',
