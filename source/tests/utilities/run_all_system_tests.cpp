d5de10dc33e121a6ac5f5d55b0ea890c4af00705
Maxx Boehme <maxx.boehme@ni.com>
2024-07-02 14:46:11 -0500

    Add more debugging logs

--- source/tests/utilities/run_all_system_tests.cpp
+++ source/tests/utilities/run_all_system_tests.cpp
@@ -1,8 +1,13 @@
 #include <gtest/gtest.h>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
-  // setlocale(LC_ALL, "");
+  std::cout << "Before setlocale" << std::endl;
+  setlocale(LC_ALL, "");
+  std::cout << "After setlocale" << std::endl;
   ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  const int run_result = RUN_ALL_TESTS();
+  std::cout << "Run_result: " << run_result << std::endl;
+  return run_result; 
 }
