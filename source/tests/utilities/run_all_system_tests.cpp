28cc450a4ba0173c05261ae3b0977e38e66bcba6
Maxx Boehme <maxx.boehme@ni.com>
2024-07-26 14:40:20 -0500

    Add in a basic google test

--- source/tests/utilities/run_all_system_tests.cpp
+++ source/tests/utilities/run_all_system_tests.cpp
@@ -1,4 +1,4 @@
-// #include <gtest/gtest.h>
+#include <gtest/gtest.h>
 #include <iostream>
 
 int main(int argc, char** argv)
@@ -6,8 +6,13 @@ int main(int argc, char** argv)
   std::cout << "Before setlocale" << std::endl;
   // setlocale(LC_ALL, "");
   std::cout << "After setlocale" << std::endl;
-  // ::testing::InitGoogleTest(&argc, argv);
-  // const int run_result = RUN_ALL_TESTS();
-  // std::cout << "Run_result: " << run_result << std::endl;
-  return 0;
+  ::testing::InitGoogleTest(&argc, argv);
+  const int run_result = RUN_ALL_TESTS();
+  std::cout << "Run_result: " << run_result << std::endl;
+  return run_result;
+}
+
+TEST(TrivialTest, Add) {
+  const int result = 5 * 5;
+  EXPECT_EQ(25, result);
 }
