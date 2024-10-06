486332119caf7ac2392d2c08a6a6ea44af61efe0
sagrahar <sumit.agrahari@ni.com>
2024-10-06 23:06:59 +0530

    building only server code with only daqmx

--- source/codegen/generate_shared_service_files.py
+++ source/codegen/generate_shared_service_files.py
@@ -10,7 +10,7 @@ def _generate_register_all_services(metadata_dir: Path, output_dir: Path) -> Non
     driver_modules = [
         load_metadata(p)
         for p in sorted(metadata_dir.iterdir())
-        if p.is_dir() and "fake" not in p.name
+        if p.is_dir() and "daqmx" in p.name
     ]
 
     template = instantiate_mako_template("register_all_services.h.mako")
