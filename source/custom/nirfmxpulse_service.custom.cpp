a2c176622646b326730d86bf02299b22dfa840d8
rnagaraj <rahul.nagaraju@ni.com>
2024-10-15 16:59:30 +0530

    Pulse Initial changes

---
+++ source/custom/nirfmxpulse_service.custom.cpp
@@ -0,0 +1,19 @@
+#include <nirfmxpulse/nirfmxpulse_service.h>
+
+namespace nirfmxpulse_grpc {
+
+::grpc::Status NiRFmxPulseService::ConvertApiErrorStatusForNiRFmxInstrHandle(::grpc::ServerContextBase* context, int32_t status, niRFmxInstrHandle instrumentHandle)
+{
+  ViStatus error_code{};
+  std::string description(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
+  // Try first to get the most recent error with a dynamic message.
+  library_->GetError(instrumentHandle, &error_code, nidevice_grpc::kMaxGrpcErrorDescriptionSize, &description[0]);
+  if (error_code != status) {
+    // Since another thread has changed the status, fall back to the static message lookup.
+    description.assign(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
+    library_->GetErrorString(instrumentHandle, status, nidevice_grpc::kMaxGrpcErrorDescriptionSize, &description[0]);
+  }
+  return nidevice_grpc::ApiErrorAndDescriptionToStatus(context, status, description);
+}
+
+}  // namespace nirfmxpulse_grpc
