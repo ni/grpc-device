4e6c3eb42058e51285611db801b9f12ac4a8cbf6
Rahul Nagaraju <67006642+RahulBenaka@users.noreply.github.com>
2024-10-21 12:07:38 +0530

    Pulse Initial Service Changes (#1111)
    * Adding LTE List APIs
    
    * Pulse Initial changes
    
    * Update code-readiness to "NextRelease" in config.py file
    
    Co-authored-by: Ryan Eckenrode <77176215+reckenro@users.noreply.github.com>
    
    * generating code after changing CodeReadiness flag from Release to NextRelease
    
    ---------
    
    Co-authored-by: rnagaraj <rahul.nagaraju@ni.com>
    Co-authored-by: Ryan Eckenrode <77176215+reckenro@users.noreply.github.com>

--- generated/register_all_services.cpp
+++ generated/register_all_services.cpp
@@ -45,6 +45,9 @@
 #include "nirfmxnr/nirfmxnr_service_registrar.h"
 #endif // defined(_MSC_VER)
 #if defined(_MSC_VER)
+#include "nirfmxpulse/nirfmxpulse_service_registrar.h"
+#endif // defined(_MSC_VER)
+#if defined(_MSC_VER)
 #include "nirfmxspecan/nirfmxspecan_service_registrar.h"
 #endif // defined(_MSC_VER)
 #if defined(_MSC_VER)
@@ -202,6 +205,13 @@ std::shared_ptr<std::vector<std::shared_ptr<void>>> register_all_services(
       server_builder, 
       ni_r_fmx_instr_handle_repository,
       vi_session_repository,
+      feature_toggles));
+#endif // defined(_MSC_VER)
+#if defined(_MSC_VER)
+  service_vector->push_back(
+    nirfmxpulse_grpc::register_service(
+      server_builder, 
+      ni_r_fmx_instr_handle_repository,
       feature_toggles));
 #endif // defined(_MSC_VER)
 #if defined(_MSC_VER)
