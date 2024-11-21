886e6b0139f930ab3e1549716b87f61ced805733
rnagaraj <rahul.nagaraju@ni.com>
2024-11-21 18:59:42 +0530

    Updating the version to 25.0.0

--- source/codegen/metadata/nirfmxvna/config.py
+++ source/codegen/metadata/nirfmxvna/config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 config = {
-    'api_version': '24.8.0',
+    'api_version': '25.0.0',
     'c_header': 'niRFmxVNA.h',
     'c_function_prefix': 'RFmxVNA_',
     'service_class_prefix': 'NiRFmxVNA',
