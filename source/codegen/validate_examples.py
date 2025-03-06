4cfb33c022a51c683cff9ae3bc32970708597580
Christian Aguilar <christian.aguilar@ni.com>
2025-03-06 13:53:54 -0600

    ran black over file

--- source/codegen/validate_examples.py
+++ source/codegen/validate_examples.py
@@ -65,7 +65,7 @@ def _validate_examples(
 
         proto_files_str = str.join(" ", [file.name for file in proto_dir.glob("*.proto")])
 
-        move({examples_dir}/"pyproject.toml", {staging_dir})
+        move({examples_dir} / "pyproject.toml", {staging_dir})
         _system("poetry install")
 
         _system(
