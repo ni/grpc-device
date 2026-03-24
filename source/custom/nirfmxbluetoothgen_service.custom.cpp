de89464536d70670362330071d10cb39216f58f9
rnagaraj <rahul.nagaraju@ni.com>
2026-03-24 15:16:17 +0530

    Fixing error function for visession case

--- source/custom/nirfmxbluetoothgen_service.custom.cpp
+++ source/custom/nirfmxbluetoothgen_service.custom.cpp
@@ -16,14 +16,15 @@ namespace nirfmxbluetoothgen_grpc {
 
 ::grpc::Status NiRFmxBluetoothGenService::ConvertApiErrorStatusForViSession(::grpc::ServerContextBase* context, int32_t status, ViSession RFSGHandle)
 {
-  ViStatus error_code{};
-  std::string description(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
-  if (status < 0) {
-    // Since another thread has changed the status, fall back to the static message lookup.
-    description.assign(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
-    library_->GetErrorString((unsigned long long *)RFSGHandle, status, &description[0], nidevice_grpc::kMaxGrpcErrorDescriptionSize);
-  }
-  return nidevice_grpc::ApiErrorAndDescriptionToStatus(context, status, description);
+  // ViStatus error_code{};
+  // std::string description(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
+  // if (status < 0) {
+  //   // Since another thread has changed the status, fall back to the static message lookup.
+  //   description.assign(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
+  //   library_->GetErrorString((unsigned long long *)RFSGHandle, status, &description[0], nidevice_grpc::kMaxGrpcErrorDescriptionSize);
+  // }
+  // return nidevice_grpc::ApiErrorAndDescriptionToStatus(context, status, description);
+  return ::grpc::Status(grpc::StatusCode::UNKNOWN, "NI-BLUETOOTHGEN error handling for RFSG error is not implemented, call GetError from RFSG driver instead.");
 }
 
 }  // namespace nirfmxbluetoothgen_grpc
--- source/custom/nirfmxwlangen_service.custom.cpp
+++ source/custom/nirfmxwlangen_service.custom.cpp
@@ -16,14 +16,15 @@ namespace nirfmxwlangen_grpc {
 
 ::grpc::Status NiRFmxWLANGenService::ConvertApiErrorStatusForViSession(::grpc::ServerContextBase* context, int32_t status, ViSession RFSGHandle)
 {
-  ViStatus error_code{};
-  std::string description(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
-  if (status < 0) {
-    // Since another thread has changed the status, fall back to the static message lookup.
-    description.assign(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
-    library_->GetErrorString((unsigned long long*)RFSGHandle, status, &description[0], nidevice_grpc::kMaxGrpcErrorDescriptionSize);
-  }
-  return nidevice_grpc::ApiErrorAndDescriptionToStatus(context, status, description);
+  // ViStatus error_code{};
+  // std::string description(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
+  // if (status < 0) {
+  //   // Since another thread has changed the status, fall back to the static message lookup.
+  //   description.assign(nidevice_grpc::kMaxGrpcErrorDescriptionSize, '\0');
+  //   library_->GetErrorString((unsigned long long*)RFSGHandle, status, &description[0], nidevice_grpc::kMaxGrpcErrorDescriptionSize);
+  // }
+  // return nidevice_grpc::ApiErrorAndDescriptionToStatus(context, status, description);
+  return ::grpc::Status(grpc::StatusCode::UNKNOWN, "NI-WLANGEN error handling for RFSG error is not implemented, call GetError from RFSG driver instead.");
 }
 
 }  // namespace nirfmxwlangen_grpc
