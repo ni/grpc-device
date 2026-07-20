ed52e9c38b13f9b222fd7bdba2a3bb870e407a77
Raghavan Vasudevan <raghavan.vasudevan@emerson.com>
2026-07-20 11:15:13 -0500

    Add enum values for new deembedding types
    Signed-off-by: Raghavan Vasudevan <raghavan.vasudevan@emerson.com>

--- source/codegen/metadata/nirfsa/enums.py
+++ source/codegen/metadata/nirfsa/enums.py
@@ -330,6 +330,20 @@ enums = {
                 },
                 'name': 'VECTOR',
                 'value': 3902
+            },
+            {
+                'documentation': {
+                    'description': ' De-embeds the measurement using wideband amplitude flatness correction.'
+                },
+                'name': 'AMPLITUDE_FLATNESS',
+                'value': 3903
+            },
+            {
+                'documentation': {
+                    'description': ' De-embeds the measurement using wideband amplitude and phase flatness correction.'
+                },
+                'name': 'AMPLITUDE_AND_PHASE_FLATNESS',
+                'value': 3904
             }
         ]
     },
