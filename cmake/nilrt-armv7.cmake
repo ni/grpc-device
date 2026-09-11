3c5ae81290e8b78dd2475927c22271d8a4b06fa8
Mark Silva <mark.silva@emerson.com>
2026-09-11 10:30:34 -0400

    Add NI Linux RT ARMv7 artifacts
    Signed-off-by: Mark Silva <mark.silva@emerson.com>

--- cmake/nilrt-armv7.cmake
+++ cmake/nilrt-armv7.cmake
@@ -1,38 +1,36 @@
 #----------------------------------------------------------------------
-# CMake toolchain file for cross-compiling for NI Linux Real-Time
+# CMake toolchain file for cross-compiling for NI Linux Real-Time ARMv7
 #----------------------------------------------------------------------
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_SYSTEM_PROCESSOR armv7-a)
 
 #----------------------------------------------------------------------
 # Path variables for toolchains
 #----------------------------------------------------------------------
-if(NILRT_TOOLCHAIN_ROOT)
-	set(toolchain_path ${NILRT_TOOLCHAIN_ROOT}/sysroots)
-else()
-	find_program(COMPILER_PATH x86_64-nilrt-linux-gcc REQUIRED)
-	get_filename_component(toolchain_path ${COMPILER_PATH}/../../../../.. REALPATH DIRECTORY)
+if(NOT NILRT_TOOLCHAIN_ROOT)
+  message(FATAL_ERROR "NILRT_TOOLCHAIN_ROOT must point to the extracted NI Linux RT toolchain")
 endif()
-set(include_path core2-64-nilrt-linux/usr/include/c++/6.3.0)
+set(toolchain_path ${NILRT_TOOLCHAIN_ROOT}/sysroots)
+set(include_path cortexa9-vfpv3-nilrt-linux-gnueabi/usr/include/c++/6.3.0)
 
 #----------------------------------------------------------------------
 # Compilers
 #----------------------------------------------------------------------
-set(CMAKE_C_COMPILER x86_64-nilrt-linux-gcc)
-set(CMAKE_CXX_COMPILER x86_64-nilrt-linux-g++)
-find_program(CMAKE_OBJCOPY x86_64-nilrt-linux-objcopy REQUIRED)
+set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
+find_program(CMAKE_OBJCOPY arm-linux-gnueabi-objcopy REQUIRED)
 
 #----------------------------------------------------------------------
 # Default compiler flags
 #----------------------------------------------------------------------
-set(CMAKE_SYSROOT ${toolchain_path}/core2-64-nilrt-linux)
+set(CMAKE_SYSROOT ${toolchain_path}/cortexa9-vfpv3-nilrt-linux-gnueabi)
 set(CMAKE_C_STANDARD_INCLUDE_DIRECTORIES ${CMAKE_SYSROOT}/usr/include)
 set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES
   ${toolchain_path}/${include_path}
-  ${toolchain_path}/${include_path}/x86_64-nilrt-linux
+  ${toolchain_path}/${include_path}/arm-nilrt-linux-gnueabi
   ${CMAKE_SYSROOT}/usr/include)
-set(CMAKE_C_FLAGS_INIT "-Wall -fmessage-length=0")
-set(CMAKE_CXX_FLAGS_INIT "-Wall -fmessage-length=0")
+set(CMAKE_C_FLAGS_INIT "-Wall -fmessage-length=0 -march=armv7-a -mfpu=vfpv3 -mfloat-abi=softfp")
+set(CMAKE_CXX_FLAGS_INIT "-Wall -fmessage-length=0 -march=armv7-a -mfpu=vfpv3 -mfloat-abi=softfp")
 set(CMAKE_C_FLAGS_DEBUG_INIT "-O0 -g3")
 set(CMAKE_CXX_FLAGS_DEBUG_INIT "-O0 -g3")
 set(CMAKE_C_FLAGS_RELEASE_INIT "-O3")
